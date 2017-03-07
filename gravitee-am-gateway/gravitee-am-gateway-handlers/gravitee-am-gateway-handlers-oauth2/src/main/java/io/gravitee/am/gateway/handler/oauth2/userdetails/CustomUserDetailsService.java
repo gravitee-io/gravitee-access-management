bda901e3f5df819e556393f0ca68f2b2e13e99ac
brasseld <brasseld@gmail.com>
2017-03-07 21:04:24 +0100

    fix(oauth2): Use same username for userDetails

--- gravitee-am-gateway/gravitee-am-gateway-handlers/gravitee-am-gateway-handlers-oauth2/src/main/java/io/gravitee/am/gateway/handler/oauth2/userdetails/CustomUserDetailsService.java
+++ gravitee-am-gateway/gravitee-am-gateway-handlers/gravitee-am-gateway-handlers-oauth2/src/main/java/io/gravitee/am/gateway/handler/oauth2/userdetails/CustomUserDetailsService.java
@@ -15,6 +15,6 @@ public class CustomUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return new User("toto", "password", Collections.emptyList());
+        return new User(username, "password", Collections.emptyList());
     }
 }
