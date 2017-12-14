53964061c306f522243f74905e9eb00b21ad0d40
brasseld <brasseld@gmail.com>
2017-12-14 13:24:29 +0100

    feat(mongodb): Add MongoDB identity-provider
    Closes gravitee-io/graviteeio-access-management#193

---
+++ gravitee-am-identityprovider/gravitee-am-identityprovider-mongo/src/main/java/io/gravitee/am/identityprovider/mongo/MongoIdentityProvider.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.am.identityprovider.mongo;
+
+import io.gravitee.am.identityprovider.api.AuthenticationProvider;
+import io.gravitee.am.identityprovider.api.IdentityProvider;
+import io.gravitee.am.identityprovider.api.IdentityProviderConfiguration;
+import io.gravitee.am.identityprovider.api.IdentityProviderMapper;
+import io.gravitee.am.identityprovider.mongo.authentication.MongoAuthenticationProvider;
+
+/**
+ * @author David BRASSELY (david.brassely at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+public class MongoIdentityProvider implements IdentityProvider {
+    public Class<? extends IdentityProviderConfiguration> configuration() {
+        return MongoIdentityProviderConfiguration.class;
+    }
+
+    public Class<? extends AuthenticationProvider> authenticationProvider() {
+        return MongoAuthenticationProvider.class;
+    }
+
+    public Class<? extends IdentityProviderMapper> mapper() {
+        return MongoIdentityProviderMapper.class;
+    }
+
+    public Class<? extends MongoIdentityProviderRoleMapper> roleMapper() {
+        return MongoIdentityProviderRoleMapper.class;
+    }
+}
