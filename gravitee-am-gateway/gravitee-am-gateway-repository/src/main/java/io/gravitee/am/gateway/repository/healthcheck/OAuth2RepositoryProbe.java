6be6ed7566386da0feda08c6f80bfde2756eedbf
Titouan COMPIEGNE <titouan.compiegne@gmail.com>
2018-11-22 18:27:20 +0100

    chore(): add health check probes for the Management API and the Gateway
    closes #453

---
+++ gravitee-am-gateway/gravitee-am-gateway-repository/src/main/java/io/gravitee/am/gateway/repository/healthcheck/OAuth2RepositoryProbe.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.am.gateway.repository.healthcheck;
+
+import io.gravitee.am.repository.oauth2.api.AccessTokenRepository;
+import io.gravitee.node.api.healthcheck.Probe;
+import io.gravitee.node.api.healthcheck.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * @author Titouan COMPIEGNE (titouan.compiegne at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+public class OAuth2RepositoryProbe implements Probe {
+
+    private static final String TOKEN = "token";
+
+    @Autowired
+    private AccessTokenRepository accessTokenRepository;
+
+    @Override
+    public String id() {
+        return "oauth2-repository";
+    }
+
+    @Override
+    public CompletableFuture<Result> check() {
+        // Search for an oauth 2.0 token to check repository connection
+        final CompletableFuture<Result> future = new CompletableFuture<>();
+
+        accessTokenRepository.findByToken(TOKEN)
+                .subscribe(
+                        domain -> future.complete(Result.healthy()),
+                        error -> future.complete(Result.unhealthy(error)),
+                        () -> future.complete(Result.healthy())); // repository is up but returned no result
+
+        return future;
+    }
+}
---
+++ gravitee-am-gateway/gravitee-am-gateway-standalone/gravitee-am-gateway-standalone-container/src/main/java/io/gravitee/am/gateway/healthcheck/HttpServerProbe.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.am.gateway.healthcheck;
+
+import io.gravitee.node.api.healthcheck.Probe;
+import io.gravitee.node.api.healthcheck.Result;
+import io.vertx.core.net.NetClientOptions;
+import io.vertx.reactivex.core.Vertx;
+import io.vertx.reactivex.core.net.NetClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * HTTP Probe used to check the gateway itself.
+ *
+ * @author Titouan COMPIEGNE (titouan.compiegne at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+public class HttpServerProbe implements Probe {
+
+    @Value("${http.port:8092}")
+    private int port;
+
+    @Value("${http.host:localhost}")
+    private String host;
+
+    @Autowired
+    private Vertx vertx;
+
+    @Override
+    public String id() {
+        return "http-server";
+    }
+
+    @Override
+    public CompletableFuture<Result> check() {
+        final CompletableFuture<Result> future = new CompletableFuture<>();
+
+        NetClientOptions options = new NetClientOptions().setConnectTimeout(500);
+        NetClient client = vertx.createNetClient(options);
+
+        client.rxConnect(port, host)
+                .subscribe(
+                        socket -> future.complete(Result.healthy()),
+                        error -> future.complete(Result.unhealthy(error.getCause())));
+        return future;
+    }
+}
