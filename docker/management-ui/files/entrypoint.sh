5bb3c5561a6b3aece4bdcca6b3e0423f48abd3e4
Pierre Letessier <pletessier@ina.fr>
2018-11-28 17:19:44 +0100

    Add the possibility to change the ui basepath to avoid the use of nginx sub_filter

--- docker/management-ui/files/entrypoint.sh
+++ docker/management-ui/files/entrypoint.sh
@@ -20,6 +20,8 @@ setup() {
     echo "Configure management API and management UI"
     cat /var/www/html/constants.json.template | \
     sed "s#http://localhost:8093/#${MGMT_API_URL}#g;s#http://localhost:4200/#${MGMT_UI_URL}#g" > /var/www/html/constants.json
+
+    [[ -n $BASEPATH ]] && sed -i "s%<base href=\"/\">%<base href=\"$BASEPATH\">%g" /var/www/html/index.html
 }
 
 setup
