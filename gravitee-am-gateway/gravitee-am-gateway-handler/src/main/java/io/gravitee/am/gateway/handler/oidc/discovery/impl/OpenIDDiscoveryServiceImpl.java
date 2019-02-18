6b2c0553831febfce0b510ad64290638e6b5e35c
Titouan COMPIEGNE <titouan.compiegne@gmail.com>
2019-02-18 16:45:11 +0100

    feat(oidc): add subject_types_supported 'public' value
    Closes gravitee-io/issues#1928

--- gravitee-am-gateway/gravitee-am-gateway-handler/src/main/java/io/gravitee/am/gateway/handler/oidc/discovery/impl/OpenIDDiscoveryServiceImpl.java
+++ gravitee-am-gateway/gravitee-am-gateway-handler/src/main/java/io/gravitee/am/gateway/handler/oidc/discovery/impl/OpenIDDiscoveryServiceImpl.java
@@ -82,7 +82,7 @@ public class OpenIDDiscoveryServiceImpl implements OpenIDDiscoveryService {
         openIDProviderMetadata.setClaimsSupported(Stream.of(Scope.values()).map(Scope::getClaims).flatMap(Collection::stream).distinct().collect(Collectors.toList()));
         openIDProviderMetadata.setCodeChallengeMethodsSupported(Arrays.asList(CodeChallengeMethod.PLAIN, CodeChallengeMethod.S256));
         openIDProviderMetadata.setClaimsParameterSupported(true);
-        //openIDProviderMetadata.setSubjectTypesSupported(Arrays.asList(SubjectType.PUBLIC));
+        openIDProviderMetadata.setSubjectTypesSupported(Arrays.asList(SubjectType.PUBLIC));
 
         return openIDProviderMetadata;
     }
