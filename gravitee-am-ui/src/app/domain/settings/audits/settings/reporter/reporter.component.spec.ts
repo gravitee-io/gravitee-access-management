826ed1349d0e6ab12f1ddb53f7e404ccf34fefe4
Titouan COMPIEGNE <titouan.compiegne@gmail.com>
2019-04-17 14:15:45 +0200

    chore(): add missing reporter spec.ts file

--- gravitee-am-ui/src/app/domain/settings/audits/settings/reporter/reporter.component.spec.ts
+++ gravitee-am-ui/src/app/domain/settings/audits/settings/reporter/reporter.component.spec.ts
@@ -13,3 +13,28 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ReporterComponent } from './reporter.component';
+
+describe('ReporterComponent', () => {
+  let component: ReporterComponent;
+  let fixture: ComponentFixture<ReporterComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ReporterComponent ]
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ReporterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should be created', () => {
+    expect(component).toBeTruthy();
+  });
+});
