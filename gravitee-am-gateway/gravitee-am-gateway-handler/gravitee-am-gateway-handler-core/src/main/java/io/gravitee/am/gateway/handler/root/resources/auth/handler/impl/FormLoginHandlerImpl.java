8fd84d32e80183128af2e35243d7601cdb08b8d3
Titouan COMPIEGNE <titouan.compiegne@gmail.com>
2019-09-16 17:12:49 +0200

    chore(): remove technical error messages to avoid potential security leaks
    closes gravitee-io/issues#2614

--- gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-core/src/main/java/io/gravitee/am/gateway/handler/root/resources/auth/handler/impl/FormLoginHandlerImpl.java
+++ gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-core/src/main/java/io/gravitee/am/gateway/handler/root/resources/auth/handler/impl/FormLoginHandlerImpl.java
@@ -18,9 +18,6 @@ package io.gravitee.am.gateway.handler.root.resources.auth.handler.impl;
 import io.gravitee.am.common.jwt.Claims;
 import io.gravitee.am.common.oauth2.Parameters;
 import io.gravitee.am.gateway.handler.common.vertx.utils.UriBuilderRequest;
-import io.gravitee.am.service.exception.authentication.AuthenticationException;
-import io.gravitee.am.service.exception.authentication.BadCredentialsException;
-import io.gravitee.am.service.exception.authentication.UsernameNotFoundException;
 import io.gravitee.common.http.HttpHeaders;
 import io.vertx.core.MultiMap;
 import io.vertx.core.http.HttpMethod;
@@ -36,7 +33,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.net.URISyntaxException;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -125,7 +121,7 @@ public class FormLoginHandlerImpl extends io.vertx.ext.web.handler.impl.FormLogi
                             req.response().end(DEFAULT_DIRECT_LOGGED_IN_OK_PAGE);
                         }
                     } else {
-                        handleException(context, res.cause());
+                        handleException(context);
                     }
                 });
             }
@@ -136,7 +132,7 @@ public class FormLoginHandlerImpl extends io.vertx.ext.web.handler.impl.FormLogi
         response.putHeader(HttpHeaders.LOCATION, url).setStatusCode(302).end();
     }
 
-    private void handleException(RoutingContext context, Throwable throwable) {
+    private void handleException(RoutingContext context) {
         final HttpServerRequest req = context.request();
         final HttpServerResponse resp = context.response();
         final MultiMap requestParameters = req.params();
@@ -146,18 +142,6 @@ public class FormLoginHandlerImpl extends io.vertx.ext.web.handler.impl.FormLogi
             Map<String, String> parameters = new LinkedHashMap<>();
             parameters.put(Parameters.CLIENT_ID, requestParameters.get(Parameters.CLIENT_ID));
             parameters.put("error", "login_failed");
-            if (throwable != null && throwable instanceof AuthenticationException) {
-                AuthenticationException authenticationException = (AuthenticationException) throwable;
-                // we don't want to expose potential security leaks
-                if (!(throwable instanceof BadCredentialsException) && !(throwable instanceof UsernameNotFoundException)) {
-                    parameters.put("error_code", authenticationException.getErrorCode());
-                } else {
-                    parameters.put("error_code", "invalid_account");
-                }
-                if (authenticationException.getDetails() != null) {
-                    parameters.putAll(authenticationException.getDetails());
-                }
-            }
             String uri = UriBuilderRequest.resolveProxyRequest(
                     new io.vertx.reactivex.core.http.HttpServerRequest(req),
                     req.uri(),
--- gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-core/src/main/java/io/gravitee/am/gateway/handler/root/resources/handler/login/LoginErrorHandler.java
+++ gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-core/src/main/java/io/gravitee/am/gateway/handler/root/resources/handler/login/LoginErrorHandler.java
@@ -15,18 +15,10 @@
  */
 package io.gravitee.am.gateway.handler.root.resources.handler.login;
 
-import io.gravitee.am.model.LoginAttempt;
-import io.gravitee.am.service.LoginAttemptService;
-import io.gravitee.am.service.exception.authentication.AccountLockedException;
-import io.vertx.core.AsyncResult;
-import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.reactivex.core.http.HttpServerRequest;
 import io.vertx.reactivex.ext.web.RoutingContext;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -36,21 +28,12 @@ import java.util.Map;
  */
 public class LoginErrorHandler implements Handler<RoutingContext> {
 
-    private static final Logger logger = LoggerFactory.getLogger(LoginErrorHandler.class);
-    private LoginAttemptService loginAttemptService;
     private static final String ERROR_PARAM = "error";
     private static final String ERROR_CODE_PARAM = "error_code";
     private static final String ERROR_DESCRIPTION_PARAM = "error_description";
-    private static final String ATTEMPT_ID_PARAM = "attempt_id";
     private static final String ERROR_CONTEXT_KEY = "error";
     private static final String ERROR_CODE_CONTEXT_KEY = "errorCode";
     private static final String ERROR_DESCRIPTION_CONTEXT_KEY = "errorDescription";
-    private static final String ERROR_DETAILS_CONTEXT_KEY = "details";
-    private static final String ACCOUNT_LOCKED_UNTIL_CONTEXT_KEY = "accountLockedUntil";
-
-    public LoginErrorHandler(LoginAttemptService loginAttemptService) {
-        this.loginAttemptService = loginAttemptService;
-    }
 
     @Override
     public void handle(RoutingContext context) {
@@ -69,37 +52,7 @@ public class LoginErrorHandler implements Handler<RoutingContext> {
         Map<String, Object> errorContext = new HashMap<>();
         errorContext.put(ERROR_CODE_CONTEXT_KEY, errorCode);
         errorContext.put(ERROR_DESCRIPTION_CONTEXT_KEY, errorDescription);
-        if (AccountLockedException.ERROR_CODE.equals(errorCode)) {
-            // handle account locked exception
-            final String attemptId = request.getParam(ATTEMPT_ID_PARAM);
-            if (attemptId != null) {
-                parseLoginAttempt(attemptId, handler -> {
-                    if (handler.failed()) {
-                        logger.debug("Failed to get login attempt information", handler.cause());
-                        context.next();
-                        return;
-                    }
-
-                    // get login attempt information
-                    LoginAttempt loginAttempt = handler.result();
-                    errorContext.put(ERROR_DETAILS_CONTEXT_KEY, Collections.singletonMap(ACCOUNT_LOCKED_UNTIL_CONTEXT_KEY, loginAttempt.getExpireAt()));
-
-                    // add data to the context
-                    context.put(ERROR_CONTEXT_KEY, errorContext);
-                    context.next();
-                });
-            }
-        } else {
-            context.put(ERROR_CONTEXT_KEY, errorContext);
-            context.next();
-        }
-    }
-
-    private void parseLoginAttempt(String attemptId, Handler<AsyncResult<LoginAttempt>> handler) {
-        loginAttemptService.findById(attemptId)
-                .subscribe(
-                        loginAttempt -> handler.handle(Future.succeededFuture(loginAttempt)),
-                        error -> handler.handle(Future.failedFuture(error)));
-
+        context.put(ERROR_CONTEXT_KEY, errorContext);
+        context.next();
     }
 }
