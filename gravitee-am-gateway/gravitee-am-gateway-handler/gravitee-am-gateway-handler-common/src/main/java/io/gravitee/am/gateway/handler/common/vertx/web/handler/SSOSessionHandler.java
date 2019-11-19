1152c71da9d6713629d54d0b402561227a788cef
Titouan COMPIEGNE <titouan.compiegne@gmail.com>
2019-11-19 11:58:03 +0100

    fix(SSO): deny access if the user is not on a shared identity provider
    fixes gravitee-io/issues#2675

--- gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-common/src/main/java/io/gravitee/am/gateway/handler/common/vertx/web/handler/SSOSessionHandler.java
+++ gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-common/src/main/java/io/gravitee/am/gateway/handler/common/vertx/web/handler/SSOSessionHandler.java
@@ -16,19 +16,30 @@
 package io.gravitee.am.gateway.handler.common.vertx.web.handler;
 
 import io.gravitee.am.common.exception.authentication.AccountDisabledException;
+import io.gravitee.am.common.exception.oauth2.InvalidRequestException;
+import io.gravitee.am.common.oauth2.Parameters;
+import io.gravitee.am.gateway.handler.common.client.ClientSyncService;
 import io.gravitee.am.gateway.handler.common.user.UserManager;
+import io.gravitee.am.model.Client;
+import io.reactivex.Completable;
+import io.reactivex.Maybe;
+import io.reactivex.Single;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
+import io.vertx.ext.web.handler.impl.HttpStatusException;
 import io.vertx.reactivex.ext.auth.User;
 import io.vertx.reactivex.ext.web.RoutingContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Optional;
+
 /**
  * SSO Session Handler to check if the user stored in the HTTP session is still "valid" upon the incoming request
  *
  * - a user is invalid if he is disabled
+ * - a user is invalid if he is not on a shared identity provider
  *
  * @author Titouan COMPIEGNE (titouan.compiegne at graviteesource.com)
  * @author GraviteeSource Team
@@ -37,9 +48,11 @@ public class SSOSessionHandler implements Handler<RoutingContext> {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SSOSessionHandler.class);
     private UserManager userManager;
+    private ClientSyncService clientSyncService;
 
-    SSOSessionHandler(UserManager userManager) {
+    SSOSessionHandler(UserManager userManager, ClientSyncService clientSyncService) {
         this.userManager = userManager;
+        this.clientSyncService = clientSyncService;
     }
 
     @Override
@@ -50,18 +63,17 @@ public class SSOSessionHandler implements Handler<RoutingContext> {
             return;
         }
 
-        // retrieve end user and check if it's authorized to call the subsequence handlers
-        User authenticatedUser = context.user();
-        io.gravitee.am.model.User endUser = ((io.gravitee.am.gateway.handler.common.vertx.web.auth.user.User) authenticatedUser.getDelegate()).getUser();
-
-        authorize(endUser, h -> {
+        authorizeUser(context, h -> {
             if (h.failed()) {
                 Throwable cause = h.cause();
-                LOGGER.debug("An error occurs while checking SSO Session upon the current user : {}", endUser.getId(), cause);
+                LOGGER.debug("An error occurs while checking SSO Session upon the current user : {}", context.user().principal(), cause);
                 if (cause instanceof AccountDisabledException) {
                     // user has been disabled, invalidate session
                     context.clearUser();
                     context.session().destroy();
+                } else if (cause instanceof InvalidRequestException) {
+                    context.fail(new HttpStatusException(403, "Invalid request for the current SSO context"));
+                    return;
                 }
             }
             context.next();
@@ -69,7 +81,30 @@ public class SSOSessionHandler implements Handler<RoutingContext> {
 
     }
 
-    private void authorize(io.gravitee.am.model.User user, Handler<AsyncResult<Void>> handler) {
+    private void authorizeUser(RoutingContext context, Handler<AsyncResult<Void>> handler) {
+        // retrieve end user and check if it's authorized to call the subsequence handlers
+        User authenticatedUser = context.user();
+        io.gravitee.am.model.User endUser = ((io.gravitee.am.gateway.handler.common.vertx.web.auth.user.User) authenticatedUser.getDelegate()).getUser();
+
+        // check account status
+        checkAccountStatus(endUser, accountHandler -> {
+            if (accountHandler.failed()) {
+                handler.handle(Future.failedFuture(accountHandler.cause()));
+                return;
+            }
+            // additional check
+            checkClient(context, endUser, clientHandler -> {
+                if (clientHandler.failed()) {
+                    handler.handle(Future.failedFuture(clientHandler.cause()));
+                    return;
+                }
+                // continue
+                handler.handle(Future.succeededFuture());
+            });
+        });
+    }
+
+    private void checkAccountStatus(io.gravitee.am.model.User user, Handler<AsyncResult<Void>> handler) {
         userManager.get(user.getId())
                 .subscribe(
                         user1 -> {
@@ -83,4 +118,59 @@ public class SSOSessionHandler implements Handler<RoutingContext> {
                         error -> handler.handle(Future.failedFuture(error)),
                         () -> handler.handle(Future.succeededFuture()));
     }
+
+    private void checkClient(RoutingContext context, io.gravitee.am.model.User user, Handler<AsyncResult<Void>> handler) {
+        final String clientId = context.request().getParam(Parameters.CLIENT_ID);
+        // no client to check, continue
+        if (clientId == null) {
+            handler.handle(Future.succeededFuture());
+            return;
+        }
+        // no client to check for the user, continue
+        if (user.getClient() == null) {
+            handler.handle(Future.succeededFuture());
+            return;
+        }
+        // check if both clients (requested and user client) share the same identity provider
+        Single.zip(getClient(clientId), getClient(user.getClient()), (optRequestedClient, optUserClient) -> {
+            Client requestedClient = optRequestedClient.get();
+            Client userClient = optUserClient.get();
+
+            // no client to check, continue
+            if (requestedClient == null) {
+                return Completable.complete();
+            }
+
+            // no client to check for the user, continue
+            if (userClient == null) {
+                return Completable.complete();
+            }
+
+            // if same client, nothing to do, continue
+            if (userClient.getId().equals(requestedClient.getId())) {
+                return Completable.complete();
+            }
+
+            // both clients are sharing the same provider, continue
+            if (requestedClient.getIdentities() == null || requestedClient.getIdentities().contains(user.getSource())) {
+                return Completable.complete();
+            }
+
+            // throw error
+            throw new InvalidRequestException("User is not on a shared identity provider");
+        }).subscribe(
+            __ -> handler.handle(Future.succeededFuture()),
+            error -> handler.handle(Future.failedFuture(error)));
+
+    }
+
+    private Single<Optional<Client>> getClient(String clientId) {
+        return clientSyncService.findById(clientId)
+                .switchIfEmpty(Maybe.defer(() -> clientSyncService.findByClientId(clientId)))
+                .map(Optional::ofNullable)
+                .defaultIfEmpty(Optional.empty())
+                .toSingle();
+    }
+
+
 }
