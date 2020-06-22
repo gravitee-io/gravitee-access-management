fd9e726d9517d9c337e6e9faab65bff6afa11379
Alexandre Faria <16895682+lusoalex@users.noreply.github.com>
2020-06-22 20:32:34 +0200

    merge permission resource ids

--- gravitee-am-service/src/test/java/io/gravitee/am/service/PermissionTicketServiceTest.java
+++ gravitee-am-service/src/test/java/io/gravitee/am/service/PermissionTicketServiceTest.java
@@ -26,16 +26,15 @@ import io.reactivex.Completable;
 import io.reactivex.Maybe;
 import io.reactivex.Single;
 import io.reactivex.observers.TestObserver;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -164,6 +163,44 @@ public class PermissionTicketServiceTest {
 
         testObserver.assertNoErrors().assertComplete().assertValue(permissionTicket -> "success".equals(permissionTicket.getId()));
         verify(repository, times(1)).create(any());
+    }
+
+    @Test
+    public void create_successMultipleResources_withDupliucatedResourceIds() {
+        //Prepare request
+        List<PermissionRequest> request = Arrays.asList(
+                new PermissionRequest().setResourceId("one").setResourceScopes(new ArrayList<>(Arrays.asList("a","b"))),
+                new PermissionRequest().setResourceId("one").setResourceScopes(new ArrayList<>(Arrays.asList("c"))),
+                new PermissionRequest().setResourceId("two").setResourceScopes(new ArrayList<>(Arrays.asList("c","d")))
+        );
+
+        // Prepare Resource
+        List<Resource> found = Arrays.asList(
+                new Resource().setId("one").setResourceScopes(Arrays.asList("a","b","c")),
+                new Resource().setId("two").setResourceScopes(Arrays.asList("c","d"))
+        );
+
+        ArgumentCaptor<PermissionTicket> permissionTicketArgumentCaptor = ArgumentCaptor.forClass(PermissionTicket.class);
+
+        when(resourceService.findByDomainAndClientAndUserAndResources(DOMAIN_ID, CLIENT_ID, USER_ID,Arrays.asList("one","two"))).thenReturn(Single.just(found));
+        when(repository.create(permissionTicketArgumentCaptor.capture())).thenReturn(Single.just(new PermissionTicket().setId("success")));
+
+        TestObserver<PermissionTicket> testObserver = service.create(request, DOMAIN_ID, CLIENT_ID, USER_ID).test();
+
+        testObserver.assertNoErrors().assertComplete().assertValue(permissionTicket -> "success".equals(permissionTicket.getId()));
+        verify(repository, times(1)).create(any());
+
+        //Ensure resource_id have been merged
+        Assert.assertTrue(assertMerged(permissionTicketArgumentCaptor.getValue()));
+
+    }
+
+    private boolean assertMerged(PermissionTicket permissionTicket) {
+        return permissionTicket.getPermissionRequest().size() == 2 &&
+                permissionTicket.getPermissionRequest().stream().filter(pr ->
+                        (pr.getResourceId().equals("one") && pr.getResourceScopes().containsAll(Arrays.asList("a","b","c"))) ||
+                                (pr.getResourceId().equals("two") && pr.getResourceScopes().containsAll(Arrays.asList("c","d")))
+                ).count() == 2;
     }
 
     @Test
