lboullo0/graviteeio-access-management

feat(command-handlers): move org and env creation to command handlers
Closes gravitee-io/issues#4217

gravitee-am-management-api/gravitee-am-management-api-service/src/main/java/io/gravitee/am/management/service/impl/commands/EnvironmentCommandHandler.java
@@ -1,0 +1,66 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.am.management.service.impl.commands;
+
+import io.gravitee.am.service.EnvironmentService;
+import io.gravitee.am.service.model.NewEnvironment;
+import io.gravitee.cockpit.api.command.Command;
+import io.gravitee.cockpit.api.command.CommandHandler;
+import io.gravitee.cockpit.api.command.CommandStatus;
+import io.gravitee.cockpit.api.command.environment.EnvironmentCommand;
+import io.gravitee.cockpit.api.command.environment.EnvironmentReply;
+import io.gravitee.cockpit.api.command.environment.EnvironmentPayload;
+import io.reactivex.Single;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Jeoffrey HAEYAERT (jeoffrey.haeyaert at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+@Component
+public class EnvironmentCommandHandler implements CommandHandler<EnvironmentCommand, EnvironmentReply> {
+
+    private final Logger logger = LoggerFactory.getLogger(EnvironmentCommandHandler.class);
+
+    private final EnvironmentService environmentService;
+
+    public EnvironmentCommandHandler(EnvironmentService environmentService) {
+        this.environmentService = environmentService;
+    }
+
+    @Override
+    public Command.Type handleType() {
+        return Command.Type.ENVIRONMENT_COMMAND;
+    }
+
+    @Override
+    public Single<EnvironmentReply> handle(EnvironmentCommand command) {
+
+        EnvironmentPayload environmentPayload = command.getPayload();
+        NewEnvironment newEnvironment = new NewEnvironment();
+        newEnvironment.setName(environmentPayload.getName());
+        newEnvironment.setDescription(environmentPayload.getDescription());
+        newEnvironment.setDomainRestrictions(environmentPayload.getDomainRestrictions());
+
+        return environmentService.createOrUpdate(environmentPayload.getOrganizationId(), environmentPayload.getId(), newEnvironment, null)
+                .map(organization -> new EnvironmentReply(command.getId(), CommandStatus.SUCCEEDED))
+                .doOnSuccess(reply -> logger.info("Environment [{}] created with id [{}].", environmentPayload.getName(), environmentPayload.getId()))
+                .doOnError(error -> logger.info("Error occurred when creating environment [{}] with id [{}].", environmentPayload.getName(), environmentPayload.getId(), error))
+                .onErrorReturn(throwable -> new EnvironmentReply(command.getId(), CommandStatus.ERROR));
+    }
+}

gravitee-am-management-api/gravitee-am-management-api-service/src/main/java/io/gravitee/am/management/service/impl/commands/OrganizationCommandHandler.java
@@ -1,0 +1,66 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.am.management.service.impl.commands;
+
+import io.gravitee.am.service.OrganizationService;
+import io.gravitee.am.service.model.NewOrganization;
+import io.gravitee.cockpit.api.command.Command;
+import io.gravitee.cockpit.api.command.CommandHandler;
+import io.gravitee.cockpit.api.command.CommandStatus;
+import io.gravitee.cockpit.api.command.organization.OrganizationCommand;
+import io.gravitee.cockpit.api.command.organization.OrganizationReply;
+import io.gravitee.cockpit.api.command.organization.OrganizationPayload;
+import io.reactivex.Single;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Jeoffrey HAEYAERT (jeoffrey.haeyaert at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+@Component
+public class OrganizationCommandHandler implements CommandHandler<OrganizationCommand, OrganizationReply> {
+
+    private final Logger logger = LoggerFactory.getLogger(OrganizationCommandHandler.class);
+
+    private final OrganizationService organizationService;
+
+    public OrganizationCommandHandler(OrganizationService organizationService) {
+        this.organizationService = organizationService;
+    }
+
+    @Override
+    public Command.Type handleType() {
+        return Command.Type.ORGANIZATION_COMMAND;
+    }
+
+    @Override
+    public Single<OrganizationReply> handle(OrganizationCommand command) {
+
+        OrganizationPayload organizationPayload = command.getPayload();
+        NewOrganization newOrganization = new NewOrganization();
+        newOrganization.setName(organizationPayload.getName());
+        newOrganization.setDescription(organizationPayload.getDescription());
+        newOrganization.setDomainRestrictions(organizationPayload.getDomainRestrictions());
+
+        return organizationService.createOrUpdate(organizationPayload.getId(), newOrganization, null)
+                .map(organization -> new OrganizationReply(command.getId(), CommandStatus.SUCCEEDED))
+                .doOnSuccess(reply -> logger.info("Organization [{}] created with id [{}].", organizationPayload.getName(), organizationPayload.getId()))
+                .doOnError(error -> logger.info("Error occurred when creating organization [{}] with id [{}].", organizationPayload.getName(), organizationPayload.getId(), error))
+                .onErrorReturn(throwable -> new OrganizationReply(command.getId(), CommandStatus.ERROR));
+    }
+}

gravitee-am-management-api/gravitee-am-management-api-service/src/test/java/io/gravitee/am/management/service/impl/commands/OrganizationCommandHandlerTest.java
@@ -1,0 +1,105 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.am.management.service.impl.commands;
+
+import io.gravitee.am.model.Organization;
+import io.gravitee.am.repository.exceptions.TechnicalException;
+import io.gravitee.am.service.OrganizationService;
+import io.gravitee.am.service.model.NewOrganization;
+import io.gravitee.cockpit.api.command.Command;
+import io.gravitee.cockpit.api.command.CommandStatus;
+import io.gravitee.cockpit.api.command.organization.OrganizationCommand;
+import io.gravitee.cockpit.api.command.organization.OrganizationPayload;
+import io.gravitee.cockpit.api.command.organization.OrganizationReply;
+import io.reactivex.Single;
+import io.reactivex.observers.TestObserver;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.when;
+
+/**
+ * @author Jeoffrey HAEYAERT (jeoffrey.haeyaert at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class OrganizationCommandHandlerTest {
+
+    @Mock
+    private OrganizationService organizationService;
+
+    public OrganizationCommandHandler cut;
+
+    @Before
+    public void before() {
+        cut = new OrganizationCommandHandler(organizationService);
+    }
+
+    @Test
+    public void handleType() {
+        assertEquals(Command.Type.ORGANIZATION_COMMAND, cut.handleType());
+    }
+
+    @Test
+    public void handle() {
+
+        OrganizationPayload organizationPayload = new OrganizationPayload();
+        OrganizationCommand command = new OrganizationCommand(organizationPayload);
+
+        organizationPayload.setId("orga#1");
+        organizationPayload.setDescription("Organization description");
+        organizationPayload.setName("Organization name");
+        organizationPayload.setDomainRestrictions(Arrays.asList("domain.restriction1.io", "domain.restriction2.io"));
+
+        when(organizationService.createOrUpdate(eq("orga#1"),
+                argThat(newOrganization -> newOrganization.getDescription().equals(organizationPayload.getDescription())
+                        && newOrganization.getName().equals(organizationPayload.getName())
+                        && newOrganization.getDomainRestrictions().equals(organizationPayload.getDomainRestrictions())),
+                isNull())).thenReturn(Single.just(new Organization()));
+
+        TestObserver<OrganizationReply> obs = cut.handle(command).test();
+
+        obs.awaitTerminalEvent();
+        obs.assertValue(reply -> reply.getCommandId().equals(command.getId()) && reply.getCommandStatus().equals(CommandStatus.SUCCEEDED));
+    }
+
+    @Test
+    public void handleWithException() {
+
+        OrganizationPayload organizationPayload = new OrganizationPayload();
+        OrganizationCommand command = new OrganizationCommand(organizationPayload);
+
+        organizationPayload.setId("orga#1");
+        organizationPayload.setDescription("Organization description");
+        organizationPayload.setName("Organization name");
+        organizationPayload.setDomainRestrictions(Arrays.asList("domain.restriction1.io", "domain.restriction2.io"));
+
+        when(organizationService.createOrUpdate(eq("orga#1"), any(NewOrganization.class), isNull())).thenReturn(Single.error(new TechnicalException()));
+
+        TestObserver<OrganizationReply> obs = cut.handle(command).test();
+
+        obs.awaitTerminalEvent();
+        obs.assertNoErrors();
+        obs.assertValue(reply -> reply.getCommandId().equals(command.getId()) && reply.getCommandStatus().equals(CommandStatus.ERROR));
+    }
+}

gravitee-io/gravitee-access-management

gravitee-am-management-api/gravitee-am-management-api-service/src/main/java/io/gravitee/am/management/service/impl/commands/EnvironmentCommandHandler.java
@@ -1,0 +1,70 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.am.management.service.impl.commands;
+
+import io.gravitee.am.service.EnvironmentService;
+import io.gravitee.am.service.model.NewEnvironment;
+import io.gravitee.cockpit.api.command.Command;
+import io.gravitee.cockpit.api.command.CommandHandler;
+import io.gravitee.cockpit.api.command.CommandStatus;
+import io.gravitee.cockpit.api.command.environment.EnvironmentCommand;
+import io.gravitee.cockpit.api.command.environment.EnvironmentPayload;
+import io.gravitee.cockpit.api.command.environment.EnvironmentReply;
+import io.reactivex.Single;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+
+import java.util.stream.Stream;
+
+/**
+ * @author Jeoffrey HAEYAERT (jeoffrey.haeyaert at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+@Component
+public class EnvironmentCommandHandler implements CommandHandler<EnvironmentCommand, EnvironmentReply> {
+
+    private final Logger logger = LoggerFactory.getLogger(EnvironmentCommandHandler.class);
+
+    private final EnvironmentService environmentService;
+
+    public EnvironmentCommandHandler(EnvironmentService environmentService) {
+        this.environmentService = environmentService;
+    }
+
+    @Override
+    public Command.Type handleType() {
+        return Command.Type.ENVIRONMENT_COMMAND;
+    }
+
+    @Override
+    public Single<EnvironmentReply> handle(EnvironmentCommand command) {
+
+        EnvironmentPayload environmentPayload = command.getPayload();
+        NewEnvironment newEnvironment = new NewEnvironment();
+        newEnvironment.setHrids(environmentPayload.getHrids());
+        newEnvironment.setName(environmentPayload.getName());
+        newEnvironment.setDescription(environmentPayload.getDescription());
+        newEnvironment.setDomainRestrictions(environmentPayload.getDomainRestrictions());
+
+        return environmentService.createOrUpdate(environmentPayload.getOrganizationId(), environmentPayload.getId(), newEnvironment, null)
+                .map(organization -> new EnvironmentReply(command.getId(), CommandStatus.SUCCEEDED))
+                .doOnSuccess(reply -> logger.info("Environment [{}] created with id [{}].", environmentPayload.getName(), environmentPayload.getId()))
+                .doOnError(error -> logger.error("Error occurred when creating environment [{}] with id [{}].", environmentPayload.getName(), environmentPayload.getId(), error))
+                .onErrorReturn(throwable -> new EnvironmentReply(command.getId(), CommandStatus.ERROR));
+    }
+}

gravitee-am-management-api/gravitee-am-management-api-service/src/main/java/io/gravitee/am/management/service/impl/commands/OrganizationCommandHandler.java
@@ -1,0 +1,67 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.am.management.service.impl.commands;
+
+import io.gravitee.am.service.OrganizationService;
+import io.gravitee.am.service.model.NewOrganization;
+import io.gravitee.cockpit.api.command.Command;
+import io.gravitee.cockpit.api.command.CommandHandler;
+import io.gravitee.cockpit.api.command.CommandStatus;
+import io.gravitee.cockpit.api.command.organization.OrganizationCommand;
+import io.gravitee.cockpit.api.command.organization.OrganizationPayload;
+import io.gravitee.cockpit.api.command.organization.OrganizationReply;
+import io.reactivex.Single;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Jeoffrey HAEYAERT (jeoffrey.haeyaert at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+@Component
+public class OrganizationCommandHandler implements CommandHandler<OrganizationCommand, OrganizationReply> {
+
+    private final Logger logger = LoggerFactory.getLogger(OrganizationCommandHandler.class);
+
+    private final OrganizationService organizationService;
+
+    public OrganizationCommandHandler(OrganizationService organizationService) {
+        this.organizationService = organizationService;
+    }
+
+    @Override
+    public Command.Type handleType() {
+        return Command.Type.ORGANIZATION_COMMAND;
+    }
+
+    @Override
+    public Single<OrganizationReply> handle(OrganizationCommand command) {
+
+        OrganizationPayload organizationPayload = command.getPayload();
+        NewOrganization newOrganization = new NewOrganization();
+        newOrganization.setHrids(organizationPayload.getHrids());
+        newOrganization.setName(organizationPayload.getName());
+        newOrganization.setDescription(organizationPayload.getDescription());
+        newOrganization.setDomainRestrictions(organizationPayload.getDomainRestrictions());
+
+        return organizationService.createOrUpdate(organizationPayload.getId(), newOrganization, null)
+                .map(organization -> new OrganizationReply(command.getId(), CommandStatus.SUCCEEDED))
+                .doOnSuccess(reply -> logger.info("Organization [{}] created with id [{}].", organizationPayload.getName(), organizationPayload.getId()))
+                .doOnError(error -> logger.error("Error occurred when creating organization [{}] with id [{}].", organizationPayload.getName(), organizationPayload.getId(), error))
+                .onErrorReturn(throwable -> new OrganizationReply(command.getId(), CommandStatus.ERROR));
+    }
+}

gravitee-am-management-api/gravitee-am-management-api-service/src/test/java/io/gravitee/am/management/service/impl/commands/OrganizationCommandHandlerTest.java
@@ -1,0 +1,108 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.am.management.service.impl.commands;
+
+import io.gravitee.am.model.Organization;
+import io.gravitee.am.repository.exceptions.TechnicalException;
+import io.gravitee.am.service.OrganizationService;
+import io.gravitee.am.service.model.NewOrganization;
+import io.gravitee.cockpit.api.command.Command;
+import io.gravitee.cockpit.api.command.CommandStatus;
+import io.gravitee.cockpit.api.command.organization.OrganizationCommand;
+import io.gravitee.cockpit.api.command.organization.OrganizationPayload;
+import io.gravitee.cockpit.api.command.organization.OrganizationReply;
+import io.reactivex.Single;
+import io.reactivex.observers.TestObserver;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.when;
+
+/**
+ * @author Jeoffrey HAEYAERT (jeoffrey.haeyaert at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class OrganizationCommandHandlerTest {
+
+    @Mock
+    private OrganizationService organizationService;
+
+    public OrganizationCommandHandler cut;
+
+    @Before
+    public void before() {
+        cut = new OrganizationCommandHandler(organizationService);
+    }
+
+    @Test
+    public void handleType() {
+        assertEquals(Command.Type.ORGANIZATION_COMMAND, cut.handleType());
+    }
+
+    @Test
+    public void handle() {
+
+        OrganizationPayload organizationPayload = new OrganizationPayload();
+        OrganizationCommand command = new OrganizationCommand(organizationPayload);
+
+        organizationPayload.setId("orga#1");
+        organizationPayload.setHrids(Collections.singletonList("orga-1"));
+        organizationPayload.setDescription("Organization description");
+        organizationPayload.setName("Organization name");
+        organizationPayload.setDomainRestrictions(Arrays.asList("domain.restriction1.io", "domain.restriction2.io"));
+
+        when(organizationService.createOrUpdate(eq("orga#1"),
+                argThat(newOrganization -> newOrganization.getHrids().equals(organizationPayload.getHrids())
+                        && newOrganization.getDescription().equals(organizationPayload.getDescription())
+                        && newOrganization.getName().equals(organizationPayload.getName())
+                        && newOrganization.getDomainRestrictions().equals(organizationPayload.getDomainRestrictions())),
+                isNull())).thenReturn(Single.just(new Organization()));
+
+        TestObserver<OrganizationReply> obs = cut.handle(command).test();
+
+        obs.awaitTerminalEvent();
+        obs.assertValue(reply -> reply.getCommandId().equals(command.getId()) && reply.getCommandStatus().equals(CommandStatus.SUCCEEDED));
+    }
+
+    @Test
+    public void handleWithException() {
+
+        OrganizationPayload organizationPayload = new OrganizationPayload();
+        OrganizationCommand command = new OrganizationCommand(organizationPayload);
+
+        organizationPayload.setId("orga#1");
+        organizationPayload.setDescription("Organization description");
+        organizationPayload.setName("Organization name");
+        organizationPayload.setDomainRestrictions(Arrays.asList("domain.restriction1.io", "domain.restriction2.io"));
+
+        when(organizationService.createOrUpdate(eq("orga#1"), any(NewOrganization.class), isNull())).thenReturn(Single.error(new TechnicalException()));
+
+        TestObserver<OrganizationReply> obs = cut.handle(command).test();
+
+        obs.awaitTerminalEvent();
+        obs.assertNoErrors();
+        obs.assertValue(reply -> reply.getCommandId().equals(command.getId()) && reply.getCommandStatus().equals(CommandStatus.ERROR));
+    }
+}