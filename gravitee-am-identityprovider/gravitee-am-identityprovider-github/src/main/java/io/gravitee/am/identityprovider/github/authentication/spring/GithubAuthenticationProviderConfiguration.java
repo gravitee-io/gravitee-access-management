micivray/graviteeio-access-management

feat(proxy): add http proxy support for all identity providers
Closes gravitee-io/issues#4396

gravitee-am-identityprovider/gravitee-am-identityprovider-github/src/main/java/io/gravitee/am/identityprovider/github/authentication/spring/GithubAuthenticationProviderConfiguration.java
@@ -16,6 +16,7 @@
 package io.gravitee.am.identityprovider.github.authentication.spring;
 
 import io.gravitee.am.identityprovider.github.GithubIdentityProviderConfiguration;
+import io.gravitee.am.service.spring.WebClientBuilder;
 import io.vertx.ext.web.client.WebClientOptions;
 import io.vertx.reactivex.core.Vertx;
 import io.vertx.reactivex.ext.web.client.WebClient;
@@ -40,14 +41,20 @@ public class GithubAuthenticationProviderConfiguration {
     private GithubIdentityProviderConfiguration configuration;
 
     @Bean
+    public WebClientBuilder webClientBuilder() {
+        return new WebClientBuilder();
+    }
+
+    @Bean
     @Qualifier("gitHubWebClient")
-    public WebClient httpClient() {
+    public WebClient httpClient(WebClientBuilder webClientBuilder) {
         WebClientOptions httpClientOptions = new WebClientOptions();
         httpClientOptions
                 .setUserAgent(DEFAULT_USER_AGENT)
                 .setConnectTimeout(configuration.getConnectTimeout())
-                .setMaxPoolSize(configuration.getMaxPoolSize());
+                .setMaxPoolSize(configuration.getMaxPoolSize())
+                .setSsl(true); // TLS is mandatory for GitHub IdP.
 
-        return WebClient.create(vertx, httpClientOptions);
+        return webClientBuilder.createWebClient(vertx, httpClientOptions);
     }
 }

gravitee-am-identityprovider/gravitee-am-identityprovider-github/src/test/java/io/gravitee/am/identityprovider/github/authentication/GithubAuthenticationProviderTestConfiguration.java
@@ -19,6 +19,7 @@ import io.gravitee.am.identityprovider.api.AuthenticationProvider;
 import io.gravitee.am.identityprovider.github.GithubIdentityProviderConfiguration;
 import io.gravitee.am.identityprovider.github.GithubIdentityProviderMapper;
 import io.gravitee.am.identityprovider.github.GithubIdentityProviderRoleMapper;
+import io.gravitee.am.service.spring.WebClientBuilder;
 import io.vertx.reactivex.core.Vertx;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -68,5 +69,10 @@ public class GithubAuthenticationProviderTestConfiguration {
     @Bean
     public GithubIdentityProviderMapper mapper() {
         return new GithubIdentityProviderMapper();
+    }
+
+    @Bean
+    public WebClientBuilder webClientBuilder() {
+        return new WebClientBuilder();
     }
 }

gravitee-am-identityprovider/gravitee-am-identityprovider-http/src/main/java/io/gravitee/am/identityprovider/http/authentication/spring/HttpAuthenticationProviderConfiguration.java
@@ -16,6 +16,7 @@
 package io.gravitee.am.identityprovider.http.authentication.spring;
 
 import io.gravitee.am.identityprovider.http.configuration.HttpIdentityProviderConfiguration;
+import io.gravitee.am.service.spring.WebClientBuilder;
 import io.vertx.ext.web.client.WebClientOptions;
 import io.vertx.reactivex.core.Vertx;
 import io.vertx.reactivex.ext.web.client.WebClient;
@@ -40,20 +41,27 @@ public class HttpAuthenticationProviderConfiguration {
     private HttpIdentityProviderConfiguration configuration;
 
     @Bean
+    public WebClientBuilder webClientBuilder() {
+        return new WebClientBuilder();
+    }
+
+    @Bean
     @Qualifier("idpHttpAuthWebClient")
-    public WebClient httpClient() {
+    public WebClient httpClient(WebClientBuilder webClientBuilder) {
         WebClientOptions httpClientOptions = new WebClientOptions();
         httpClientOptions
                 .setUserAgent(DEFAULT_USER_AGENT)
                 .setConnectTimeout(configuration.getConnectTimeout())
                 .setMaxPoolSize(configuration.getMaxPoolSize());
 
-        if (configuration.getAuthenticationResource().getBaseURL() != null
-                && configuration.getAuthenticationResource().getBaseURL().startsWith("https://")) {
+        final boolean ssl = configuration.getAuthenticationResource().getBaseURL() != null
+                && configuration.getAuthenticationResource().getBaseURL().startsWith("https://");
+        if (ssl) {
             httpClientOptions.setSsl(true);
             httpClientOptions.setTrustAll(true);
         }
 
-        return WebClient.create(vertx, httpClientOptions);
+
+        return webClientBuilder.createWebClient(vertx, httpClientOptions);
     }
 }

gravitee-am-identityprovider/gravitee-am-identityprovider-http/src/test/java/io/gravitee/am/identityprovider/http/authentication/HttpAuthenticationProviderTestConfiguration.java
@@ -21,6 +21,7 @@ import io.gravitee.am.identityprovider.http.HttpIdentityProviderRoleMapper;
 import io.gravitee.am.identityprovider.http.configuration.HttpIdentityProviderConfiguration;
 import io.gravitee.am.identityprovider.http.configuration.HttpResourceConfiguration;
 import io.gravitee.am.identityprovider.http.configuration.HttpResponseErrorCondition;
+import io.gravitee.am.service.spring.WebClientBuilder;
 import io.gravitee.common.http.HttpHeader;
 import io.gravitee.common.http.HttpMethod;
 import io.vertx.core.json.JsonObject;
@@ -82,5 +83,10 @@ public class HttpAuthenticationProviderTestConfiguration {
     @Bean
     public Vertx vertx() {
         return Vertx.vertx();
+    }
+
+    @Bean
+    public WebClientBuilder webClientBuilder() {
+        return new WebClientBuilder();
     }
 }

gravitee-am-identityprovider/gravitee-am-identityprovider-oauth2-generic/src/main/java/io/gravitee/am/identityprovider/oauth2/authentication/spring/OAuth2GenericAuthenticationProviderConfiguration.java
@@ -16,6 +16,7 @@
 package io.gravitee.am.identityprovider.oauth2.authentication.spring;
 
 import io.gravitee.am.identityprovider.oauth2.OAuth2GenericIdentityProviderConfiguration;
+import io.gravitee.am.service.spring.WebClientBuilder;
 import io.vertx.ext.web.client.WebClientOptions;
 import io.vertx.reactivex.core.Vertx;
 import io.vertx.reactivex.ext.web.client.WebClient;
@@ -32,6 +33,7 @@ import org.springframework.context.annotation.Configuration;
 public class OAuth2GenericAuthenticationProviderConfiguration {
 
     private static final String DEFAULT_USER_AGENT = "Gravitee.io-AM/3";
+    private static final String HTTPS = "https://";
 
     @Autowired
     private Vertx vertx;
@@ -40,14 +42,33 @@ public class OAuth2GenericAuthenticationProviderConfiguration {
     private OAuth2GenericIdentityProviderConfiguration configuration;
 
     @Bean
+    public WebClientBuilder webClientBuilder() {
+        return new WebClientBuilder();
+    }
+
+    @Bean
     @Qualifier("oauthWebClient")
-    public WebClient httpClient() {
+    public WebClient httpClient(WebClientBuilder webClientBuilder) {
         WebClientOptions httpClientOptions = new WebClientOptions();
         httpClientOptions
                 .setUserAgent(DEFAULT_USER_AGENT)
                 .setConnectTimeout(configuration.getConnectTimeout())
-                .setMaxPoolSize(configuration.getMaxPoolSize());
+                .setMaxPoolSize(configuration.getMaxPoolSize())
+                .setSsl(isTLS());
 
-        return WebClient.create(vertx, httpClientOptions);
+        return webClientBuilder.createWebClient(vertx, httpClientOptions);
+    }
+
+    /**
+     * Check if all defined oauth2 urls are secured or not.
+     * This method is mainly used to determine if ssl should be enabled on the webClient used to communicate with the oauth2 server.
+     *
+     * @return <code>true</code> if all urls are secured, <code>false</code> else.
+     */
+    private boolean isTLS() {
+        return configuration.getAccessTokenUri() != null && configuration.getAccessTokenUri().startsWith(HTTPS)
+                && configuration.getUserAuthorizationUri() != null && configuration.getUserAuthorizationUri().startsWith(HTTPS)
+                && configuration.getUserProfileUri() != null && configuration.getUserProfileUri().startsWith(HTTPS)
+                && configuration.getWellKnownUri() != null && configuration.getWellKnownUri().startsWith(HTTPS);
     }
 }

gravitee-am-identityprovider/gravitee-am-identityprovider-oauth2-generic/src/test/java/io/gravitee/am/identityprovider/oauth2/authentication/OAuth2GenericAuthenticationProviderTestConfiguration.java
@@ -19,6 +19,7 @@ import io.gravitee.am.identityprovider.api.AuthenticationProvider;
 import io.gravitee.am.identityprovider.oauth2.OAuth2GenericIdentityProviderConfiguration;
 import io.gravitee.am.identityprovider.oauth2.OAuth2GenericIdentityProviderMapper;
 import io.gravitee.am.identityprovider.oauth2.OAuth2GenericIdentityProviderRoleMapper;
+import io.gravitee.am.service.spring.WebClientBuilder;
 import io.vertx.reactivex.core.Vertx;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -68,5 +69,10 @@ public class OAuth2GenericAuthenticationProviderTestConfiguration {
     @Bean
     public Vertx vertx() {
         return Vertx.vertx();
+    }
+
+    @Bean
+    public WebClientBuilder webClientBuilder() {
+        return new WebClientBuilder();
     }
 }

gravitee-am-service/src/main/java/io/gravitee/am/service/spring/WebClientBuilder.java
@@ -1,0 +1,113 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.am.service.spring;
+
+import io.vertx.core.net.ProxyOptions;
+import io.vertx.core.net.ProxyType;
+import io.vertx.ext.web.client.WebClientOptions;
+import io.vertx.reactivex.core.Vertx;
+import io.vertx.reactivex.ext.web.client.WebClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+
+/**
+ * @author Jeoffrey HAEYAERT (jeoffrey.haeyaert at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+public class WebClientBuilder {
+
+    private static final String HTTPS_SCHEME = "https";
+
+    @Value("${httpClient.timeout:10000}")
+    private int httpClientTimeout;
+
+    @Value("${httpClient.proxy.type:HTTP}")
+    private String httpClientProxyType;
+
+    @Value("${httpClient.proxy.http.host:#{systemProperties['http.proxyHost'] ?: 'localhost'}}")
+    private String httpClientProxyHttpHost;
+
+    @Value("${httpClient.proxy.http.port:#{systemProperties['http.proxyPort'] ?: 3128}}")
+    private int httpClientProxyHttpPort;
+
+    @Value("${httpClient.proxy.http.username:#{null}}")
+    private String httpClientProxyHttpUsername;
+
+    @Value("${httpClient.proxy.http.password:#{null}}")
+    private String httpClientProxyHttpPassword;
+
+    @Value("${httpClient.proxy.https.host:#{systemProperties['https.proxyHost'] ?: 'localhost'}}")
+    private String httpClientProxyHttpsHost;
+
+    @Value("${httpClient.proxy.https.port:#{systemProperties['https.proxyPort'] ?: 3128}}")
+    private int httpClientProxyHttpsPort;
+
+    @Value("${httpClient.proxy.https.username:#{null}}")
+    private String httpClientProxyHttpsUsername;
+
+    @Value("${httpClient.proxy.https.password:#{null}}")
+    private String httpClientProxyHttpsPassword;
+
+    @Value("#{systemProperties['httpClient.proxy'] == null ? false : true }")
+    private boolean isProxyConfigured;
+
+    public WebClient createWebClient(Vertx vertx, URL url) {
+
+        final int port = url.getPort() != -1 ? url.getPort() : (HTTPS_SCHEME.equals(url.getProtocol()) ? 443 : 80);
+
+        WebClientOptions options = new WebClientOptions()
+                .setDefaultPort(port)
+                .setDefaultHost(url.getHost())
+                .setKeepAlive(true)
+                .setMaxPoolSize(10)
+                .setTcpKeepAlive(true)
+                .setConnectTimeout(httpClientTimeout)
+                .setSsl(url.getProtocol().equals(HTTPS_SCHEME));
+
+        return createWebClient(vertx, options);
+    }
+
+    public WebClient createWebClient(Vertx vertx, WebClientOptions options) {
+
+        setProxySettings(options);
+        return WebClient.create(vertx, options);
+    }
+
+    private void setProxySettings(WebClientOptions options) {
+
+        if (this.isProxyConfigured) {
+            ProxyOptions proxyOptions = new ProxyOptions();
+            proxyOptions.setType(ProxyType.valueOf(httpClientProxyType));
+            if (options.isSsl()) {
+                proxyOptions.setHost(httpClientProxyHttpsHost);
+                proxyOptions.setPort(httpClientProxyHttpsPort);
+                proxyOptions.setUsername(httpClientProxyHttpsUsername);
+                proxyOptions.setPassword(httpClientProxyHttpsPassword);
+            } else {
+                proxyOptions.setHost(httpClientProxyHttpHost);
+                proxyOptions.setPort(httpClientProxyHttpPort);
+                proxyOptions.setUsername(httpClientProxyHttpUsername);
+                proxyOptions.setPassword(httpClientProxyHttpPassword);
+            }
+            options.setProxyOptions(proxyOptions);
+        }
+    }
+}

gravitee-io/gravitee-access-management

gravitee-am-identityprovider/gravitee-am-identityprovider-github/src/main/java/io/gravitee/am/identityprovider/github/authentication/spring/GithubAuthenticationProviderConfiguration.java
@@ -16,6 +16,7 @@
 package io.gravitee.am.identityprovider.github.authentication.spring;
 
 import io.gravitee.am.identityprovider.github.GithubIdentityProviderConfiguration;
+import io.gravitee.am.service.http.WebClientBuilder;
 import io.vertx.ext.web.client.WebClientOptions;
 import io.vertx.reactivex.core.Vertx;
 import io.vertx.reactivex.ext.web.client.WebClient;
@@ -40,14 +41,20 @@ public class GithubAuthenticationProviderConfiguration {
     private GithubIdentityProviderConfiguration configuration;
 
     @Bean
+    public WebClientBuilder webClientBuilder() {
+        return new WebClientBuilder();
+    }
+
+    @Bean
     @Qualifier("gitHubWebClient")
-    public WebClient httpClient() {
+    public WebClient httpClient(WebClientBuilder webClientBuilder) {
         WebClientOptions httpClientOptions = new WebClientOptions();
         httpClientOptions
                 .setUserAgent(DEFAULT_USER_AGENT)
                 .setConnectTimeout(configuration.getConnectTimeout())
-                .setMaxPoolSize(configuration.getMaxPoolSize());
+                .setMaxPoolSize(configuration.getMaxPoolSize())
+                .setSsl(true); // TLS is mandatory for GitHub IdP.
 
-        return WebClient.create(vertx, httpClientOptions);
+        return webClientBuilder.createWebClient(vertx, httpClientOptions);
     }
 }

gravitee-am-identityprovider/gravitee-am-identityprovider-github/src/test/java/io/gravitee/am/identityprovider/github/authentication/GithubAuthenticationProviderTestConfiguration.java
@@ -19,6 +19,7 @@ import io.gravitee.am.identityprovider.api.AuthenticationProvider;
 import io.gravitee.am.identityprovider.github.GithubIdentityProviderConfiguration;
 import io.gravitee.am.identityprovider.github.GithubIdentityProviderMapper;
 import io.gravitee.am.identityprovider.github.GithubIdentityProviderRoleMapper;
+import io.gravitee.am.service.http.WebClientBuilder;
 import io.vertx.reactivex.core.Vertx;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -68,5 +69,10 @@ public class GithubAuthenticationProviderTestConfiguration {
     @Bean
     public GithubIdentityProviderMapper mapper() {
         return new GithubIdentityProviderMapper();
+    }
+
+    @Bean
+    public WebClientBuilder webClientBuilder() {
+        return new WebClientBuilder();
     }
 }

gravitee-am-identityprovider/gravitee-am-identityprovider-http/src/main/java/io/gravitee/am/identityprovider/http/authentication/spring/HttpAuthenticationProviderConfiguration.java
@@ -16,6 +16,7 @@
 package io.gravitee.am.identityprovider.http.authentication.spring;
 
 import io.gravitee.am.identityprovider.http.configuration.HttpIdentityProviderConfiguration;
+import io.gravitee.am.service.http.WebClientBuilder;
 import io.vertx.ext.web.client.WebClientOptions;
 import io.vertx.reactivex.core.Vertx;
 import io.vertx.reactivex.ext.web.client.WebClient;
@@ -40,20 +41,27 @@ public class HttpAuthenticationProviderConfiguration {
     private HttpIdentityProviderConfiguration configuration;
 
     @Bean
+    public WebClientBuilder webClientBuilder() {
+        return new WebClientBuilder();
+    }
+
+    @Bean
     @Qualifier("idpHttpAuthWebClient")
-    public WebClient httpClient() {
+    public WebClient httpClient(WebClientBuilder webClientBuilder) {
         WebClientOptions httpClientOptions = new WebClientOptions();
         httpClientOptions
                 .setUserAgent(DEFAULT_USER_AGENT)
                 .setConnectTimeout(configuration.getConnectTimeout())
                 .setMaxPoolSize(configuration.getMaxPoolSize());
 
-        if (configuration.getAuthenticationResource().getBaseURL() != null
-                && configuration.getAuthenticationResource().getBaseURL().startsWith("https://")) {
+        final boolean ssl = configuration.getAuthenticationResource().getBaseURL() != null
+                && configuration.getAuthenticationResource().getBaseURL().startsWith("https://");
+        if (ssl) {
             httpClientOptions.setSsl(true);
             httpClientOptions.setTrustAll(true);
         }
 
-        return WebClient.create(vertx, httpClientOptions);
+
+        return webClientBuilder.createWebClient(vertx, httpClientOptions);
     }
 }

gravitee-am-identityprovider/gravitee-am-identityprovider-http/src/test/java/io/gravitee/am/identityprovider/http/authentication/HttpAuthenticationProviderTestConfiguration.java
@@ -21,6 +21,7 @@ import io.gravitee.am.identityprovider.http.HttpIdentityProviderRoleMapper;
 import io.gravitee.am.identityprovider.http.configuration.HttpIdentityProviderConfiguration;
 import io.gravitee.am.identityprovider.http.configuration.HttpResourceConfiguration;
 import io.gravitee.am.identityprovider.http.configuration.HttpResponseErrorCondition;
+import io.gravitee.am.service.http.WebClientBuilder;
 import io.gravitee.common.http.HttpHeader;
 import io.gravitee.common.http.HttpMethod;
 import io.vertx.core.json.JsonObject;
@@ -82,5 +83,10 @@ public class HttpAuthenticationProviderTestConfiguration {
     @Bean
     public Vertx vertx() {
         return Vertx.vertx();
+    }
+
+    @Bean
+    public WebClientBuilder webClientBuilder() {
+        return new WebClientBuilder();
     }
 }

gravitee-am-identityprovider/gravitee-am-identityprovider-oauth2-generic/src/main/java/io/gravitee/am/identityprovider/oauth2/authentication/spring/OAuth2GenericAuthenticationProviderConfiguration.java
@@ -16,6 +16,7 @@
 package io.gravitee.am.identityprovider.oauth2.authentication.spring;
 
 import io.gravitee.am.identityprovider.oauth2.OAuth2GenericIdentityProviderConfiguration;
+import io.gravitee.am.service.http.WebClientBuilder;
 import io.vertx.ext.web.client.WebClientOptions;
 import io.vertx.reactivex.core.Vertx;
 import io.vertx.reactivex.ext.web.client.WebClient;
@@ -32,6 +33,7 @@ import org.springframework.context.annotation.Configuration;
 public class OAuth2GenericAuthenticationProviderConfiguration {
 
     private static final String DEFAULT_USER_AGENT = "Gravitee.io-AM/3";
+    private static final String HTTPS = "https://";
 
     @Autowired
     private Vertx vertx;
@@ -40,14 +42,33 @@ public class OAuth2GenericAuthenticationProviderConfiguration {
     private OAuth2GenericIdentityProviderConfiguration configuration;
 
     @Bean
+    public WebClientBuilder webClientBuilder() {
+        return new WebClientBuilder();
+    }
+
+    @Bean
     @Qualifier("oauthWebClient")
-    public WebClient httpClient() {
+    public WebClient httpClient(WebClientBuilder webClientBuilder) {
         WebClientOptions httpClientOptions = new WebClientOptions();
         httpClientOptions
                 .setUserAgent(DEFAULT_USER_AGENT)
                 .setConnectTimeout(configuration.getConnectTimeout())
-                .setMaxPoolSize(configuration.getMaxPoolSize());
+                .setMaxPoolSize(configuration.getMaxPoolSize())
+                .setSsl(isTLS());
 
-        return WebClient.create(vertx, httpClientOptions);
+        return webClientBuilder.createWebClient(vertx, httpClientOptions);
+    }
+
+    /**
+     * Check if all defined oauth2 urls are secured or not.
+     * This method is mainly used to determine if ssl should be enabled on the webClient used to communicate with the oauth2 server.
+     *
+     * @return <code>true</code> if all urls are secured, <code>false</code> else.
+     */
+    private boolean isTLS() {
+        return configuration.getAccessTokenUri() != null && configuration.getAccessTokenUri().startsWith(HTTPS)
+                && configuration.getUserAuthorizationUri() != null && configuration.getUserAuthorizationUri().startsWith(HTTPS)
+                && configuration.getUserProfileUri() != null && configuration.getUserProfileUri().startsWith(HTTPS)
+                && configuration.getWellKnownUri() != null && configuration.getWellKnownUri().startsWith(HTTPS);
     }
 }

gravitee-am-identityprovider/gravitee-am-identityprovider-oauth2-generic/src/test/java/io/gravitee/am/identityprovider/oauth2/authentication/OAuth2GenericAuthenticationProviderTestConfiguration.java
@@ -19,6 +19,7 @@ import io.gravitee.am.identityprovider.api.AuthenticationProvider;
 import io.gravitee.am.identityprovider.oauth2.OAuth2GenericIdentityProviderConfiguration;
 import io.gravitee.am.identityprovider.oauth2.OAuth2GenericIdentityProviderMapper;
 import io.gravitee.am.identityprovider.oauth2.OAuth2GenericIdentityProviderRoleMapper;
+import io.gravitee.am.service.http.WebClientBuilder;
 import io.vertx.reactivex.core.Vertx;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -68,5 +69,10 @@ public class OAuth2GenericAuthenticationProviderTestConfiguration {
     @Bean
     public Vertx vertx() {
         return Vertx.vertx();
+    }
+
+    @Bean
+    public WebClientBuilder webClientBuilder() {
+        return new WebClientBuilder();
     }
 }

gravitee-am-service/src/main/java/io/gravitee/am/service/http/WebClientBuilder.java
@@ -1,0 +1,109 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.am.service.http;
+
+import io.vertx.core.net.ProxyOptions;
+import io.vertx.core.net.ProxyType;
+import io.vertx.ext.web.client.WebClientOptions;
+import io.vertx.reactivex.core.Vertx;
+import io.vertx.reactivex.ext.web.client.WebClient;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.net.URL;
+
+/**
+ * @author Jeoffrey HAEYAERT (jeoffrey.haeyaert at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+public class WebClientBuilder {
+
+    private static final String HTTPS_SCHEME = "https";
+
+    @Value("${httpClient.timeout:10000}")
+    private int httpClientTimeout;
+
+    @Value("${httpClient.proxy.type:HTTP}")
+    private String httpClientProxyType;
+
+    @Value("${httpClient.proxy.http.host:#{systemProperties['http.proxyHost'] ?: 'localhost'}}")
+    private String httpClientProxyHttpHost;
+
+    @Value("${httpClient.proxy.http.port:#{systemProperties['http.proxyPort'] ?: 3128}}")
+    private int httpClientProxyHttpPort;
+
+    @Value("${httpClient.proxy.http.username:#{null}}")
+    private String httpClientProxyHttpUsername;
+
+    @Value("${httpClient.proxy.http.password:#{null}}")
+    private String httpClientProxyHttpPassword;
+
+    @Value("${httpClient.proxy.https.host:#{systemProperties['https.proxyHost'] ?: 'localhost'}}")
+    private String httpClientProxyHttpsHost;
+
+    @Value("${httpClient.proxy.https.port:#{systemProperties['https.proxyPort'] ?: 3128}}")
+    private int httpClientProxyHttpsPort;
+
+    @Value("${httpClient.proxy.https.username:#{null}}")
+    private String httpClientProxyHttpsUsername;
+
+    @Value("${httpClient.proxy.https.password:#{null}}")
+    private String httpClientProxyHttpsPassword;
+
+    @Value("#{systemProperties['httpClient.proxy'] == null ? false : true }")
+    private boolean isProxyConfigured;
+
+    public WebClient createWebClient(Vertx vertx, URL url) {
+
+        final int port = url.getPort() != -1 ? url.getPort() : (HTTPS_SCHEME.equals(url.getProtocol()) ? 443 : 80);
+
+        WebClientOptions options = new WebClientOptions()
+                .setDefaultPort(port)
+                .setDefaultHost(url.getHost())
+                .setKeepAlive(true)
+                .setMaxPoolSize(10)
+                .setTcpKeepAlive(true)
+                .setConnectTimeout(httpClientTimeout)
+                .setSsl(url.getProtocol().equals(HTTPS_SCHEME));
+
+        return createWebClient(vertx, options);
+    }
+
+    public WebClient createWebClient(Vertx vertx, WebClientOptions options) {
+
+        setProxySettings(options);
+        return WebClient.create(vertx, options);
+    }
+
+    private void setProxySettings(WebClientOptions options) {
+
+        if (this.isProxyConfigured) {
+            ProxyOptions proxyOptions = new ProxyOptions();
+            proxyOptions.setType(ProxyType.valueOf(httpClientProxyType));
+            if (options.isSsl()) {
+                proxyOptions.setHost(httpClientProxyHttpsHost);
+                proxyOptions.setPort(httpClientProxyHttpsPort);
+                proxyOptions.setUsername(httpClientProxyHttpsUsername);
+                proxyOptions.setPassword(httpClientProxyHttpsPassword);
+            } else {
+                proxyOptions.setHost(httpClientProxyHttpHost);
+                proxyOptions.setPort(httpClientProxyHttpPort);
+                proxyOptions.setUsername(httpClientProxyHttpUsername);
+                proxyOptions.setPassword(httpClientProxyHttpPassword);
+            }
+            options.setProxyOptions(proxyOptions);
+        }
+    }
+}