Site-Command/graviteeio-access-management

feat(Gateway/Policy): addition of Enrich Auth Flow policy
fixes gravitee-io/issues#4883

gravitee-am-model/src/main/java/io/gravitee/am/model/AuthenticationFlowContext.java
@@ -1,0 +1,108 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.am.model;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * @author Eric LELEU (eric.leleu at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+public class AuthenticationFlowContext {
+    /**
+     * Context identifier
+     */
+    private String sessionId;
+
+    /**
+     * version of the context
+     */
+    private int version;
+
+    /**
+     * Context information to store
+     */
+    private Map<String, Object> data;
+
+    /**
+     * The context creation date
+     */
+    private Date createdAt;
+
+    /**
+     * The context expiration date
+     */
+    private Date expireAt;
+
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(String sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+
+    public Map<String, Object> getData() {
+        if (data == null) {
+            data = new HashMap<>();
+        }
+        return data;
+    }
+
+    public void setData(Map<String, Object> data) {
+        this.data = data;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getExpireAt() {
+        return expireAt;
+    }
+
+    public void setExpireAt(Date expireAt) {
+        this.expireAt = expireAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof AuthenticationFlowContext)) return false;
+        AuthenticationFlowContext that = (AuthenticationFlowContext) o;
+        return version == that.version && Objects.equals(sessionId, that.sessionId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(sessionId, version);
+    }
+}

gravitee-am-repository/gravitee-am-repository-jdbc/src/test/java/io/gravitee/am/repository/jdbc/common/AbstractJdbcTest.java
@@ -108,6 +108,7 @@ public abstract class AbstractJdbcTest {
             tables.add("organizations");
             tables.add("organization_identities");
             tables.add("organization_domain_restrictions");
+            tables.add("authentication_contexts");
 
             Connection connection = Flowable.fromPublisher(cnxFact.create()).blockingFirst();
             connection.beginTransaction();

gravitee-am-service/src/test/java/io/gravitee/am/service/AuthenticationFlowContextServiceTest.java
@@ -1,0 +1,108 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.am.service;
+
+import io.gravitee.am.model.AuthenticationFlowContext;
+import io.gravitee.am.repository.management.api.AuthenticationFlowContextRepository;
+import io.gravitee.am.service.exception.AuthenticationFlowConsistencyException;
+import io.gravitee.am.service.impl.AuthenticationFlowContextServiceImpl;
+import io.reactivex.Completable;
+import io.reactivex.Maybe;
+import io.reactivex.observers.TestObserver;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+/**
+ * @author Eric LELEU (eric.leleu at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class AuthenticationFlowContextServiceTest {
+    private static final String SESSION_ID = "someid";
+
+    @InjectMocks
+    private AuthenticationFlowContextServiceImpl service;
+
+    @Mock
+    private AuthenticationFlowContextRepository authFlowContextRepository;
+
+    @Test
+    public void testLoadContext_UnknownSessionId() {
+        // if sessionId is unknown load default Context
+        when(authFlowContextRepository.findLastBySession(any())).thenReturn(Maybe.empty());
+
+        TestObserver<AuthenticationFlowContext> testObserver = service.loadContext(SESSION_ID, 1).test();
+        testObserver.awaitTerminalEvent();
+        testObserver.assertNoErrors();
+        testObserver.assertValue(ctx -> ctx.getVersion() == 0);
+        verify(authFlowContextRepository).findLastBySession(any());
+    }
+
+    @Test
+    public void testLoadContext() {
+        AuthenticationFlowContext context = new AuthenticationFlowContext();
+        context.setVersion(1);
+        context.setSessionId(SESSION_ID);
+
+        // if sessionId is unknown load default Context
+        when(authFlowContextRepository.findLastBySession(any())).thenReturn(Maybe.just(context));
+
+        TestObserver<AuthenticationFlowContext> testObserver = service.loadContext(SESSION_ID, 1).test();
+        testObserver.awaitTerminalEvent();
+        testObserver.assertNoErrors();
+        testObserver.assertValue(ctx -> ctx.getVersion() == 1);
+        testObserver.assertValue(ctx -> ctx.getSessionId().equals(SESSION_ID));
+        verify(authFlowContextRepository).findLastBySession(any());
+    }
+
+    @Test
+    public void testLoadContext_ConsistencyError() {
+        AuthenticationFlowContext context = new AuthenticationFlowContext();
+        context.setVersion(1);
+        context.setSessionId(SESSION_ID);
+
+        // if sessionId is unknown load default Context
+        when(authFlowContextRepository.findLastBySession(any())).thenReturn(Maybe.just(context));
+
+        TestObserver<AuthenticationFlowContext> testObserver = service.loadContext(SESSION_ID, 2).test();
+        testObserver.awaitTerminalEvent();
+        testObserver.assertError(error -> error instanceof AuthenticationFlowConsistencyException);
+        verify(authFlowContextRepository).findLastBySession(any());
+    }
+
+    @Test
+    public void testClearContext() {
+        when(authFlowContextRepository.delete(SESSION_ID)).thenReturn(Completable.complete());
+        TestObserver<Void> testObserver = service.clearContext(SESSION_ID).test();
+        testObserver.awaitTerminalEvent();
+        testObserver.assertNoErrors();
+        verify(authFlowContextRepository).delete(SESSION_ID);
+    }
+
+    @Test
+    public void testClearContext_NullId() {
+        TestObserver<Void> testObserver = service.clearContext(null).test();
+        testObserver.awaitTerminalEvent();
+        testObserver.assertNoErrors();
+        verify(authFlowContextRepository, never()).delete(any());
+    }
+}

gravitee-io/gravitee-access-management

gravitee-am-model/src/main/java/io/gravitee/am/model/AuthenticationFlowContext.java
@@ -1,0 +1,108 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.am.model;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * @author Eric LELEU (eric.leleu at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+public class AuthenticationFlowContext {
+    /**
+     * Context identifier
+     */
+    private String transactionId;
+
+    /**
+     * version of the context
+     */
+    private int version;
+
+    /**
+     * Context information to store
+     */
+    private Map<String, Object> data;
+
+    /**
+     * The context creation date
+     */
+    private Date createdAt;
+
+    /**
+     * The context expiration date
+     */
+    private Date expireAt;
+
+    public String getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(String transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+
+    public Map<String, Object> getData() {
+        if (data == null) {
+            data = new HashMap<>();
+        }
+        return data;
+    }
+
+    public void setData(Map<String, Object> data) {
+        this.data = data;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getExpireAt() {
+        return expireAt;
+    }
+
+    public void setExpireAt(Date expireAt) {
+        this.expireAt = expireAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof AuthenticationFlowContext)) return false;
+        AuthenticationFlowContext that = (AuthenticationFlowContext) o;
+        return version == that.version && Objects.equals(transactionId, that.transactionId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(transactionId, version);
+    }
+}

gravitee-am-repository/gravitee-am-repository-jdbc/src/test/java/io/gravitee/am/repository/jdbc/common/AbstractJdbcTest.java
@@ -19,20 +19,7 @@ import io.gravitee.am.repository.jdbc.common.dialect.DatabaseDialectHelper;
 import io.r2dbc.spi.Connection;
 import io.r2dbc.spi.ConnectionFactory;
 import io.reactivex.Flowable;
-import io.reactivex.Scheduler;
 import io.reactivex.schedulers.Schedulers;
-import liquibase.change.AbstractSQLChange;
-import liquibase.changelog.ChangeLogParameters;
-import liquibase.changelog.ChangeSet;
-import liquibase.changelog.DatabaseChangeLog;
-import liquibase.database.Database;
-import liquibase.database.DatabaseFactory;
-import liquibase.parser.ChangeLogParser;
-import liquibase.parser.ChangeLogParserFactory;
-import liquibase.resource.ClassLoaderResourceAccessor;
-import liquibase.resource.FileSystemResourceAccessor;
-import liquibase.resource.ResourceAccessor;
-import liquibase.sqlgenerator.SqlGeneratorFactory;
 import org.junit.After;
 import org.junit.Before;
 import org.slf4j.Logger;
@@ -40,10 +27,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.relational.core.sql.SqlIdentifier;
 
-import java.nio.file.Paths;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.stream.Stream;
 
 /**
  * @author Eric LELEU (eric.leleu at graviteesource.com)
@@ -111,6 +96,7 @@ public abstract class AbstractJdbcTest {
             tables.add("organization_domain_restrictions");
             tables.add("organization_hrids");
             tables.add("installations");
+            tables.add("auth_flow_ctx");
 
             Connection connection = Flowable.fromPublisher(cnxFact.create()).blockingFirst();
             connection.beginTransaction();

gravitee-am-service/src/test/java/io/gravitee/am/service/AuthenticationFlowContextServiceTest.java
@@ -1,0 +1,108 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.am.service;
+
+import io.gravitee.am.model.AuthenticationFlowContext;
+import io.gravitee.am.repository.management.api.AuthenticationFlowContextRepository;
+import io.gravitee.am.service.exception.AuthenticationFlowConsistencyException;
+import io.gravitee.am.service.impl.AuthenticationFlowContextServiceImpl;
+import io.reactivex.Completable;
+import io.reactivex.Maybe;
+import io.reactivex.observers.TestObserver;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+/**
+ * @author Eric LELEU (eric.leleu at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class AuthenticationFlowContextServiceTest {
+    private static final String SESSION_ID = "someid";
+
+    @InjectMocks
+    private AuthenticationFlowContextServiceImpl service;
+
+    @Mock
+    private AuthenticationFlowContextRepository authFlowContextRepository;
+
+    @Test
+    public void testLoadContext_UnknownSessionId() {
+        // if sessionId is unknown load default Context
+        when(authFlowContextRepository.findLastByTransactionId(any())).thenReturn(Maybe.empty());
+
+        TestObserver<AuthenticationFlowContext> testObserver = service.loadContext(SESSION_ID, 1).test();
+        testObserver.awaitTerminalEvent();
+        testObserver.assertNoErrors();
+        testObserver.assertValue(ctx -> ctx.getVersion() == 0);
+        verify(authFlowContextRepository).findLastByTransactionId(any());
+    }
+
+    @Test
+    public void testLoadContext() {
+        AuthenticationFlowContext context = new AuthenticationFlowContext();
+        context.setVersion(1);
+        context.setTransactionId(SESSION_ID);
+
+        // if sessionId is unknown load default Context
+        when(authFlowContextRepository.findLastByTransactionId(any())).thenReturn(Maybe.just(context));
+
+        TestObserver<AuthenticationFlowContext> testObserver = service.loadContext(SESSION_ID, 1).test();
+        testObserver.awaitTerminalEvent();
+        testObserver.assertNoErrors();
+        testObserver.assertValue(ctx -> ctx.getVersion() == 1);
+        testObserver.assertValue(ctx -> ctx.getTransactionId().equals(SESSION_ID));
+        verify(authFlowContextRepository).findLastByTransactionId(any());
+    }
+
+    @Test
+    public void testLoadContext_ConsistencyError() {
+        AuthenticationFlowContext context = new AuthenticationFlowContext();
+        context.setVersion(1);
+        context.setTransactionId(SESSION_ID);
+
+        // if sessionId is unknown load default Context
+        when(authFlowContextRepository.findLastByTransactionId(any())).thenReturn(Maybe.just(context));
+
+        TestObserver<AuthenticationFlowContext> testObserver = service.loadContext(SESSION_ID, 2).test();
+        testObserver.awaitTerminalEvent();
+        testObserver.assertError(error -> error instanceof AuthenticationFlowConsistencyException);
+        verify(authFlowContextRepository).findLastByTransactionId(any());
+    }
+
+    @Test
+    public void testClearContext() {
+        when(authFlowContextRepository.delete(SESSION_ID)).thenReturn(Completable.complete());
+        TestObserver<Void> testObserver = service.clearContext(SESSION_ID).test();
+        testObserver.awaitTerminalEvent();
+        testObserver.assertNoErrors();
+        verify(authFlowContextRepository).delete(SESSION_ID);
+    }
+
+    @Test
+    public void testClearContext_NullId() {
+        TestObserver<Void> testObserver = service.clearContext(null).test();
+        testObserver.awaitTerminalEvent();
+        testObserver.assertNoErrors();
+        verify(authFlowContextRepository, never()).delete(any());
+    }
+}