Site-Command/graviteeio-access-management

feat(reporter): addition of file reporter
Fixe gravitee-io/issues#4731

gravitee-am-service/src/main/java/io/gravitee/am/service/reporter/vertx/EventBusReporterWrapper.java
@@ -64,6 +64,11 @@ public class EventBusReporterWrapper implements Reporter, Handler<Message<Report
     }
 
     @Override
+    public boolean handlesSearch() {
+        return this.reporter.handlesSearch();
+    }
+
+    @Override
     public void handle(Message<Reportable> reportableMsg) {
         Reportable reportable = reportableMsg.body();
 

gravitee-io/gravitee-access-management

gravitee-am-service/src/main/java/io/gravitee/am/service/reporter/vertx/EventBusReporterWrapper.java
@@ -64,6 +64,11 @@ public class EventBusReporterWrapper implements Reporter, Handler<Message<Report
     }
 
     @Override
+    public boolean canSearch() {
+        return this.reporter.canSearch();
+    }
+
+    @Override
     public void handle(Message<Reportable> reportableMsg) {
         Reportable reportable = reportableMsg.body();
 