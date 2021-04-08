2f70acedd3216f16520c2891f138440b49eee95b
eric <eric.leleu.dev@gmail.com>
2021-04-08 13:54:00 +0200

    fix(reporter): add NoOpReporter to provide empty search result if the reporter isn't ready, this avoid UI issue due to too long requets time
    Fixes gravitee-io/issues#5350

--- gravitee-am-management-api/gravitee-am-management-api-service/src/main/java/io/gravitee/am/management/service/impl/AuditReporterManagerImpl.java
+++ gravitee-am-management-api/gravitee-am-management-api-service/src/main/java/io/gravitee/am/management/service/impl/AuditReporterManagerImpl.java
@@ -20,6 +20,7 @@ import io.gravitee.am.management.service.AuditReporterManager;
 import io.gravitee.am.model.ReferenceType;
 import io.gravitee.am.model.common.event.Payload;
 import io.gravitee.am.plugins.reporter.core.ReporterPluginManager;
+import io.gravitee.am.reporter.api.provider.NoOpReporter;
 import io.gravitee.am.reporter.api.provider.Reporter;
 import io.gravitee.am.service.ReporterService;
 import io.gravitee.am.service.exception.ReporterNotFoundForDomainException;
@@ -55,7 +56,6 @@ import java.util.concurrent.ConcurrentMap;
 public class AuditReporterManagerImpl extends AbstractService<AuditReporterManager> implements AuditReporterManager, EventListener<ReporterEvent, Payload> {
 
     private static final Logger logger = LoggerFactory.getLogger(AuditReporterManagerImpl.class);
-    private static final long retryTimeout = 10000;
     private String deploymentId;
 
     @Autowired
@@ -80,12 +80,17 @@ public class AuditReporterManagerImpl extends AbstractService<AuditReporterManag
 
     private Reporter internalReporter;
 
+    private Reporter noOpReporter;
+
     @Override
     protected void doStart() throws Exception {
         super.doStart();
 
         logger.info("Register event listener for reporter events for the management API");
         eventManager.subscribeForEvents(this, ReporterEvent.class);
+
+        // init noOpReporter
+        noOpReporter = new NoOpReporter();
 
         if (useMongoReporter()) {
             logger.info("Initializing internal audit mongodb reporter");
@@ -198,7 +203,7 @@ public class AuditReporterManagerImpl extends AbstractService<AuditReporterManag
     public Reporter getReporter(ReferenceType referenceType, String referenceId) {
 
         if (referenceType == ReferenceType.DOMAIN) {
-            return doGetReporter(referenceId, System.currentTimeMillis());
+            return doGetReporter(referenceId);
         } else {
             // Internal reporter must be use for all other resources.
             return internalReporter;
@@ -207,10 +212,10 @@ public class AuditReporterManagerImpl extends AbstractService<AuditReporterManag
 
     @Override
     public Reporter getReporter(String domain) {
-        return doGetReporter(domain, System.currentTimeMillis());
+        return doGetReporter(domain);
     }
 
-    private Reporter doGetReporter(String domain, long startTime) {
+    private Reporter doGetReporter(String domain) {
         Optional<Reporter> optionalReporter = auditReporters
                 .entrySet()
                 .stream()
@@ -223,35 +228,19 @@ public class AuditReporterManagerImpl extends AbstractService<AuditReporterManag
         }
 
         // reporter can be missing as it can take sometime for the reporter events
-        // to propagate across the cluster so if the next call comes
-        // in quickly at a different node there is a possibility it isn't available yet.
+        // to propagate across the cluster so if there are at least one reporter for the domain, return the NoOpReporter to avoid
+        // too long waiting time that may lead to unexpected even on the UI.
         try {
             List<io.gravitee.am.model.Reporter> reporters = reporterService.findByDomain(domain).blockingGet();
             if (reporters.isEmpty()) {
                 throw new ReporterNotFoundForDomainException(domain);
             }
-            // retry
-            while (!optionalReporter.isPresent() && System.currentTimeMillis() - startTime < retryTimeout) {
-                optionalReporter = doGetReporter0(domain);
-            }
-            if (optionalReporter.isPresent()) {
-                return optionalReporter.get();
-            } else {
-                throw new ReporterNotFoundForDomainException(domain);
-            }
+            logger.warn("Reporter for domain {} isn't bootstrapped yet", domain);
+            return noOpReporter;
         } catch (Exception ex) {
             logger.error("An error has occurred while fetching reporter for domain {}", domain, ex);
             throw new IllegalStateException(ex);
         }
-    }
-
-    private Optional<Reporter> doGetReporter0(String domain) {
-        return auditReporters
-                .entrySet()
-                .stream()
-                .filter(entry -> domain.equals(entry.getKey().getDomain()))
-                .map(entry -> entry.getValue())
-                .findFirst();
     }
 
     private void deployReporter(String reporterId) {
---
+++ gravitee-am-reporter/gravitee-am-reporter-api/src/main/java/io/gravitee/am/reporter/api/provider/NoOpReporter.java
@@ -0,0 +1,96 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.am.reporter.api.provider;
+
+import io.gravitee.am.common.analytics.Type;
+import io.gravitee.am.common.audit.Status;
+import io.gravitee.am.model.ReferenceType;
+import io.gravitee.am.model.common.Page;
+import io.gravitee.am.reporter.api.audit.AuditReportableCriteria;
+import io.gravitee.am.reporter.api.audit.AuditReporter;
+import io.gravitee.am.reporter.api.audit.model.Audit;
+import io.gravitee.common.component.Lifecycle;
+import io.gravitee.reporter.api.Reportable;
+import io.reactivex.Maybe;
+import io.reactivex.Single;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Eric LELEU (eric.leleu at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+public class NoOpReporter implements AuditReporter {
+    private static final Logger LOGGER = LoggerFactory.getLogger(NoOpReporter.class);
+
+    @Override
+    public Single<Page<Audit>> search(ReferenceType referenceType, String referenceId, AuditReportableCriteria criteria, int page, int size) {
+        LOGGER.debug("NoOp Reporter call, real reporter not yet bootstrapped");
+        return Single.just(new Page<>(Collections.emptyList(), page, size));
+    }
+
+    @Override
+    public Single<Map<Object, Object>> aggregate(ReferenceType referenceType, String referenceId, AuditReportableCriteria criteria, Type analyticsType) {
+        LOGGER.debug("NoOp Reporter call, real reporter not yet bootstrapped");
+        switch (analyticsType) {
+            case DATE_HISTO:
+                // just fill with default values mainly for UI purpose
+                String fieldSuccess = (criteria.types().get(0) + "_" + Status.SUCCESS).toLowerCase();
+                String fieldFailure = (criteria.types().get(0) + "_" + Status.FAILURE).toLowerCase();
+                Map<Object, Object> result = new HashMap<>();
+                result.put(fieldSuccess, new ArrayList<>(Collections.nCopies(25, 0l)));
+                result.put(fieldFailure, new ArrayList<>(Collections.nCopies(25, 0l)));
+                return Single.just(result);
+            case GROUP_BY:
+                return Single.just(Collections.emptyMap());
+            case COUNT:
+                return Single.just(Collections.singletonMap("data", 0l));
+            default:
+                return Single.error(new IllegalArgumentException("Analytics [" + analyticsType + "] cannot be calculated"));
+        }
+    }
+
+    @Override
+    public Maybe<Audit> findById(ReferenceType referenceType, String referenceId, String id) {
+        LOGGER.debug("NoOp Reporter call, real reporter not yet bootstrapped");
+        return Maybe.empty();
+    }
+
+    @Override
+    public void report(Reportable reportable) {
+        LOGGER.debug("NoOp Reporter call, real reporter not yet bootstrapped");
+    }
+
+    @Override
+    public Lifecycle.State lifecycleState() {
+        return Lifecycle.State.INITIALIZED;
+    }
+
+    @Override
+    public Object start() throws Exception {
+        return this;
+    }
+
+    @Override
+    public Object stop() throws Exception {
+        return this;
+    }
+}
