404940d6cbf961e5cc7d3ebf3651cbff4ab24296
Titouan COMPIEGNE <titouan.compiegne@gmail.com>
2021-04-19 13:48:00 +0200

    fix(management): jwt generator appears to not be loaded during cockpit authentification filter construction
    fixes gravitee-io/issues#5454

--- gravitee-am-management-api/gravitee-am-management-api-rest/src/main/java/io/gravitee/am/management/handlers/management/api/authentication/filter/CockpitAuthenticationFilter.java
+++ gravitee-am-management-api/gravitee-am-management-api-rest/src/main/java/io/gravitee/am/management/handlers/management/api/authentication/filter/CockpitAuthenticationFilter.java
@@ -30,6 +30,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.util.StringUtils;
@@ -83,6 +84,7 @@ public class CockpitAuthenticationFilter extends GenericFilterBean {
     private String keyAlias;
 
     @Autowired
+    @Lazy
     private JWTGenerator jwtGenerator;
 
     @Autowired
