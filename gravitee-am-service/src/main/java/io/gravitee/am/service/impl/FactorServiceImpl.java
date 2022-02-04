08d771e3a1eca000765716110a14204125bce7f9
eric <eric.leleu.dev@gmail.com>
2022-02-04 18:14:52 +0100

    fix: manage missing node into filterSensitiveData method
    fixes gravitee-io/issues#7056

--- gravitee-am-service/src/main/java/io/gravitee/am/service/impl/FactorServiceImpl.java
+++ gravitee-am-service/src/main/java/io/gravitee/am/service/impl/FactorServiceImpl.java
@@ -46,7 +46,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 import static java.util.Objects.nonNull;
 
