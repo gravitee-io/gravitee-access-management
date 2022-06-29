96281710d89fbb90d24bcead71a4e16e2f45bd9c
eric <eric.leleu.dev@gmail.com>
2022-06-29 17:56:53 +0200

    fix: do not consider fromName as provided
    fixes gravitee-io/issues#7933

--- gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-common/src/main/java/io/gravitee/am/gateway/handler/common/email/impl/EmailServiceImpl.java
+++ gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-common/src/main/java/io/gravitee/am/gateway/handler/common/email/impl/EmailServiceImpl.java
@@ -15,6 +15,7 @@
  */
 package io.gravitee.am.gateway.handler.common.email.impl;
 
+import com.google.common.base.Strings;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
@@ -131,7 +132,7 @@ public class EmailServiceImpl implements EmailService {
                         new Template("from", new StringReader(email.getFrom()), freemarkerConfiguration),
                         params);
                 // compute email fromName
-                final String fromName = processTemplateIntoString(
+                final String fromName = Strings.isNullOrEmpty(email.getFromName()) ? null : processTemplateIntoString(
                         new Template("fromName", new StringReader(email.getFromName()), freemarkerConfiguration),
                         params);
                 // compute email subject
