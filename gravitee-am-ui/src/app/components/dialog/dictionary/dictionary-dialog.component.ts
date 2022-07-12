61118b436b8e6501efd67e1dff8ce0fa92692cd6
eric <eric.leleu.dev@gmail.com>
2022-07-12 16:43:19 +0200

    feat: UI for domain language storage.
    fixes gravitee-io/issues#8289

--- gravitee-am-ui/src/app/components/dialog/dictionary/dictionary-dialog.component.ts
+++ gravitee-am-ui/src/app/components/dialog/dictionary/dictionary-dialog.component.ts
@@ -14,35 +14,71 @@
  * limitations under the License.
  */
 import {Component, Inject, OnInit} from '@angular/core';
-import {MatDialogRef} from '@angular/material/dialog';
-import {MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {FormControl} from "@angular/forms";
+import {Observable} from "rxjs";
+import {map, startWith} from 'rxjs/operators';
+
+interface Language {
+  code: string;
+  name: string;
+}
 
 @Component({
-  selector: 'app-confirm',//TODO does this need to be unique to dictionary dialog?
+  selector: 'app-dictionary-dialog',
   templateUrl: './dictionary-dialog.component.html',
   styleUrls: ['./dictionary-dialog.component.scss']
 })
 export class DictionaryDialog implements OnInit {
 
-  public prop1: string;
-  public prop2: string;
-  public prop1Label: string;
-  public prop2Label: string;
-  public title: string;
+  prop1: string;
+  prop2: string;
+  prop1Label: string;
+  prop2Label: string;
+  title: string;
+  languageCodes: Language[];
+  selectedCode = '123';
+  languageCtrl = new FormControl('');
+  filtered: Observable<Language[]>;
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: {title: string, prop1Label: string, prop2Label: string}, public dialogRef: MatDialogRef<DictionaryDialog>) {
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: { title: string, prop1Label: string, prop2Label: string, languageCodes: string[] }, public dialogRef: MatDialogRef<DictionaryDialog>) {
     this.prop1Label = data.prop1Label;
     this.prop2Label = data.prop2Label;
     this.title = data.title;
     this.dialogRef = dialogRef;
+    this.languageCodes = [];
+    if (data.languageCodes) {
+      for (const [key, value] of data.languageCodes) {
+        this.languageCodes.push({code: key, name: value});
+      }
+    }
   }
 
   ngOnInit() {
+    this.filtered = this.languageCtrl.valueChanges.pipe(
+      startWith(''),
+      map(value => this._filter(value || "")),
+    );
   }
 
-  validate(){
+  private _filter(value: any): Language[] {
+    const filterValue = typeof value === "string" ? value.toLowerCase() : value.code.toLowerCase();
+    return this.languageCodes.filter(lang => lang.code.includes(filterValue));
+  }
+
+  validate() {
     if ((this.prop1 && this.prop1.trim().length > 0) && (this.prop2 && this.prop2.trim().length > 0)) {
       this.dialogRef.close({prop1: this.prop1, prop2: this.prop2})
     }
+  }
+
+  langSelect(event) {
+    this.prop1 = event.option.value.code;
+    this.prop2 = event.option.value.name;
+  }
+
+  displayFn(language: Language): string {
+    return language && language.code ? language.code : '';
   }
 }
