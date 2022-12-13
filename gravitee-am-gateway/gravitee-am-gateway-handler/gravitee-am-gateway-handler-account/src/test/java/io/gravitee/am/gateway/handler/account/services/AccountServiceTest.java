8532aec8cbfc7b1271893f7140d899c00ba2983f
eric <eric.leleu.dev@gmail.com>
2022-12-13 12:26:04 +0100

    AM-79 improve user profile update through SelfAccount API

--- gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-account/src/test/java/io/gravitee/am/gateway/handler/account/services/AccountServiceTest.java
+++ gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-account/src/test/java/io/gravitee/am/gateway/handler/account/services/AccountServiceTest.java
@@ -15,20 +15,30 @@
  */
 package io.gravitee.am.gateway.handler.account.services;
 
+import io.gravitee.am.common.oidc.StandardClaims;
 import io.gravitee.am.gateway.handler.account.services.impl.AccountServiceImpl;
+import io.gravitee.am.gateway.handler.common.auth.idp.IdentityProviderManager;
+import io.gravitee.am.gateway.handler.common.user.UserService;
 import io.gravitee.am.identityprovider.api.DefaultUser;
 import io.gravitee.am.identityprovider.api.User;
+import io.gravitee.am.identityprovider.api.UserProvider;
 import io.gravitee.am.model.Credential;
+import io.gravitee.am.model.Domain;
+import io.gravitee.am.repository.management.api.UserRepository;
 import io.gravitee.am.service.AuditService;
 import io.gravitee.am.service.CredentialService;
+import io.gravitee.am.service.validators.user.UserValidator;
 import io.reactivex.Completable;
 import io.reactivex.Maybe;
+import io.reactivex.Single;
 import io.reactivex.observers.TestObserver;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Map;
 
 import static org.mockito.Mockito.*;
 
@@ -43,7 +53,19 @@ public class AccountServiceTest {
     private CredentialService credentialService;
 
     @Mock
+    private UserValidator userValidator;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private IdentityProviderManager identityProviderManager;
+
+    @Mock
     private AuditService auditService;
+
+    @Mock
+    private Domain domain;
 
     @InjectMocks
     private AccountService accountService = new AccountServiceImpl();
@@ -106,5 +128,54 @@ public class AccountServiceTest {
         verify(credentialService, times(1)).findById(credentialId);
         verify(credentialService, never()).delete(credentialId);
         verify(auditService, never()).report(any());
+    }
+
+    @Test
+    public void shouldUpdateUser() {
+        final String userId = "user-id";
+        final io.gravitee.am.model.User userUpdate = new io.gravitee.am.model.User();
+        userUpdate.setSource("source");
+        userUpdate.setId(userId);
+        userUpdate.setExternalId("ext-"+userId);
+        userUpdate.setAddress(Map.of("street", "my street", "city", "my city"));
+        userUpdate.setBirthdate("01/01/1970");
+        userUpdate.setDisplayName("Display Name");
+        userUpdate.setEmail("user-update@acme.com");
+        userUpdate.setFirstName("Updated FN");
+        userUpdate.setLastName("Updated LN");
+        userUpdate.setNickName("Updated NN");
+        userUpdate.setMiddleName("Updated MN");
+        userUpdate.setLocale("fr");
+        userUpdate.setZoneInfo("UTC");
+        userUpdate.setPhoneNumber("123456789");
+        userUpdate.setPicture("https://picturestore.org/my/picture");
+        userUpdate.setProfile("https://picturestore.org/my/profile");
+        userUpdate.setWebsite("https://crazyhost/user-id");
+
+        final UserProvider userProvider = mock(UserProvider.class);
+
+        when(userValidator.validate(userUpdate)).thenReturn(Completable.complete());
+        when(identityProviderManager.getUserProvider(userUpdate.getSource())).thenReturn(Maybe.just(userProvider));
+        when(userProvider.update(any(), any())).thenReturn(Single.just(new DefaultUser()));
+        when(userRepository.update(any())).thenReturn(Single.just(userUpdate));
+
+        TestObserver testObserver = accountService.update(userUpdate).test();
+        testObserver.awaitTerminalEvent();
+        testObserver.assertComplete();
+        testObserver.assertNoErrors();
+
+        verify(userProvider).update(any(), argThat(idpUser ->
+                userUpdate.getFirstName().equals(idpUser.getFirstName()) &&
+                        userUpdate.getLastName().equals(idpUser.getLastName()) &&
+                        userUpdate.getNickName().equals(idpUser.getAdditionalInformation().get(StandardClaims.NICKNAME)) &&
+                        userUpdate.getMiddleName().equals(idpUser.getAdditionalInformation().get(StandardClaims.MIDDLE_NAME)) &&
+                        userUpdate.getBirthdate().equals(idpUser.getAdditionalInformation().get(StandardClaims.BIRTHDATE)) &&
+                        userUpdate.getPicture().equals(idpUser.getAdditionalInformation().get(StandardClaims.PICTURE)) &&
+                        userUpdate.getProfile().equals(idpUser.getAdditionalInformation().get(StandardClaims.PROFILE)) &&
+                        userUpdate.getWebsite().equals(idpUser.getAdditionalInformation().get(StandardClaims.WEBSITE)) &&
+                        userUpdate.getEmail().equals(idpUser.getEmail())&&
+                        userUpdate.getPhoneNumber().equals(idpUser.getAdditionalInformation().get(StandardClaims.PHONE_NUMBER))
+        ));
+        verify(userRepository).update(any());
     }
 }
