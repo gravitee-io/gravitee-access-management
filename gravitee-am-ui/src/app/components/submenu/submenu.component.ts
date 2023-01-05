8a69c87deb7ee5b447e01c2066497a5121177631
Titouan Compiegne <titouancompiegne@grvtfr1033.home>
2023-01-05 23:08:34 +0100

    feat(ui): integrate gio-submenu component
    closes AM-143

---
+++ gravitee-am-ui/src/app/components/submenu/submenu.component.ts
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {Router} from "@angular/router";
+import {NavigationService} from "app/services/navigation.service";
+import {Subscription} from "rxjs";
+
+@Component({
+  selector: 'gv-submenu',
+  templateUrl: './submenu.component.html',
+  styleUrls: ['./submenu.component.scss']
+})
+export class SubmenuComponent implements OnInit, OnDestroy {
+
+  @Input('displaySections') displaySections: boolean = true;
+  @Input('hasTitle') hasTitle: boolean = false;
+  @Input('isTemplate') isTemplate: boolean = false;
+  @Input('templateTooltip') templateTooltip: string;
+  @Input('componentType') componentType: string;
+  @Input('componentName') componentName: string;
+  subMenuItems: any;
+  subscription: Subscription;
+
+  constructor(private router: Router,
+              private navigationService: NavigationService) {
+  }
+
+  ngOnInit(): void {
+    this.subscription = this.navigationService.level2MenuItemsObs$
+      .subscribe(items => this.subMenuItems = items.reduce((r, a) => {
+        r[a.section] = r[a.section] || [];
+        r[a.section].push(a);
+        return r;
+      }, {}));
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+}
+
+@Component({
+  selector: 'gv-submenu-items',
+  template: `<ng-container *ngFor="let link of items">
+    <a title="{{ link.label }}" [routerLink]="link.path"
+       routerLinkActive #rla="routerLinkActive">
+      <gio-submenu-item [active]="rla.isActive">{{ link.label }}</gio-submenu-item>
+    </a>
+  </ng-container>
+  `
+})
+export class SubmenuItemsComponent {
+
+  @Input('items') items: any[];
+
+  constructor() {}
+}
