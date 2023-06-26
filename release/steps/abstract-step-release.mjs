c1640f133a96d9c7fae63d60587f697b3382e61c
Matthieu Fernandes <matthieu.fernandes@gmail.com>
2023-06-27 00:22:21 +0200

    feat: create mjs scripts to trigger release steps
    The goal of this PR is to avoid usage of `am-tooling` repo to do an AM
    release.
    
    For more information, in `release` folder after an `npm install` do:
    
    ```
    npm run am-release -- help
    ```
    
    With the new flow, this is how you can apply a full AM release:
    
    ```
    npm run am-release -- --branch master --gio-version 4.3.0 --dry-run release-git
    npm run am-release -- --branch master --gio-version 4.3.0 --dry-run publish-maven-central
    npm run am-release -- --branch master --gio-version 4.3.0 --dry-run publish-docker-images --tag-latest --no-tag-latest-support
    npm run am-release -- --branch master --gio-version 4.3.0 --dry-run publish-rpms
    npm run am-release -- --branch master --gio-version 4.3.0 --dry-run release-helm
    npm run am-release -- --branch master --gio-version 4.3.0 --dry-run release-notes
    ```
    
    Notes:
    
    * except a little change on release-notes script, this doesn't
    change the current release action on Circle-CI part. The impact here are
    only on the way to trigger it.
    * The documentation contains a mermaid diagram, in Intellij Idea you can:
    ** install the Ascidoctor plugin
    ** Then go to "Languages & Framworks" > "Asciidoc" > check "Enable
    built-in Mermaid diagram..."
    
    linked to DEVOPS-218
    
    (cherry picked from commit 7a52218b8de99896c0e058b6fae9d75e62b62785)

---
+++ release/steps/abstract-step-release.mjs
@@ -0,0 +1,30 @@
+import logger from '../helpers/logger.mjs';
+import { callCircle } from '../helpers/circleci-helper.mjs';
+
+export class AbstractStepRelease {
+  buildCommand(program, commandName) {
+    this.command = program
+      .command(commandName)
+      .usage('npm run am-release -- --branch="3.21.x" --gio-version="3.21.22" --dry-run publish-maven-central')
+      .action(async (options) => {
+        await this.action({ ...options, ...program.opts() });
+      });
+  }
+
+  async action(options) {
+    logger.debug('options: %o', options);
+
+    const customParameter = this.computePipelineParameters(options);
+
+    const pipelineArgs = {
+      branch: options.branch,
+      parameters: {
+        dry_run: options.dryRun,
+        graviteeio_version: options.gioVersion,
+        ...customParameter,
+      },
+    };
+
+    await callCircle(options.org, options.repo, pipelineArgs);
+  }
+}
