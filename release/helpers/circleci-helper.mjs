93049633663b6cb281e00582474593295397d74f
Matthieu Fernandes <matthieu.fernandes@gmail.com>
2023-06-27 00:22:21 +0200

    feat: create mjs scripts to trigger release steps
    The goal of this PR is to avoid usage of `am-tooling` repo to do an AM
    release.
    
    For more information, in `release` folder after an `npm install` do:
    
    ```
    npm run am-release -- help
    ```
    
    With the new flow, this is how you can apply a full AM release:
    
    ```
    npm run am-release -- --branch master --gio-version 4.3.0 --dry-run release-git
    npm run am-release -- --branch master --gio-version 4.3.0 --dry-run publish-maven-central
    npm run am-release -- --branch master --gio-version 4.3.0 --dry-run publish-docker-images --tag-latest --no-tag-latest-support
    npm run am-release -- --branch master --gio-version 4.3.0 --dry-run publish-rpms
    npm run am-release -- --branch master --gio-version 4.3.0 --dry-run release-helm
    npm run am-release -- --branch master --gio-version 4.3.0 --dry-run release-notes
    ```
    
    Notes:
    
    * except a little change on release-notes script, this doesn't
    change the current release action on Circle-CI part. The impact here are
    only on the way to trigger it.
    * The documentation contains a mermaid diagram, in Intellij Idea you can:
    ** install the Ascidoctor plugin
    ** Then go to "Languages & Framworks" > "Asciidoc" > check "Enable
    built-in Mermaid diagram..."
    
    linked to DEVOPS-218
    
    (cherry picked from commit 7a52218b8de99896c0e058b6fae9d75e62b62785)

--- release/helpers/circleci-helper.mjs
+++ release/helpers/circleci-helper.mjs
@@ -1,12 +1,17 @@
+import logger from './logger.mjs';
+
 require('dotenv').config();
 
 /**
  * Check if user has a valid CircleCI Token. If no token, asks him to enter one.
  * @return {Promise<void>}
  */
-export async function checkToken() {
+export async function checkCircleCIToken() {
   if (!process.env.CIRCLECI_TOKEN) {
-    console.log(chalk.red('Create .env file and add the variable CIRCLECI_TOKEN'));
+    logger.error('CIRCLECI_TOKEN environment variable is not set');
+    logger.error('Please setup your token before calling this script:');
+    logger.error('By doing: `export CIRCLECI_TOKEN=<your token>`');
+    logger.error('Or create an `.env` file and add the variable CIRCLECI_TOKEN');
     process.exit();
   }
 
@@ -18,9 +23,38 @@ export async function checkToken() {
 
   let body = await response.json();
   if (response.status === 401) {
-    console.log(chalk.red('Unauthorized CircleCI token'));
+    logger.error('Unauthorized CircleCI token');
     process.exit();
   } else {
-    console.log(chalk.green(`Logged as ${body.login}\n`));
+    logger.info(`Logged as ${body.login}\n`);
+  }
+}
+/**
+ * Check if user has a valid CircleCI Token. If no token, asks him to enter one.
+ * @return {Promise<void>}
+ */
+export async function callCircle(organisation_name, repository_name, pipeline_arguments) {
+  logger.debug(
+    `fetch https://circleci.com/api/v2/project/gh/${organisation_name}/${repository_name}/pipeline with ${JSON.stringify(
+      pipeline_arguments,
+    )}`,
+  );
+
+  const response = await fetch(`https://circleci.com/api/v2/project/gh/${organisation_name}/${repository_name}/pipeline`, {
+    method: 'post',
+    body: JSON.stringify(pipeline_arguments),
+    headers: {
+      'Content-Type': 'application/json',
+      'Circle-Token': process.env.CIRCLECI_TOKEN,
+    },
+  });
+
+  const data = await response.json();
+
+  if (response.status === 201) {
+    logger.info(`Pipeline created with number: ${data.number}`);
+    logger.info(`Follow its progress on: https://app.circleci.com/pipelines/github/${organisation_name}/${repository_name}/${data.number}`);
+  } else {
+    logger.error(`Something went wrong during a call to CircleCI to ${repository_name} with args: ${pipeline_arguments}`);
   }
 }
