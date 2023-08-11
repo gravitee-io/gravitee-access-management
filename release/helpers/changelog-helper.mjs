11197e8fb12d0deb7091cdbe8a0079c01eb1e294
Gaëtan Maisse <gaetanmaisse@gmail.com>
2023-08-11 14:02:05 +0200

    chore: update changelog generation process
    The update enhances the changelog generation process in multiple ways.
    Firstly, it uses the new documentation website and ensures that directory and branch names follow a more uniform naming pattern.
    Also, it optimizes how the changelog information is extracted from Jira by filtering out irrelevant issues and fetching additional details, enhancing the document's content.
    Temporary files used in the process are now being stored in a local '.tmp' folder, which is ignored by Git, keeping the repository clean.

--- release/helpers/changelog-helper.mjs
+++ release/helpers/changelog-helper.mjs
@@ -1,23 +1,23 @@
 /**
- * Get the Ascii doc formatted changelog for input issues
+ * Get the MarkDown doc formatted changelog for input issues
  *
  * @param title {string}
- * @param issues {Array<{id: string, fields: Array<{customfield_10115: string, summary: string}>}>}
+ * @param issues {Array<{id: string, key: string, githubIssue: string, summary: string, components: Array<string>, issueTypeName: string, statusName: string}>}
  */
 export function getChangelogFor(title, issues) {
-  const authorizedIssueTypes = ['Public Bug', 'Story'];
+  const authorizedIssueTypes = ['Public Bug', 'Public Security', 'Story'];
 
   const filteredIssues = issues
     .filter((issue) => {
-      return !!issue.fields.issuetype.name && authorizedIssueTypes.includes(issue.fields.issuetype.name);
+      return !!issue.issueTypeName && authorizedIssueTypes.includes(issue.issueTypeName);
     })
     .filter((issue) => {
-      return issue.fields.status.name === 'Done';
+      return issue.statusName === 'Done';
     })
     .sort((issue1, issue2) => {
       // if null or undefined, put it at the end
-      const githubIssueNumber1 = issue1.fields.customfield_10115;
-      const githubIssueNumber2 = issue2.fields.customfield_10115;
+      const githubIssueNumber1 = issue1.githubIssue;
+      const githubIssueNumber2 = issue2.githubIssue;
       if (!githubIssueNumber1) {
         return 1;
       }
@@ -27,13 +27,13 @@ export function getChangelogFor(title, issues) {
       return githubIssueNumber1 - githubIssueNumber2;
     })
     .map((issue) => {
-      const githubIssue = issue.fields.customfield_10115;
+      const githubIssue = issue.githubIssue;
       let publicIssueContent = '';
       if (githubIssue) {
         const githubLink = `https://github.com/gravitee-io/issues/issues/${githubIssue}`;
         publicIssueContent = ` ${githubLink}[#${githubIssue}]`;
       }
-      return `* ${issue.fields.summary}${publicIssueContent}`;
+      return `* ${issue.summary}${publicIssueContent}`;
     })
     .join('\n');
 
