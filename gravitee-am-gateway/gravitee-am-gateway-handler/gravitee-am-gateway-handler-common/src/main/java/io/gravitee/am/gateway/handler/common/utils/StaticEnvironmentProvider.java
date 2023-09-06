4a4ac669841c4f2eac829bbc47955d361a7124e9
Mariano Fernandez <mariano.fernandez@graviteesource.com>
2023-09-06 17:32:20 +0200

    fix: add sanitizeParametersEncoding toggle
    (cherry picked from commit 962c67434ddca10e05a456ecaea720aff364bb4c)
    
    # Conflicts:
    #	gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-common/src/main/java/io/gravitee/am/gateway/handler/common/vertx/utils/UriBuilderRequest.java
    #	gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-core/src/main/java/io/gravitee/am/gateway/handler/root/resources/endpoint/mfa/MFAChallengeFailureHandler.java

---
+++ gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-common/src/main/java/io/gravitee/am/gateway/handler/common/utils/StaticEnvironmentProvider.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.am.gateway.handler.common.utils;
+
+import org.springframework.core.env.Environment;
+
+public class StaticEnvironmentProvider {
+
+    private static Environment env = null;
+    private static Boolean sanitizeParametersEncoding = null;
+
+    public static void setEnvironment(Environment environment) {
+        env = environment;
+        // reset cached values
+        sanitizeParametersEncoding = null;
+    }
+
+    public static boolean sanitizeParametersEncoding() {
+        if (sanitizeParametersEncoding != null) {
+            return sanitizeParametersEncoding;
+        }
+        sanitizeParametersEncoding = getEnvironmentProperty("legacy.openid.sanitizeParametersEncoding", boolean.class, true);
+        return sanitizeParametersEncoding;
+    }
+
+    private static <T> T getEnvironmentProperty(String property, Class<T> type, T defaultValue) {
+        return env != null ? env.getProperty(property, type, defaultValue) : defaultValue;
+    }
+}
