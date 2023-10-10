3b4e7fe927398676a5c20bd5699a71133c4defd1
eric <eric.leleu@graviteesource.com>
2023-10-10 15:08:11 +0200

    fix: manage migration of OrganizationUsers duplicates
    fixes AM-942

--- docs/upgrades/username_uniqueness/username_uniqueness.js
+++ docs/upgrades/username_uniqueness/username_uniqueness.js
@@ -30,8 +30,8 @@ const dryRun = true;
 Search duplicates and sort them by loginsCount (higests come first) & loggedAt date (earliest comes first)
 if never logged in, take the first profile created
 */
-function searchDuplicates() {
-    return db.users.aggregate([
+function searchDuplicates(collection) {
+    return db.getCollection(collection).aggregate([
         {
             "$group":{
                 "_id":{
@@ -52,7 +52,7 @@ function searchDuplicates() {
         },
         {
             "$lookup":{
-                "from":"users",
+                "from":collection,
                 "localField":"_id.source",
                 "foreignField":"source",
                 "as":"result"
@@ -121,15 +121,14 @@ function getIdpUsernameField(source) {
  *  =================
  */
 
-
 // create a report object to generate final summary
 const report = {
     dryRun: dryRun
 };
 
 // count duplicates;
-const duplicatesUsers = searchDuplicates();
-console.log("[INFO] Found Duplicates: " + duplicatesUsers.length);
+const duplicatesUsers = searchDuplicates("users");
+console.log("[INFO] Found EndUser Duplicates: " + duplicatesUsers.length);
 report['duplicates'] = duplicatesUsers.length;
 
 // group duplicates by username/source
@@ -217,5 +216,94 @@ Object.keys(duplicatesUsersGroupByUsernameAndSource).forEach(entry => {
     }
 });
 
-console.log("[INFO] End of migration, generating summary...", '', '' );
-console.log(JSON.stringify(report));
+console.log("[INFO] End of EndUsers migration, generating summary...", '', '' );
+
+
+// count duplicates;
+const duplicatesOrgUsers = searchDuplicates("organization_users");
+console.log("[INFO] Found Organization User Duplicates: " + duplicatesOrgUsers.length);
+
+const orgReport = {
+    dryRun: dryRun
+};
+orgReport['organizationDuplicates'] = duplicatesOrgUsers.length;
+
+// group duplicates by username/source
+const duplicatesOrgUsersGroupByUsernameAndSource = duplicatesOrgUsers.reduce((acc, user) => {
+    const userRef = user._id.username + '/' + user._id.source ;
+    if (!acc[userRef]) {
+        acc[userRef] = [];
+    }
+    acc[userRef].push(user)
+
+    return acc;
+}, {});
+
+
+console.log("[INFO] Process Organization User duplicates...");
+const orgUsersCollection = db.getCollection("organization_users");
+Object.keys(duplicatesOrgUsersGroupByUsernameAndSource).forEach(entry => {
+    console.log("[INFO] Entry: " + entry);
+    const reportEntry={};
+    orgReport[entry] = reportEntry;
+
+    const dupUsers = duplicatesOrgUsersGroupByUsernameAndSource[entry];
+
+    const referenceUser = dupUsers.shift();
+
+    if (referenceUser._id.source !== "gravitee" && referenceUser._id.source !== "cockpit") {
+        console.error("[ERROR] Unable to rename remaining users with username [" + referenceUser._id.username + "] IDP collection is not managed by AM");
+        reportEntry['error'] = "Idp collection [" + idpId +"] is not Cockpit or Gravitee";
+    } else {
+        console.log("[INFO] Keep: user_id=" + referenceUser.result._id);
+
+        reportEntry['referenceUser'] = {
+            username: referenceUser.result.username,
+            createdAt: referenceUser.result.createdAt,
+            loggedAt: referenceUser.result.loggedAt,
+            loginsCount: referenceUser.result.loginsCount,
+            userId: referenceUser.result._id,
+            idpName: referenceUser._id.source,
+            organizationId: referenceUser.result.referenceId
+        }
+        reportEntry['renamedUsers'] = [];
+
+        try {
+            // rename remaining users
+            for (i = 0; i < dupUsers.length; ++i) {
+                const entryToUpdate = dupUsers[i];
+                const updatedName = entryToUpdate._id.username + "_" + i + "_TO_RENAME_OR_DELETE";
+
+                const renamedEntityDesc = {
+                    username: entryToUpdate.result.username,
+                    createdAt: entryToUpdate.result.createdAt,
+                    loggedAt: entryToUpdate.result.loggedAt,
+                    loginsCount: entryToUpdate.result.loginsCount,
+                    userId: entryToUpdate.result._id,
+                    idpName: entryToUpdate._id.source,
+                    organizationId: entryToUpdate.result.referenceId,
+                    renameTo: updatedName
+                };
+
+                reportEntry['renamedUsers'].push(renamedEntityDesc);
+                console.log("[INFO] Renaming... " + JSON.stringify(renamedEntityDesc));
+
+                if (dryRun) {
+                    console.log("[INFO] Skip user update (DRY RUN) mode");
+                } else {
+                    orgUsersCollection.updateOne({"_id": entryToUpdate.result._id}, {"$set": {"username": updatedName}});
+                }
+            }
+
+        } catch (error) {
+            console.error("[ERROR] Unable to rename remaining users with username [" + referenceUser._id.username + "] due to : " + error);
+            reportEntry['error'] = error;
+        }
+    }
+});
+
+console.log("[INFO] End of OrganizationUsers migration, generating summary...", '', '' );
+
+
+console.log("EndUsers summary: "+JSON.stringify(report));
+console.log("OrganizationUsers summary: "+JSON.stringify(orgReport));
