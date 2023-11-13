5b76ca91f88e9955f177e750d1729b211b58349e
Jeoffrey Haeyaert <jeoffrey.haeyaert@gmail.com>
2023-11-13 09:23:54 +0100

    refactor: move to LicenseManager
    https://gravitee.atlassian.net/browse/ARCHI-278

--- gravitee-am-management-api/gravitee-am-management-api-rest/src/main/java/io/gravitee/am/management/handlers/management/api/resources/platform/LicenseResource.java
+++ gravitee-am-management-api/gravitee-am-management-api-rest/src/main/java/io/gravitee/am/management/handlers/management/api/resources/platform/LicenseResource.java
@@ -18,7 +18,8 @@ package io.gravitee.am.management.handlers.management.api.resources.platform;
 import io.gravitee.am.management.handlers.management.api.resources.AbstractResource;
 import io.gravitee.am.service.model.GraviteeLicense;
 import io.gravitee.common.http.MediaType;
-import io.gravitee.node.api.license.NodeLicenseService;
+import io.gravitee.node.api.license.License;
+import io.gravitee.node.api.license.LicenseManager;
 import io.reactivex.rxjava3.core.Single;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.ws.rs.Consumes;
@@ -35,17 +36,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class LicenseResource extends AbstractResource {
 
     @Autowired
-    private NodeLicenseService licenseService;
+    private LicenseManager licenseManager;
 
     @GET
     @Operation(summary = "Get current node License")
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
     public void get(@Suspended final AsyncResponse response) {
-        GraviteeLicense license = GraviteeLicense.builder()
-                .tier(licenseService.getTier())
-                .packs(licenseService.getPacks())
-                .features(licenseService.getFeatures())
+        final License platformLicense = licenseManager.getPlatformLicense();
+        GraviteeLicense license;
+        license = GraviteeLicense.builder()
+                .tier(platformLicense.getTier())
+                .packs(platformLicense.getPacks())
+                .features(platformLicense.getFeatures())
                 .build();
 
         Single.just(license)
