155359019f7f84315381ae10bdb09a51d4b3a0bd
eric <eric.leleu@graviteesource.com>
2023-11-28 18:16:06 +0100

    fix: keep the SAML protocol info into the session on callbak failure
    fixes AM-1072
    
    gravitee-io/issues#9393
    
    (cherry picked from commit 322d9507ad245e1d334f71bca3ee94a6653f87eb)

--- gravitee-am-common/src/main/java/io/gravitee/am/common/utils/ConstantKeys.java
+++ gravitee-am-common/src/main/java/io/gravitee/am/common/utils/ConstantKeys.java
@@ -228,6 +228,7 @@ public interface ConstantKeys {
         Claims.auth_time,
         Claims.updated_at
     );
+    
     String REGISTRATION_VERIFY_SUCCESS = "registration_verify_success";
 
     String POLICY_CHAIN_ERROR_KEY_MFA_CHALLENGE_ERROR = "GATEWAY_POLICY_MFA_CHALLENGE_ERROR";
@@ -248,4 +249,7 @@ public interface ConstantKeys {
 
     String CONTINUE_CALLBACK_PROCESSING = "continueCallbackProcess";
 
+    String PROTOCOL_KEY = "protocol";
+    String PROTOCOL_VALUE_SAML_REDIRECT = "SAML/HTTP-Redirect";
+    String PROTOCOL_VALUE_SAML_POST = "SAML/HTTP-POST";
 }
