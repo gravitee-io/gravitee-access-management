290c767847da2d30eb525a456c1c7d5e48396a2a
eric <eric.leleu@graviteesource.com>
2023-12-08 15:35:32 +0100

    chore: add and update gatling simulation
      addtion of multi domain simulation
    
      change the agent injection to be in closed model
    
    (cherry picked from commit df8d6b14061a4fd853d638f31bea272609f3735a)

--- gravitee-am-performance/src/test/scala/io/gravitee/am/performance/utils/GatewayCalls.scala
+++ gravitee-am-performance/src/test/scala/io/gravitee/am/performance/utils/GatewayCalls.scala
@@ -91,6 +91,19 @@ object GatewayCalls {
       .check(jsonPath("$.access_token").saveAs("access_token"))
   }
 
+  def requestAccessTokenWithUserCredentialsOnDomain = {
+    http("Ask Token")
+      .post(GATEWAY_BASE_URL + "/${currentDomain}/oauth/token")
+      .basicAuth(APP_NAME, APP_NAME)
+      .formParam("grant_type", "password")
+      .formParam("redirect_uri", s"https://callback-${APP_NAME}")
+      .formParam("client_id", APP_NAME)
+      .formParam("username", "${username}")
+      .formParam("password", "${password}")
+      .check(status.is(200))
+      .check(jsonPath("$.access_token").saveAs("access_token"))
+  }
+
   def logout = {
     http("logout")
       .get(GATEWAY_BASE_URL + s"/${DOMAIN_NAME}/logout")
@@ -100,6 +113,15 @@ object GatewayCalls {
   def introspectToken = {
     http("Introspect Access Token")
       .post(GATEWAY_BASE_URL + s"/${DOMAIN_NAME}/oauth/introspect")
+      .basicAuth(APP_NAME, APP_NAME)
+      .formParam("token", "${access_token}")
+      .check(status.is(200))
+      .check(jsonPath("$.active").is("true"))
+  }
+
+  def introspectTokenOnDomain = {
+    http("Introspect Access Token")
+      .post(GATEWAY_BASE_URL + "/${currentDomain}/oauth/introspect")
       .basicAuth(APP_NAME, APP_NAME)
       .formParam("token", "${access_token}")
       .check(status.is(200))
