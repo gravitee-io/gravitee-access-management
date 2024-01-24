a9e99a0e32cd885f60dbe7ec9b06af262b490076
eric <eric.leleu@graviteesource.com>
2024-01-24 15:41:48 +0100

    fix: evaluate PAR parameters from the AuthenticationFlowContext
    fixes AM-2134
    
    gravitee-io/issues#9478
    
    (cherry picked from commit 231b02a9010c7f03dfb27a2020194b626ecf3136)
    
    # Conflicts:
    #	gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-core/src/main/java/io/gravitee/am/gateway/handler/root/resources/endpoint/ParamUtils.java
    #	gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-core/src/test/java/io/gravitee/am/gateway/handler/root/resources/endpoint/ParamUtilsTest.java

--- gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-core/src/test/java/io/gravitee/am/gateway/handler/root/resources/endpoint/ParamUtilsTest.java
+++ gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-core/src/test/java/io/gravitee/am/gateway/handler/root/resources/endpoint/ParamUtilsTest.java
@@ -19,10 +19,24 @@ package io.gravitee.am.gateway.handler.root.resources.endpoint;
 import static io.gravitee.am.gateway.handler.root.resources.endpoint.ParamUtils.redirectMatches;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import io.vertx.rxjava3.core.http.HttpServerRequest;
+import io.vertx.rxjava3.ext.web.RoutingContext;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import java.util.stream.Stream;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.PlainJWT;
+import io.gravitee.am.common.utils.ConstantKeys;
+import io.gravitee.am.model.AuthenticationFlowContext;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.Map;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class ParamUtilsTest {
 
@@ -46,5 +60,108 @@ public class ParamUtilsTest {
                 Arguments.of("https://test.com/people", "https://test.com", true, false),
                 Arguments.of("https://test.com/people?v=123", "https://test.com", true, false)
         );
+    }
+
+    @Test
+    public void redirectMatch_url_without_path_success() {
+        final String requestRedirectUri = "https://test.com?id=10";
+        final String registeredRedirectUri1 = "https://test.com/*";
+
+        assertTrue(ParamUtils.redirectMatches(requestRedirectUri, registeredRedirectUri1, false));
+
+        final String registeredRedirectUri2 = "https://test.com*";
+        assertTrue(ParamUtils.redirectMatches(requestRedirectUri, registeredRedirectUri2, false));
+    }
+
+    @Test
+    public void redirectMatch_url_path_with_param_success() {
+        final String requestRedirectUri = "https://test.com/department?id=10";
+        final String registeredRedirectUri = "https://test.com/department*";
+
+        boolean matched = ParamUtils.redirectMatches(requestRedirectUri, registeredRedirectUri, false);
+        assertTrue( matched);
+    }
+
+    @Test
+    public void redirectMatch_url_without_path_fail() {
+        final String requestRedirectUri = "https://test.com?id=10";
+        final String registeredRedirectUri = "https://test.com/department*";
+
+        boolean matched = ParamUtils.redirectMatches(requestRedirectUri, registeredRedirectUri, false);
+        assertFalse( matched);
+    }
+
+    @Test
+    public void redirectMatch_url_with_param_strict_fail() {
+        final String requestRedirectUri = "https://test.com?id=10";
+        final String registeredRedirectUri = "https://test.com";
+
+        assertFalse(ParamUtils.redirectMatches(requestRedirectUri, registeredRedirectUri, true));
+
+        final String registeredRedirectUriWildCard = "https://test.com*";
+        assertFalse(ParamUtils.redirectMatches(requestRedirectUri, registeredRedirectUriWildCard, true));
+
+        final String requestRedirectUriParam = "https://test.com/people";
+        assertFalse(ParamUtils.redirectMatches(requestRedirectUriParam, registeredRedirectUri, true));
+
+        final String requestRedirectUriParamQuery = "https://test.com/people?v=123";
+        assertFalse(ParamUtils.redirectMatches(requestRedirectUriParamQuery, registeredRedirectUri, true));
+    }
+
+    @Test
+    public void should_getOAuthParameter_from_requestParam() {
+        RoutingContext ctx = Mockito.mock(RoutingContext.class);
+        HttpServerRequest request = mock(HttpServerRequest.class);
+        when(ctx.request()).thenReturn(request);
+
+        final String paramName = "scope";
+        final String paramValue = "scopeFromRequest";
+        when(request.getParam(paramName)).thenReturn(paramValue);
+
+        assertEquals(paramValue, ParamUtils.getOAuthParameter(ctx, paramName));
+    }
+
+    @Test
+    public void should_getOAuthParameter_from_requestObject() {
+        RoutingContext ctx = Mockito.mock(RoutingContext.class);
+        HttpServerRequest request = mock(HttpServerRequest.class);
+        when(ctx.request()).thenReturn(request);
+
+        final String paramName = "scope";
+        final String paramValue = "scopeFromJwtObject";
+        when(ctx.get(ConstantKeys.REQUEST_OBJECT_KEY)).thenReturn(new PlainJWT(new JWTClaimsSet.Builder().claim(paramName, paramValue).build()));
+
+        assertEquals(paramValue, ParamUtils.getOAuthParameter(ctx, paramName));
+    }
+
+    @Test
+    public void should_getOAuthParameter_from_authenticationFlowContext() {
+        RoutingContext ctx = Mockito.mock(RoutingContext.class);
+        HttpServerRequest request = mock(HttpServerRequest.class);
+        when(ctx.request()).thenReturn(request);
+
+        final String paramName = "scope";
+        final String paramValue = "scopeFromAuthFlowCtx";
+        AuthenticationFlowContext flowCtx = new AuthenticationFlowContext();
+        flowCtx.setData(Map.of(ConstantKeys.REQUEST_PARAMETERS_KEY, Map.of(paramName, paramValue)));
+        when(ctx.get(ConstantKeys.AUTH_FLOW_CONTEXT_KEY)).thenReturn(flowCtx);
+
+        assertEquals(paramValue, ParamUtils.getOAuthParameter(ctx, paramName));
+    }
+
+    @Test
+    public void should_getOAuthParameter_from_requestParam_fallback() {
+        RoutingContext ctx = Mockito.mock(RoutingContext.class);
+        HttpServerRequest request = mock(HttpServerRequest.class);
+        when(ctx.request()).thenReturn(request);
+
+        final String paramName = "scope";
+        final String paramValue = "scopeFromRequest";
+        when(request.getParam(paramName)).thenReturn(paramValue);
+        // set the AuthFlow without RequestObject key to test branching condition and fallback
+        AuthenticationFlowContext flowCtx = new AuthenticationFlowContext();
+        when(ctx.get(ConstantKeys.AUTH_FLOW_CONTEXT_KEY)).thenReturn(flowCtx);
+
+        assertEquals(paramValue, ParamUtils.getOAuthParameter(ctx, paramName));
     }
 }
