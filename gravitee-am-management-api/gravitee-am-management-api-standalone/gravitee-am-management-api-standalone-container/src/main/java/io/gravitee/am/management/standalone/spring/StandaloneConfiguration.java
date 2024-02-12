b98a202d9cdda81cbfa0bbf26818508c07e8a4c6
eric <eric.leleu@graviteesource.com>
2024-02-12 20:30:53 +0100

    chore: update spring-data-r2dbc
    related to AM-2153

--- gravitee-am-management-api/gravitee-am-management-api-standalone/gravitee-am-management-api-standalone-container/src/main/java/io/gravitee/am/management/standalone/spring/StandaloneConfiguration.java
+++ gravitee-am-management-api/gravitee-am-management-api-standalone/gravitee-am-management-api-standalone-container/src/main/java/io/gravitee/am/management/standalone/spring/StandaloneConfiguration.java
@@ -95,7 +95,6 @@ public class StandaloneConfiguration {
     }
 
     @Bean
-    @Qualifier("managementObjectMapper")
     public ObjectMapper objectMapper() {
         return new ObjectMapper();
     }
