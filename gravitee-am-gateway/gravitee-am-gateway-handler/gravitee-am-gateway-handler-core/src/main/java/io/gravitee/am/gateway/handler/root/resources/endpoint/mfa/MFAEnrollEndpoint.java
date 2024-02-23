97e9f97d757b16369f2c310d7decb95d0e123a86
Lukasz Gawel <lukasz.gawel@graviteesource.com>
2024-02-23 14:56:26 +0100

    AM-1138: Fixes

--- gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-core/src/main/java/io/gravitee/am/gateway/handler/root/resources/endpoint/mfa/MFAEnrollEndpoint.java
+++ gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-core/src/main/java/io/gravitee/am/gateway/handler/root/resources/endpoint/mfa/MFAEnrollEndpoint.java
@@ -174,8 +174,8 @@ public class MFAEnrollEndpoint extends AbstractEndpoint implements Handler<Routi
         if (alternativeFactorId != null && !alternativeFactorId.isEmpty()) {
             // check if this alternative factor still exists
             Optional<Factor> optionalFactor = factors.stream()
-                    .filter(factor -> mfaContext.evaluateFactorRuleByFactorId(factor.getId()))
                     .filter(factor -> alternativeFactorId.equals(factor.getId()))
+                    .filter(factor -> mfaContext.evaluateFactorRuleByFactorId(factor.getId()))
                     .findFirst();
             if (optionalFactor.isPresent()) {
                 return List.of(optionalFactor.get());
