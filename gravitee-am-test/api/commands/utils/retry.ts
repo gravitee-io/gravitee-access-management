6b1b81fc77ea677d1d40f9fae9ac1b4600172a0c
Lukasz Gawel <lukasz.gawel@graviteesource.com>
2024-03-12 05:07:34 +0100

    AM-2673 - tweaked jest util methods
    (cherry picked from commit ae321a8d36bc983e6b845f7f1be84f6934e47b8b)

---
+++ gravitee-am-test/api/commands/utils/retry.ts
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { waitFor } from '@management-commands/domain-management-commands';
+
+export async function withRetry(operation, retries = 50, delay = 100) {
+  let success = false;
+  while (!(success || retries === 0)) {
+    try {
+      const result = await operation();
+      success = true;
+      return result;
+    } catch (e) {
+      retries = retries - 1;
+      if (retries === 0) {
+        throw e;
+      }
+      await waitFor(delay);
+    }
+  }
+}
