c55478b3287d6d47e77b76b8145708d650926186
eric <eric.leleu@graviteesource.com>
2024-03-25 13:38:34 +0100

    fix: make webAuthnCredentialInternalId available into the session
    fixes AM-2872
    
    gravitee-io/issues#9620
    
    (cherry picked from commit 3482364176afc5092746502320211d298e384ccb)
    
    # Conflicts:
    #	gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-core/src/main/java/io/gravitee/am/gateway/handler/root/resources/handler/webauthn/WebAuthnHandler.java
    #	gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-core/src/main/java/io/gravitee/am/gateway/handler/root/resources/handler/webauthn/WebAuthnLoginHandler.java
    #	gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-core/src/main/java/io/gravitee/am/gateway/handler/root/resources/handler/webauthn/WebAuthnRegisterHandler.java
    #	gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-core/src/main/java/io/gravitee/am/gateway/handler/root/resources/handler/webauthn/WebAuthnResponseHandler.java
    #	gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-core/src/test/java/io/gravitee/am/gateway/handler/root/resources/endpoint/mfa/MFAChallengeEndpointTest.java
    #	gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-oidc/src/main/java/io/gravitee/am/gateway/handler/oauth2/resources/endpoint/authorization/AuthorizationEndpoint.java
    #	gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-oidc/src/main/java/io/gravitee/am/gateway/handler/oauth2/resources/handler/authorization/AuthorizationRequestFailureHandler.java

--- gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-core/src/main/java/io/gravitee/am/gateway/handler/root/resources/handler/webauthn/WebAuthnLoginHandler.java
+++ gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-core/src/main/java/io/gravitee/am/gateway/handler/root/resources/handler/webauthn/WebAuthnLoginHandler.java
@@ -203,6 +203,7 @@ public class WebAuthnLoginHandler extends WebAuthnHandler {
                             // the user has upgraded from unauthenticated to authenticated
                             // session should be upgraded as recommended by owasp
                             session.regenerateId();
+<<<<<<< HEAD
                             // keep the webauthn action into session to be able to do distinction
                             // between login or registration action
                             session.put(PASSWORDLESS_AUTH_ACTION_KEY, PASSWORDLESS_AUTH_ACTION_VALUE_LOGIN);
@@ -212,6 +213,24 @@ public class WebAuthnLoginHandler extends WebAuthnHandler {
                         },
                         ctx::fail
                 );
+=======
+                            final io.gravitee.am.model.User authenticatedUser = ((io.gravitee.am.gateway.handler.common.vertx.web.auth.user.User) user).getUser();
+                            // update the credential
+                            updateCredential(authenticationContext, credentialId, authenticatedUser.getId(), credentialHandler -> {
+                                if (credentialHandler.failed()) {
+                                    logger.error("An error has occurred while authenticating user {}", username, credentialHandler.cause());
+                                    ctx.fail(401);
+                                    return;
+                                }
+                                manageFido2FactorEnrollment(ctx, client, credentialHandler.result(), authenticatedUser);
+                            });
+                        });
+                    } else {
+                        logger.error("Unexpected exception", authenticate.cause());
+                        ctx.fail(authenticate.cause());
+                    }
+                });
+>>>>>>> 3482364176 (fix: make webAuthnCredentialInternalId available into the session)
     }
 
     protected Completable checkAuthenticatorConformity(String credentialId, String username) {
