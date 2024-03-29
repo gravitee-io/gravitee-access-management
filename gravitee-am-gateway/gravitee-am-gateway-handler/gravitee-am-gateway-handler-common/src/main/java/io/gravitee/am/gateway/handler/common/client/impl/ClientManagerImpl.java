2013c33240df3c43339b0fb3ded53b1b5858b7da
Rafal Podles <rpo@spyro-soft.com>
2024-03-29 16:44:01 +0100

    fix: remove disabled applications
    (cherry picked from commit 126aafc2ee57771a00b247fc324f453ca7d815ff)

--- gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-common/src/main/java/io/gravitee/am/gateway/handler/common/client/impl/ClientManagerImpl.java
+++ gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-common/src/main/java/io/gravitee/am/gateway/handler/common/client/impl/ClientManagerImpl.java
@@ -1,12 +1,12 @@
 /**
  * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -69,6 +69,7 @@ public class ClientManagerImpl extends AbstractService implements ClientManager,
         Flowable<Application> applicationsSource = domain.isMaster() ? applicationRepository.findAll() : applicationRepository.findByDomain(domain.getId());
         applicationsSource
                 .map(Application::toClient)
+                .filter(Client::isEnabled)
                 .subscribeOn(Schedulers.io())
                 .subscribe(
                         client -> {
@@ -158,8 +159,12 @@ public class ClientManagerImpl extends AbstractService implements ClientManager,
                 .subscribeOn(Schedulers.io())
                 .subscribe(
                         client -> {
-                            clients.put(client.getId(), client);
-                            logger.info("Application {} loaded for domain {}", applicationId, domain.getName());
+                            if (!client.isEnabled()) {
+                                removeClient(applicationId);
+                            } else {
+                                clients.put(client.getId(), client);
+                                logger.info("Application {} loaded for domain {}", applicationId, domain.getName());
+                            }
                         },
                         error -> logger.error("An error has occurred when loading application {} for domain {}", applicationId, domain.getName(), error),
                         () -> logger.error("No application found with id {}", applicationId));
