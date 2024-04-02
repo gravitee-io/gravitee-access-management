8b6a02cf2f0f0a2e5cfdbb4fd1d7526f562eb36b
Rafal Podles <rpo@spyro-soft.com>
2024-04-02 11:05:29 +0200

    added tests for fix
    (cherry picked from commit 9b0110c16971aa804a644004a6e449595ed35d11)

---
+++ gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-common/src/test/java/io/gravitee/am/gateway/handler/common/client/ClientManagerTest.java
@@ -0,0 +1,103 @@
+package io.gravitee.am.gateway.handler.common.client;
+
+import io.gravitee.am.common.event.ApplicationEvent;
+import io.gravitee.am.gateway.handler.common.client.impl.ClientManagerImpl;
+import io.gravitee.am.model.Application;
+import io.gravitee.am.model.Domain;
+import io.gravitee.am.model.ReferenceType;
+import io.gravitee.am.model.common.event.Payload;
+import io.gravitee.am.model.oidc.Client;
+import io.gravitee.am.monitoring.provider.GatewayMetricProvider;
+import io.gravitee.am.repository.management.api.ApplicationRepository;
+import io.gravitee.common.event.Event;
+import io.reactivex.rxjava3.core.Maybe;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+import static org.mockito.Mockito.when;
+
+/**
+ * @author Rafal Podles (rafal.podles at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class ClientManagerTest {
+    @InjectMocks
+    private ClientManagerImpl clientManager = new ClientManagerImpl();
+
+    @Mock
+    private Domain domain;
+    @Mock
+    private Payload payload;
+    @Mock
+    private Event<ApplicationEvent, Payload> event;
+    @Mock
+    private GatewayMetricProvider gatewayMetricProvider;
+    @Mock
+    private ApplicationRepository applicationRepository;
+
+    @Before
+    public void setUp() {
+        when(domain.getId()).thenReturn("domain_id");
+        when(domain.getName()).thenReturn("domain_name");
+
+        when(payload.getReferenceType()).thenReturn(ReferenceType.DOMAIN);
+        when(payload.getReferenceId()).thenReturn("domain_id");
+
+        when(event.type()).thenReturn(ApplicationEvent.UPDATE);
+        when(event.content()).thenReturn(payload);
+
+        Client c1 = new Client();
+        c1.setId("client1");
+        c1.setEnabled(true);
+        clientManager.deploy(c1);
+        Client c2 = new Client();
+        c2.setId("client2");
+        c2.setEnabled(true);
+        clientManager.deploy(c2);
+    }
+
+    @Test
+    public void shouldNotDeployDisabledClient() {
+        Application application = new Application();
+        application.setId("client_id");
+        application.setEnabled(false);
+        when(applicationRepository.findById("client_id")).thenReturn(Maybe.just(application));
+        when(payload.getId()).thenReturn("client_id");
+        clientManager.onEvent(event);
+        await().atMost(5, TimeUnit.SECONDS)
+                .untilAsserted(() -> assertThat(clientManager.entities().size()).isEqualTo(2));
+    }
+
+    @Test
+    public void shouldDeployClientWhenEnable() {
+        Application application = new Application();
+        application.setId("client_id");
+        application.setEnabled(true);
+        when(applicationRepository.findById("client_id")).thenReturn(Maybe.just(application));
+        when(payload.getId()).thenReturn("client_id");
+        clientManager.onEvent(event);
+        await().atMost(5, TimeUnit.SECONDS)
+                .untilAsserted(() -> assertThat(clientManager.entities().size()).isEqualTo(3));
+    }
+
+    @Test
+    public void shouldRemoveDisabledClient() {
+        Application application = new Application();
+        application.setId("client2");
+        application.setEnabled(false);
+        when(applicationRepository.findById("client2")).thenReturn(Maybe.just(application));
+        when(payload.getId()).thenReturn("client2");
+        clientManager.onEvent(event);
+        await().atMost(5, TimeUnit.SECONDS)
+                .untilAsserted(() -> assertThat(clientManager.entities().size()).isEqualTo(1));
+    }
+}
