ca0cbb5b97d9e5dd46a7a80d3aac8880af1744f1
Titouan Compiegne <titouancompiegne@grvtfr1033-1.home>
2024-04-02 15:56:51 +0200

    feat(saml2): add response binding option to service provider
    fixes AM-2906
    
    gravitee-io/issues#9648
    
    (cherry picked from commit cadf59a)

--- gravitee-am-repository/gravitee-am-repository-mongodb/src/main/java/io/gravitee/am/repository/mongodb/management/MongoApplicationRepository.java
+++ gravitee-am-repository/gravitee-am-repository-mongodb/src/main/java/io/gravitee/am/repository/mongodb/management/MongoApplicationRepository.java
@@ -451,6 +451,7 @@ public class MongoApplicationRepository extends AbstractManagementMongoRepositor
         applicationSAMLSettingsMongo.setCertificate(other.getCertificate());
         applicationSAMLSettingsMongo.setWantResponseSigned(other.isWantResponseSigned());
         applicationSAMLSettingsMongo.setWantAssertionsSigned(other.isWantAssertionsSigned());
+        applicationSAMLSettingsMongo.setResponseBinding(other.getResponseBinding());
         return applicationSAMLSettingsMongo;
     }
 
@@ -465,6 +466,7 @@ public class MongoApplicationRepository extends AbstractManagementMongoRepositor
         applicationSAMLSettings.setCertificate(other.getCertificate());
         applicationSAMLSettings.setWantResponseSigned(other.isWantResponseSigned());
         applicationSAMLSettings.setWantAssertionsSigned(other.isWantAssertionsSigned());
+        applicationSAMLSettings.setResponseBinding(other.getResponseBinding());
         return applicationSAMLSettings;
     }
 
