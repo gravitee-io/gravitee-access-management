8bc910c7054d9eaa877d895be1cfd2a7d19c8e42
Lukasz Gawel <lukasz.gawel@graviteesource.com>
2024-04-23 15:37:00 +0200

    fix: Limit on webauthn credentials sent in response fixes: AM-3066
    (cherry picked from commit f8be7177261ba80aa843303d3f2e3eba21d8c800)
    
    # Conflicts:
    #	gravitee-am-repository/gravitee-am-repository-mongodb/src/main/java/io/gravitee/am/repository/mongodb/management/MongoCredentialRepository.java

--- gravitee-am-repository/gravitee-am-repository-mongodb/src/main/java/io/gravitee/am/repository/mongodb/management/MongoCredentialRepository.java
+++ gravitee-am-repository/gravitee-am-repository-mongodb/src/main/java/io/gravitee/am/repository/mongodb/management/MongoCredentialRepository.java
@@ -42,6 +42,8 @@ public class MongoCredentialRepository extends AbstractManagementMongoRepository
     private static final String FIELD_USERNAME = "username";
     private static final String FIELD_CREDENTIAL_ID = "credentialId";
     private static final String FIELD_AAGUID = "aaguid";
+    private static final String FIELD_CREATED_AT = "createdAt";
+
     private MongoCollection<CredentialMongo> credentialsCollection;
 
     @PostConstruct
@@ -78,6 +80,20 @@ public class MongoCredentialRepository extends AbstractManagementMongoRepository
                                 eq(FIELD_USERNAME, username)
                         )
                 ))
+                .map(this::convert);
+    }
+
+    @Override
+    public Flowable<Credential> findByUsername(ReferenceType referenceType, String referenceId, String username, int limit) {
+        return Flowable.fromPublisher(
+                        credentialsCollection.find(
+                                and(
+                                        eq(FIELD_REFERENCE_TYPE, referenceType.name()),
+                                        eq(FIELD_REFERENCE_ID, referenceId),
+                                        eq(FIELD_USERNAME, username)
+                                )).sort(
+                                    new Document(FIELD_CREATED_AT, -1))
+                                .limit(limit))
                 .map(this::convert);
     }
 
