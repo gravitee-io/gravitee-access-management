1b976ea60407c718c01ec5af2a3456341c1a96f4
mstasgravitee <michal.stas@graviteesource.com>
2024-04-24 12:59:28 +0200

    chore: release pipeline for a new version and an alpha version AM-1064

---
+++ release/steps/am-release-alpha-pipeline.mjs
@@ -0,0 +1,21 @@
+import { AbstractStepRelease } from './abstract-step-release.mjs';
+
+export class ReleaseAlphaVersion extends AbstractStepRelease {
+    buildCommand(program) {
+        super.buildCommand(program, 'release-alpha-version');
+        this.command
+                .description('trigger pipeline release alpha version on CircleCI')
+                .usage('npm run am-release -- --branch="4.3.x" --gio-version="4.3.2" --dry-run release-alpha-version');
+    }
+
+    computePipelineParameters(options) {
+        return {
+            dry_run: options.dryRun,
+            graviteeio_version: options.gioVersion,
+            gio_action: 'release-alpha-version',
+            rc_requested: true,
+            tag_latest: false,
+            tag_latest_support: false
+        };
+    }
+}
