a6140372061ba63ccfd87e9e61646b707de94728
Lukasz Gawel <lukasz.gawel@graviteesource.com>
2024-05-06 08:36:18 +0200

    fix: Added crucial MFA event logs (2) fixes AM-2887
    (cherry picked from commit 376491f404db0e989e434b335bdada8930c9fe43)
    
    # Conflicts:
    #	gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-core/src/main/java/io/gravitee/am/gateway/handler/root/resources/endpoint/mfa/MFAChallengeEndpoint.java

--- gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-core/src/main/java/io/gravitee/am/gateway/handler/root/resources/endpoint/mfa/MFAChallengeEndpoint.java
+++ gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-core/src/main/java/io/gravitee/am/gateway/handler/root/resources/endpoint/mfa/MFAChallengeEndpoint.java
@@ -362,6 +362,10 @@ public class MFAChallengeEndpoint extends MFAEndpoint {
 
 
             updateStrongAuthStatus(routingContext);
+<<<<<<< HEAD
+=======
+            logger.debug("User {} strongly authenticated", endUser.getId());
+>>>>>>> 376491f404 (fix: Added crucial MFA event logs (2))
             // set the credentialId in session
             routingContext.session().put(ConstantKeys.WEBAUTHN_CREDENTIAL_ID_CONTEXT_KEY, credentialId);
             final Credential credential = ch.result();
@@ -417,6 +421,10 @@ public class MFAChallengeEndpoint extends MFAEndpoint {
                             }
                         },
                         error -> {
+<<<<<<< HEAD
+=======
+                            logger.debug("Challenge failed for user {}", factorContext.getUser().getId());
+>>>>>>> 376491f404 (fix: Added crucial MFA event logs (2))
                             final EnrolledFactor enrolledFactor = (EnrolledFactor) factorContext.getData().get(FactorContext.KEY_ENROLLED_FACTOR);
                             verifyAttemptService.incrementAttempt(factorContext.getUser().getId(), enrolledFactor.getFactorId(),
                                     factorContext.getClient(), domain, verifyAttempt).subscribe(
