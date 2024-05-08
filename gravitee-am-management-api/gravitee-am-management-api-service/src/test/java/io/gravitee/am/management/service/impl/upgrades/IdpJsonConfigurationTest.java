e14d6d9d25b653af27988841fe6b239d14ffc361
Lukasz Gawel <lukasz.gawel@graviteesource.com>
2024-05-08 15:59:06 +0200

    feature: AM-3002 - all iteration rounds should be reset for non-BCrypt algortihms

---
+++ gravitee-am-management-api/gravitee-am-management-api-service/src/test/java/io/gravitee/am/management/service/impl/upgrades/IdpJsonConfigurationTest.java
@@ -0,0 +1,56 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.am.management.service.impl.upgrades;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.gravitee.am.management.service.impl.upgrades.NonBCryptIterationsRoundsUpgrader.IdpJsonConfiguration;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class IdpJsonConfigurationTest {
+
+    @Test
+    public void shouldProcessIdpCfg() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        IdpJsonConfiguration cfg = new IdpJsonConfiguration();
+        cfg.load(objectMapper, "{\"value\": \"aaa\", \"other\": \"value\"}");
+
+        Assertions.assertTrue(cfg.hasValue("value", "aaa"));
+        Assertions.assertTrue(cfg.hasValue("other", "value"));
+        Assertions.assertFalse(cfg.hasValue("value", "bbb"));
+        Assertions.assertFalse(cfg.hasValue("aaa", "value"));
+
+        cfg.removeProperty("aaa");
+
+        Assertions.assertTrue(cfg.hasValue("value", "aaa"));
+        Assertions.assertTrue(cfg.hasValue("other", "value"));
+        Assertions.assertFalse(cfg.hasValue("value", "bbb"));
+        Assertions.assertFalse(cfg.hasValue("aaa", "value"));
+
+        cfg.removeProperty("value");
+
+        Assertions.assertFalse(cfg.hasValue("value", "aaa"));
+        Assertions.assertTrue(cfg.hasValue("other", "value"));
+
+        String json = cfg.toJson(objectMapper);
+        Assertions.assertEquals("{\"other\":\"value\"}", json);
+    }
+
+
+}
