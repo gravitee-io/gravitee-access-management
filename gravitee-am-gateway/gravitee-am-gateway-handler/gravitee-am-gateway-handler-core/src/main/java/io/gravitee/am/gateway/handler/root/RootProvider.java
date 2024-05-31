cb3d3dfb41f7f947f7a42f9f58f32026d0dcc8e1
eric <eric.leleu@graviteesource.com>
2024-05-31 15:20:11 +0200

    fix: execute session handler on forgot password
     this is useful to give access to the IP & UserAgent if the consent is given automatically through the gravitee config
    
     to be align with the previous behaviour, cookie session is destroyed at the end of the forgot password request
    
    fixes AM-3139
    
    gravitee-io/issues#9724
    
    (cherry picked from commit e9af540b17d543575a506eb9a3376c6283668057)

--- gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-core/src/main/java/io/gravitee/am/gateway/handler/root/RootProvider.java
+++ gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-core/src/main/java/io/gravitee/am/gateway/handler/root/RootProvider.java
@@ -606,6 +606,9 @@ public class RootProvider extends AbstractProtocolProvider {
                 .handler(botDetectionHandler)
                 .handler(forgotPasswordAccessHandler)
                 .handler(new ForgotPasswordSubmissionEndpoint(userService, domain));
+
+        router.route(PATH_FORGOT_PASSWORD).failureHandler(new ErrorHandler(PATH_ERROR, true));
+
         rootRouter.route(HttpMethod.GET, PATH_RESET_PASSWORD)
                 .handler(new ResetPasswordRequestParseHandler(userService))
                 .handler(clientRequestParseHandlerOptional)
@@ -699,6 +702,10 @@ public class RootProvider extends AbstractProtocolProvider {
         // Reset password endpoint
         router
                 .route(PATH_RESET_PASSWORD)
+                .handler(sessionHandler);
+
+        router
+                .route(PATH_FORGOT_PASSWORD)
                 .handler(sessionHandler);
 
         // WebAuthn endpoint
@@ -862,7 +869,6 @@ public class RootProvider extends AbstractProtocolProvider {
 
     private void errorHandler(Router router) {
         Handler<RoutingContext> errorHandler = new ErrorHandler(PATH_ERROR);
-        router.route(PATH_FORGOT_PASSWORD).failureHandler(errorHandler);
         router.route(PATH_LOGOUT).failureHandler(errorHandler);
         router.route(PATH_LOGOUT_CALLBACK).failureHandler(errorHandler);
         router.route(PATH_LOGIN).failureHandler(errorHandler);
