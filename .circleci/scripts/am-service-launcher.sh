e75d25a132bcdce57fb87b203cb0c1f5985d5ad0
eric <eric.leleu@graviteesource.com>
2024-06-21 17:31:56 +0200

    chore: rework the way services are started during IT phase to avoid running test on hanged instance
    (cherry picked from commit f2114e183c44e064a4f0066e30baae212547ff5a)

---
+++ .circleci/scripts/am-service-launcher.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+SERVICE_HOME_DIR=$1
+
+PID_FILE="/tmp/workspace/graviteeio.pid"
+SERVICE_PORT=8093
+
+if [ ${SERVICE_HOME_DIR} == "gravitee-am-gateway-standalone*" ]; then
+  SERVICE_PORT=8092
+fi
+
+${SERVICE_HOME_DIR}/bin/gravitee -d -p={$PID_FILE}
+
+timeout 240s bash -c "until nc -z localhost ${SERVICE_PORT}; do sleep 2; done"
+
+retVal=$?
+if [ $retVal -ne 0 ]; then
+  kill `cat $PID_FILE`
+  sleep 5
+  ${SERVICE_HOME_DIR}/bin/gravitee -d -p={$PID_FILE}
+fi
+
+timeout 240s bash -c "until nc -z localhost ${SERVICE_PORT}; do sleep 2; done"
+retVal=$?
+if [ $retVal -ne 0 ]; then
+  kill `cat $PID_FILE`
+  echo "STOP CI: AM Service doesn't start from ${SERVICE_HOME_DIR}"
+  exit 1
+fi
