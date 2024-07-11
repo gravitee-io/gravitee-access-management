afa457dae0cee7504b807fb46492406d423e2a86
Lukasz Gawel <lukasz.gawel@graviteesource.com>
2024-07-11 17:59:07 +0200

    feat: AM-3270 MTLS for LDAP

--- gravitee-am-ui/src/app/domain/settings/providers/provider/provider.form.enricher.ts
+++ gravitee-am-ui/src/app/domain/settings/providers/provider/provider.form.enricher.ts
@@ -13,12 +13,19 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-const OIDC_JSON_FORM_ID: string = 'urn:jsonschema:io:gravitee:am:identityprovider:oauth2:OAuth2GenericIdentityProvider';
-const OIDC_JSON_FORM_VERSION: string = '05-2024';
+const OIDC_JSON_FORM = {
+  id: 'urn:jsonschema:io:gravitee:am:identityprovider:oauth2:OAuth2GenericIdentityProvider',
+  version: '05-2024',
+};
 
-export function enrichOIDCFormWithCerts(schema: OIDCFormSchema, certs: Certificate[]): OIDCFormSchema {
+const LDAP_JSON_FORM = {
+  id: 'urn:jsonschema:com:graviteesource:am:identityprovider:ldap:LdapIdentityProviderConfiguration',
+  version: '07-2024',
+};
+
+export function enrichFormWithCerts(schema: FormSchema, certs: Certificate[]): FormSchema {
   const mTlsCerts = certs?.filter((c) => c?.usage.includes('mtls'));
-  if (mTlsCerts?.length > 0 && schema.id === OIDC_JSON_FORM_ID && schema?.version == OIDC_JSON_FORM_VERSION) {
+  if (mTlsCerts?.length > 0 && supportsMTls(schema)) {
     const updatedSchema = { ...schema };
     updatedSchema.properties.clientAuthenticationCertificate.enum = mTlsCerts.map((c) => c.id);
     updatedSchema.properties.clientAuthenticationCertificate.enumNames = mTlsCerts.map((c) => c.name);
@@ -29,7 +36,14 @@ export function enrichOIDCFormWithCerts(schema: OIDCFormSchema, certs: Certifica
   return schema;
 }
 
-interface OIDCFormSchema {
+function supportsMTls(schema: FormSchema): boolean {
+  return (
+    (schema.id === OIDC_JSON_FORM.id && schema?.version == OIDC_JSON_FORM.version) ||
+    (schema.id === LDAP_JSON_FORM.id && schema?.version == LDAP_JSON_FORM.version)
+  );
+}
+
+interface FormSchema {
   id: string;
   version: string;
   properties: {
