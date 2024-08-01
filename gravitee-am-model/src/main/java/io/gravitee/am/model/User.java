5939927393a3fe5f27e173dd4a179cdb2a95a1eb
Marcin Regulski <marcin.regulski@graviteesource.com>
2024-08-01 11:21:50 +0200

    fix mergify conflicts
    AM-3453

--- gravitee-am-model/src/main/java/io/gravitee/am/model/User.java
+++ gravitee-am-model/src/main/java/io/gravitee/am/model/User.java
@@ -407,8 +407,6 @@ public class User implements IUser {
         return null;
     }
 
-<<<<<<< HEAD
-=======
     public void setAdditionalInformation(Map<String, Object> additionalInformation) {
         if (additionalInformation == null) {
             this.additionalInformation = null;
@@ -429,7 +427,6 @@ public class User implements IUser {
                 })
                 // Collectors.toMap() doesn't work if there's any null values, and we don't have a guarantee there aren't any
                 .collect(HashMap<String, Object>::new, (map, entry) -> map.put(entry.getKey(), entry.getValue()), HashMap::putAll);
-        ;
     }
 
     public Date getLastPasswordReset() {
@@ -471,7 +468,6 @@ public class User implements IUser {
     public void setUpdatedAt(Date updatedAt) {
         this.updatedAt = updatedAt;
     }
->>>>>>> 95f146351f (fix: hide sensitive additional properties from api)
 
     public Boolean isInactive() {
         return isPreRegistration() && !isRegistrationCompleted();
