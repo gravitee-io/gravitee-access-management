5af9e93ad2dc7ce53dba02641447c1b31c5a768a
eric <eric.leleu@graviteesource.com>
2024-09-10 17:20:20 +0200

    fix: restore the default contructor for WebClientBuilder as it is used by EE plugins
    fixes AM-3873
    
    fixes AM-3845
    
    fixes AM-3790
    
    (cherry picked from commit 540fb0fd4eb6bbeebb0a4ff4243224cf4df140a1)

--- gravitee-am-service/src/main/java/io/gravitee/am/service/http/WebClientBuilder.java
+++ gravitee-am-service/src/main/java/io/gravitee/am/service/http/WebClientBuilder.java
@@ -22,6 +22,7 @@ import io.vertx.rxjava3.core.Vertx;
 import io.vertx.rxjava3.ext.web.client.WebClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.core.env.Environment;
 
@@ -49,11 +50,15 @@ public class WebClientBuilder {
     private static final String SSL_KEYSTORE_STORE_PASSWORD = "httpClient.ssl.keystore.password";
     private static final Pattern WILDCARD_PATTERN = Pattern.compile("\\*\\.");
 
-    private final Environment environment;
+    @Autowired
+    private Environment environment;
+
+    public WebClientBuilder() {}
 
     public WebClientBuilder(Environment environment) {
         this.environment = environment;
     }
+
     public WebClient createWebClient(Vertx vertx, URL url) {
 
         final int port = url.getPort() != -1 ? url.getPort() : (HTTPS_SCHEME.equals(url.getProtocol()) ? 443 : 80);
