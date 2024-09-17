b114c697ccae0bbf153c179fca189fb534ad2530
Rafal Podles <rpo@spyro-soft.com>
2024-09-17 09:34:09 +0200

    chore: rewrite discovery oidc postman collection to jest test
    (cherry picked from commit ada5a608fd7907de1313ffb36b45e873b9819c34)

---
+++ gravitee-am-test/specs/management/openid-discovery.jest.spec.ts
@@ -0,0 +1,131 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import fetch from 'cross-fetch';
+import { afterAll, beforeAll, expect, jest } from '@jest/globals';
+import { requestAdminAccessToken } from '@management-commands/token-management-commands';
+import { createDomain, deleteDomain, startDomain } from '@management-commands/domain-management-commands';
+import { buildRSA512Certificate } from '@api-fixtures/certificates';
+import { createCertificate } from '@management-commands/certificate-management-commands';
+import { getWellKnownOpenIdConfiguration, performGet } from '@gateway-commands/oauth-oidc-commands';
+import { Domain } from 'api/management/models';
+import {delay} from '@utils-commands/misc';
+
+global.fetch = fetch;
+
+let accessToken: any;
+let domain: Domain;
+let jwksUriEndpoint: any;
+
+jest.setTimeout(200000);
+
+beforeAll(async () => {
+  const adminTokenResponse = await requestAdminAccessToken();
+  accessToken = adminTokenResponse.body.access_token;
+  expect(accessToken).toBeDefined();
+
+  const createdDomain = await createDomain(accessToken, 'oidc-discovery', 'test openid connect discovery specifications');
+  expect(createdDomain).toBeDefined();
+  expect(createdDomain.id).toBeDefined();
+
+  const builtCertificate = buildRSA512Certificate();
+  const certificateResponse = await createCertificate(createdDomain.id, accessToken, builtCertificate);
+  expect(certificateResponse).toBeDefined();
+
+  const domainStarted = await startDomain(createdDomain.id, accessToken);
+  expect(domainStarted).toBeDefined();
+  expect(domainStarted.id).toEqual(createdDomain.id);
+
+  domain = domainStarted;
+});
+
+describe('well-known/openid-configuration', () => {
+  it('must describe oidc', async () => {
+    await delay(5000);
+    const wellKnown = await getWellKnownOpenIdConfiguration(domain.hrid);
+    // Discovery endpoints:
+    const openIdConfiguration = wellKnown.body;
+    expect(openIdConfiguration).toHaveProperty('authorization_endpoint');
+    expect(openIdConfiguration).toHaveProperty('token_endpoint');
+    expect(openIdConfiguration).toHaveProperty('userinfo_endpoint');
+    expect(openIdConfiguration).toHaveProperty('jwks_uri');
+    jwksUriEndpoint = openIdConfiguration.jwks_uri;
+    expect(openIdConfiguration).toHaveProperty('end_session_endpoint');
+    expect(openIdConfiguration).toHaveProperty('revocation_endpoint');
+    expect(openIdConfiguration).toHaveProperty('introspection_endpoint');
+    expect(openIdConfiguration).toHaveProperty('registration_endpoint');
+
+    // Discovery properties:
+    expect(openIdConfiguration).toHaveProperty('issuer');
+    expect(openIdConfiguration).toHaveProperty('scopes_supported');
+    expect(openIdConfiguration).toHaveProperty('response_types_supported');
+    expect(openIdConfiguration).toHaveProperty('id_token_signing_alg_values_supported');
+    expect(openIdConfiguration).toHaveProperty('id_token_encryption_alg_values_supported');
+    expect(openIdConfiguration).toHaveProperty('id_token_encryption_enc_values_supported');
+    expect(openIdConfiguration).toHaveProperty('userinfo_signing_alg_values_supported');
+    expect(openIdConfiguration).toHaveProperty('userinfo_encryption_alg_values_supported');
+    expect(openIdConfiguration).toHaveProperty('userinfo_encryption_enc_values_supported');
+    expect(openIdConfiguration).toHaveProperty('token_endpoint_auth_methods_supported');
+    expect(openIdConfiguration).toHaveProperty('claim_types_supported');
+    expect(openIdConfiguration).toHaveProperty('claims_supported');
+    expect(openIdConfiguration).toHaveProperty('code_challenge_methods_supported');
+    expect(openIdConfiguration).toHaveProperty('claims_parameter_supported');
+    expect(openIdConfiguration).toHaveProperty('request_parameter_supported');
+    expect(openIdConfiguration).toHaveProperty('request_uri_parameter_supported');
+    expect(openIdConfiguration).toHaveProperty('require_request_uri_registration');
+
+    // Check for specific values:
+    expect(openIdConfiguration.code_challenge_methods_supported).toEqual(['plain', 'S256']);
+    expect(openIdConfiguration.scopes_supported).toEqual([
+      'address',
+      'email',
+      'full_profile',
+      'groups',
+      'offline_access',
+      'openid',
+      'phone',
+      'profile',
+      'roles',
+    ]);
+  });
+
+  it('must return jwks', async () => {
+    const jwks = await performGet(jwksUriEndpoint);
+    const responseBody = jwks.body;
+    expect(responseBody).toHaveProperty('keys');
+
+    // Check for the RSA keys
+    const keyTypes = responseBody.keys.map((key) => key.kty);
+    expect(keyTypes).toContain('RSA');
+
+    // Ensure there are two RSA keys
+    const rsaKeys = responseBody.keys.filter((key) => key.kty === 'RSA');
+    expect(rsaKeys.length).toBe(2);
+
+    // Check if one of the keys has RS512 algorithm
+    const hasRS512 = rsaKeys.some((key) => key.alg === 'RS512');
+    expect(hasRS512).toBe(true);
+
+    // Check if one of the keys has RS256 algorithm
+    const hasRS256 = rsaKeys.some((key) => key.alg === 'RS256');
+    expect(hasRS256).toBe(true);
+  });
+});
+
+afterAll(async () => {
+  if (domain?.id) {
+    await deleteDomain(domain.id, accessToken);
+  }
+});
