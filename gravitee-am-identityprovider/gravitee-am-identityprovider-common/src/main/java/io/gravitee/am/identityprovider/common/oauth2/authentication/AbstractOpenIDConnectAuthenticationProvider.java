9af4e9e7b805a587fde02ffff1d44ffe3211900e
Marcin Regulski <marcin.regulski@graviteesource.com>
2024-09-17 11:47:10 +0200

    feat(gateway): support PKCE for identity providers
    AM-3685
    
    feat(gateway): PKCE for IDPs - cleanup, fixes after code review
    
    AM-3685

--- gravitee-am-identityprovider/gravitee-am-identityprovider-common/src/main/java/io/gravitee/am/identityprovider/common/oauth2/authentication/AbstractOpenIDConnectAuthenticationProvider.java
+++ gravitee-am-identityprovider/gravitee-am-identityprovider-common/src/main/java/io/gravitee/am/identityprovider/common/oauth2/authentication/AbstractOpenIDConnectAuthenticationProvider.java
@@ -60,7 +60,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.util.Assert;
-import org.springframework.util.StringUtils;
 
 import java.util.ArrayList;
 import java.util.HashMap;
