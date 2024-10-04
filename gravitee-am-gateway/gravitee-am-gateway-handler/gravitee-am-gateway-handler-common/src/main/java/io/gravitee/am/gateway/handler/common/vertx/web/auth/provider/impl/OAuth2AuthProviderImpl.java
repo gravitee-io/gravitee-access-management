0627532af8946b2026151963f7f12db4c24f3394
Lukasz Gawel <lukasz.gawel@graviteesource.com>
2024-10-04 09:42:21 +0200

    fix: introspection on refresh token fixes AM-4047
    (cherry picked from commit 43aa0cc0f767bb07cc8b5f3e96c91dfaf9ad0f10)

--- gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-common/src/main/java/io/gravitee/am/gateway/handler/common/vertx/web/auth/provider/impl/OAuth2AuthProviderImpl.java
+++ gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-common/src/main/java/io/gravitee/am/gateway/handler/common/vertx/web/auth/provider/impl/OAuth2AuthProviderImpl.java
@@ -23,6 +23,7 @@ import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 /**
  * @author Titouan COMPIEGNE (titouan.compiegne at graviteesource.com)
@@ -31,6 +32,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class OAuth2AuthProviderImpl implements OAuth2AuthProvider {
 
     @Autowired
+    @Qualifier("AccessTokenIntrospection")
     private IntrospectionTokenService introspectionTokenService;
 
     @Autowired
@@ -39,7 +41,7 @@ public class OAuth2AuthProviderImpl implements OAuth2AuthProvider {
     @Override
     public void decodeToken(String token, boolean offlineVerification, Handler<AsyncResult<OAuth2AuthResponse>> handler) {
         introspectionTokenService.introspect(token, offlineVerification)
-                .flatMapMaybe(jwt -> clientSyncService.findByDomainAndClientId(jwt.getDomain(), jwt.getAud())
+                .flatMap(jwt -> clientSyncService.findByDomainAndClientId(jwt.getDomain(), jwt.getAud())
                         .map(client -> new OAuth2AuthResponse(jwt, client)))
                 .subscribe(
                         accessToken -> handler.handle(Future.succeededFuture(accessToken)),
