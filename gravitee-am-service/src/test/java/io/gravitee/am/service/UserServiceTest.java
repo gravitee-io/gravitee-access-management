d3fd7f9c25126a3e40efd2f348311166ffd3afa5
eric <eric.leleu@graviteesource.com>
2024-10-14 15:14:41 +0200

    fix: reset mfaSkippedAt when a factor is enrolled & the code is verified
    fixes AM-4070
    
    gravitee-io/issuse#10086
    
    (cherry picked from commit 8cf61e11afde0e8dbd267bab149ca37fd57959e4)

--- gravitee-am-service/src/test/java/io/gravitee/am/service/UserServiceTest.java
+++ gravitee-am-service/src/test/java/io/gravitee/am/service/UserServiceTest.java
@@ -24,6 +24,7 @@ import io.gravitee.am.model.User;
 import io.gravitee.am.model.common.Page;
 import io.gravitee.am.model.factor.EnrolledFactor;
 import io.gravitee.am.model.factor.EnrolledFactorChannel;
+import io.gravitee.am.model.factor.FactorStatus;
 import io.gravitee.am.reporter.api.audit.model.Audit;
 import io.gravitee.am.repository.exceptions.TechnicalException;
 import io.gravitee.am.repository.management.api.UserRepository;
@@ -47,8 +48,11 @@ import io.reactivex.rxjava3.core.Single;
 import io.reactivex.rxjava3.observers.TestObserver;
 import io.reactivex.rxjava3.subscribers.TestSubscriber;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.ArgumentMatcher;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -56,6 +60,7 @@ import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.Collections;
+import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 import static io.gravitee.am.common.audit.EventType.USER_CREATED;
@@ -67,6 +72,7 @@ import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.anyString;
 import static org.mockito.Mockito.argThat;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -538,10 +544,14 @@ public class UserServiceTest {
         final var userid = "userid";
         final var enrolledFactor = new EnrolledFactor();
         enrolledFactor.setFactorId("factorid");
+        enrolledFactor.setStatus(FactorStatus.PENDING_ACTIVATION);
         enrolledFactor.setChannel(new EnrolledFactorChannel(EnrolledFactorChannel.Type.SMS, "+33606060606"));
 
-        when(userRepository.findById(userid)).thenReturn(Maybe.just(new User()));
-        when(userRepository.update(any(), any())).thenReturn(Single.just(new User()));
+        User userProfile = new User();
+        userProfile.setMfaEnrollmentSkippedAt(new Date());
+        when(userRepository.findById(userid)).thenReturn(Maybe.just(userProfile));
+        ArgumentCaptor<User> userArgs = ArgumentCaptor.forClass(User.class);
+        when(userRepository.update(userArgs.capture(), any())).thenReturn(Single.just(new User()));
 
         final var observer = userService.upsertFactor(userid, enrolledFactor, new DefaultUser()).test();
         observer.awaitDone(10, TimeUnit.SECONDS);
@@ -550,6 +560,31 @@ public class UserServiceTest {
         verify(userRepository).update(argThat(user -> user.getFactors() != null
                 && !user.getFactors().isEmpty()
                 && user.getFactors().get(0).getFactorId().equals(enrolledFactor.getFactorId()) ), any());
+        Assertions.assertNotNull(userArgs.getValue().getMfaEnrollmentSkippedAt());
+    }
+
+    @Test
+    public void shouldUpsertFactor_SMS_resetMfaSkippedAt() {
+        final var userid = "userid";
+        final var enrolledFactor = new EnrolledFactor();
+        enrolledFactor.setFactorId("factorid");
+        enrolledFactor.setStatus(FactorStatus.ACTIVATED);
+        enrolledFactor.setChannel(new EnrolledFactorChannel(EnrolledFactorChannel.Type.SMS, "+33606060606"));
+
+        User userProfile = new User();
+        userProfile.setMfaEnrollmentSkippedAt(new Date());
+        when(userRepository.findById(userid)).thenReturn(Maybe.just(userProfile));
+        ArgumentCaptor<User> userArgs = ArgumentCaptor.forClass(User.class);
+        when(userRepository.update(userArgs.capture(), any())).thenReturn(Single.just(new User()));
+
+        final var observer = userService.upsertFactor(userid, enrolledFactor, new DefaultUser()).test();
+        observer.awaitDone(10, TimeUnit.SECONDS);
+        observer.assertNoErrors();
+
+        verify(userRepository).update(argThat(user -> user.getFactors() != null
+                && !user.getFactors().isEmpty()
+                && user.getFactors().get(0).getFactorId().equals(enrolledFactor.getFactorId()) ), any());
+        Assertions.assertNull(userArgs.getValue().getMfaEnrollmentSkippedAt());
     }
 
     @Test
