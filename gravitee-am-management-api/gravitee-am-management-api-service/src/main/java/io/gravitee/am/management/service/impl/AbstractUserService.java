b5ef44e8073cb697624eae3ede6ed7146d4a323f
Marcin Regulski <165262917+mregulski-gravitee@users.noreply.github.com>
2024-10-29 13:50:33 +0100

    Disable creating service users in domains (#5076)

--- gravitee-am-management-api/gravitee-am-management-api-service/src/main/java/io/gravitee/am/management/service/impl/AbstractUserService.java
+++ gravitee-am-management-api/gravitee-am-management-api-service/src/main/java/io/gravitee/am/management/service/impl/AbstractUserService.java
@@ -321,7 +321,7 @@ public abstract class AbstractUserService<T extends io.gravitee.am.service.Commo
         user.setCreatedAt(new Date());
         user.setUpdatedAt(user.getCreatedAt());
         user.setLastPasswordReset(user.getCreatedAt());
-        user.setServiceAccount(newUser.getServiceAccount());
+        user.setServiceAccount(false);
         return user;
     }
 
