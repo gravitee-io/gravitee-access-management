0c2831d799842844ec358ef46ef7032a677150bb
Rafal Podles <rafal.podles@graviteesource.com>
2025-01-15 14:46:07 +0100

    fix: audit details when message is missing
    (cherry picked from commit 0bb2ccb0fd23cb2e4cf033a75a59a2d74e163c0a)

--- gravitee-am-ui/src/app/domain/settings/audits/audits.component.ts
+++ gravitee-am-ui/src/app/domain/settings/audits/audits.component.ts
@@ -275,7 +275,7 @@ export class AuditsComponent implements OnInit {
         return row.outcome.message;
       }
     } else {
-      return row.type + ' success';
+      return row.type + ' ' + row.outcome.status;
     }
   }
 
