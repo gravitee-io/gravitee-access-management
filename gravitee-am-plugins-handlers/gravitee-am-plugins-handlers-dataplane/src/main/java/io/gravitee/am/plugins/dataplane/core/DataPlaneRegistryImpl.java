8c563f2f005f585ef986ba28828c0673d2ab8e5f
eric <eric.leleu@graviteesource.com>
2025-01-24 12:09:08 +0100

    chore(dataplane): add PasswordHistory repository
    related-to AM-4706

--- gravitee-am-plugins-handlers/gravitee-am-plugins-handlers-dataplane/src/main/java/io/gravitee/am/plugins/dataplane/core/DataPlaneRegistryImpl.java
+++ gravitee-am-plugins-handlers/gravitee-am-plugins-handlers-dataplane/src/main/java/io/gravitee/am/plugins/dataplane/core/DataPlaneRegistryImpl.java
@@ -20,6 +20,7 @@ import io.gravitee.am.dataplane.api.DataPlaneProvider;
 import io.gravitee.am.dataplane.api.repository.CredentialRepository;
 import io.gravitee.am.dataplane.api.repository.DeviceRepository;
 import io.gravitee.am.dataplane.api.repository.GroupRepository;
+import io.gravitee.am.dataplane.api.repository.PasswordHistoryRepository;
 import io.gravitee.am.dataplane.api.repository.ScopeApprovalRepository;
 import io.gravitee.am.dataplane.api.repository.UserActivityRepository;
 import io.gravitee.am.dataplane.api.repository.UserRepository;
@@ -100,6 +101,11 @@ public class DataPlaneRegistryImpl extends AbstractService<DataPlaneRegistryImpl
     @Override
     public UserRepository getUserRepository(Domain domain) {
         return getProvider(domain).getUserRepository();
+    }
+
+    @Override
+    public PasswordHistoryRepository getPasswordHistoryRepository(Domain domain) {
+        return getProvider(domain).getPasswordHistoryRepository();
     }
 
     @Override
