7191b89efc91ee19660bbb1dee0994402efafb74
eric <eric.leleu@graviteesource.com>
2025-01-28 18:30:35 +0100

    fix: provide valid scope value to DeviceNotification service
    fixes AM-4722
    
    (cherry picked from commit 3a6cc8210f36522f8174f357460fc93235208c0f)
    
    # Conflicts:
    #	gravitee-am-authdevice-notifier/gravitee-am-authdevice-notifier-http/src/main/java/io/gravitee/am/authdevice/notifier/http/provider/HttpAuthenticationDeviceNotifierProvider.java
    #	gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-oidc/src/main/java/io/gravitee/am/gateway/handler/ciba/ad/AuthenticationDeviceNotifier.java
    #	gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-oidc/src/main/java/io/gravitee/am/gateway/handler/ciba/ad/impl/AuthenticationDeviceNotifierImpl.java

--- gravitee-am-authdevice-notifier/gravitee-am-authdevice-notifier-http/src/main/java/io/gravitee/am/authdevice/notifier/http/provider/HttpAuthenticationDeviceNotifierProvider.java
+++ gravitee-am-authdevice-notifier/gravitee-am-authdevice-notifier-http/src/main/java/io/gravitee/am/authdevice/notifier/http/provider/HttpAuthenticationDeviceNotifierProvider.java
@@ -31,6 +31,8 @@ import io.vertx.rxjava3.core.MultiMap;
 import io.vertx.rxjava3.core.buffer.Buffer;
 import io.vertx.rxjava3.ext.web.client.HttpRequest;
 import io.vertx.rxjava3.ext.web.client.WebClient;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,6 +42,7 @@ import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
 
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import static org.springframework.util.StringUtils.isEmpty;
 
@@ -48,6 +51,8 @@ import static org.springframework.util.StringUtils.isEmpty;
  * @author GraviteeSource Team
  */
 @Import(HttpAuthenticationDeviceProviderSpringConfiguration.class)
+@AllArgsConstructor
+@NoArgsConstructor
 public class HttpAuthenticationDeviceNotifierProvider implements AuthenticationDeviceNotifierProvider {
     private static final Logger LOGGER = LoggerFactory.getLogger(HttpAuthenticationDeviceNotifierProvider.class);
 
@@ -138,6 +143,28 @@ public class HttpAuthenticationDeviceNotifierProvider implements AuthenticationD
                 });
     }
 
+<<<<<<< HEAD
+=======
+    private MultiMap prepareFormData(ADNotificationRequest request) {
+        final MultiMap formData = MultiMap.caseInsensitiveMultiMap();
+
+        formData.set(TRANSACTION_ID, request.getTransactionId());
+        formData.set(STATE, request.getState());
+        formData.set(PARAM_SUBJECT, request.getSubject());
+        formData.set(PARAM_SCOPE, request.getScopes().stream().collect(Collectors.joining(" ")));
+        formData.set(PARAM_EXPIRE, Integer.toString(request.getExpiresIn()));
+
+        if (!CollectionUtils.isEmpty(request.getAcrValues())) {
+            formData.set(PARAM_ACR, request.getAcrValues());
+        }
+
+        if (hasText(request.getMessage())) {
+            formData.set(PARAM_MESSAGE, request.getMessage());
+        }
+        return formData;
+    }
+
+>>>>>>> 3a6cc8210 (fix: provide valid scope value to DeviceNotification service)
     @Override
     public Single<Optional<ADUserResponse>> extractUserResponse(ADCallbackContext callbackContext) {
         final String state = callbackContext.getParam(STATE);
