6e9d9a857044c2b2ea6dea90005d106439fc8f6e
eric <eric.leleu@graviteesource.com>
2025-03-20 18:57:43 +0100

    fix: remove the loginAttempt collection from migration scripts
     this collection was part of the gateway scope, it is useless to migrate it
    
    relate-to AM-4576
    
    (cherry picked from commit a98ff855b2e4c59382bc4344b8ab0352deeda6ef)

--- docs/upgrades/4.7.0/sqlserver-migration.sh
+++ docs/upgrades/4.7.0/sqlserver-migration.sh
@@ -34,7 +34,7 @@ SCHEMA="$(dirname $0)/sqlserver.schema"
 
 
 # List of tables to migrate (can also read from a file, see below for alternative)
-TABLES=("uma_resource_set" "uma_resource_scopes" "webauthn_credentials" "groups" "group_members" "group_roles" "devices" "password_histories" "users" "user_entitlements" "user_roles" "user_addresses" "user_attributes" "dynamic_user_roles" "dynamic_user_groups" "user_activities" "login_attempts" "user_identities" "uma_access_policies" "uma_permission_ticket")
+TABLES=("uma_resource_set" "uma_resource_scopes" "webauthn_credentials" "groups" "group_members" "group_roles" "devices" "password_histories" "users" "user_entitlements" "user_roles" "user_addresses" "user_attributes" "dynamic_user_roles" "dynamic_user_groups" "user_activities" "user_identities" "uma_access_policies" "uma_permission_ticket")
 
 # Export passwords to avoid being prompted
 export SQLCMDPASSWORD=$SOURCE_DB_PASSWORD
