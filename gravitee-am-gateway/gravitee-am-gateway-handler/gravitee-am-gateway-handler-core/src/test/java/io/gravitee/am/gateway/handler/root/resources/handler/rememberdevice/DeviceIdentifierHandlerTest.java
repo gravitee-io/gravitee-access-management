84e9d67c97a3f76bf3a094ed8a20ce65a4a08ae2
eric <eric.leleu@graviteesource.com>
2025-06-25 19:42:29 +0200

    feat: introduce Cookie base Remember device
     this feat fixes the accuracy issue linked to FingerprintJS OSS edition
     an option has been added to the FP OSS plugin to also rely on cookies
     this will allow existing user to use the cookie in the current config
     without having to adapt the templates.
    
    fixes AM-5310
    
    (cherry picked from commit e53e242b70e3213533b282013fb7312e1387b141)

--- gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-core/src/test/java/io/gravitee/am/gateway/handler/root/resources/handler/rememberdevice/DeviceIdentifierHandlerTest.java
+++ gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-core/src/test/java/io/gravitee/am/gateway/handler/root/resources/handler/rememberdevice/DeviceIdentifierHandlerTest.java
@@ -17,6 +17,10 @@
 package io.gravitee.am.gateway.handler.root.resources.handler.rememberdevice;
 
 import io.gravitee.am.gateway.handler.common.service.DeviceGatewayService;
+import io.gravitee.am.common.jwt.JWT;
+import io.gravitee.am.gateway.handler.common.jwt.JWTService;
+import io.gravitee.am.gateway.handler.manager.deviceidentifiers.DeviceIdentifierManager;
+import io.gravitee.am.gateway.handler.root.resources.handler.dummies.DummyHttpRequest;
 import io.gravitee.am.gateway.handler.root.resources.handler.dummies.SpyRoutingContext;
 import io.gravitee.am.model.Domain;
 import io.gravitee.am.model.MFASettings;
@@ -24,6 +28,7 @@ import io.gravitee.am.model.RememberDeviceSettings;
 import io.gravitee.am.model.User;
 import io.gravitee.am.model.oidc.Client;
 import io.reactivex.rxjava3.core.Single;
+import io.vertx.core.http.Cookie;
 import io.vertx.core.http.HttpMethod;
 import org.junit.Assert;
 import org.junit.Before;
@@ -34,6 +39,7 @@ import org.mockito.junit.MockitoJUnitRunner;
 import java.util.UUID;
 
 import static io.gravitee.am.common.utils.ConstantKeys.CLIENT_CONTEXT_KEY;
+import static io.gravitee.am.common.utils.ConstantKeys.DEFAULT_REMEMBER_DEVICE_COOKIE_NAME;
 import static io.gravitee.am.common.utils.ConstantKeys.DEVICE_ALREADY_EXISTS_KEY;
 import static io.gravitee.am.common.utils.ConstantKeys.DEVICE_ID;
 import static io.gravitee.am.common.utils.ConstantKeys.DEVICE_TYPE;
@@ -45,6 +51,7 @@ import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * @author Rémi SULTAN (remi.sultan at graviteesource.com)
@@ -54,6 +61,8 @@ import static org.mockito.Mockito.verify;
 public class DeviceIdentifierHandlerTest {
 
     private DeviceGatewayService deviceService;
+    private DeviceIdentifierManager deviceIdentifierManager;
+    private JWTService jwtService;
 
     private SpyRoutingContext spyRoutingContext;
     private Client client;
@@ -69,8 +78,11 @@ public class DeviceIdentifierHandlerTest {
         client.setDomain(domain.getId());
         client.setId(UUID.randomUUID().toString());
         client.setClientId(UUID.randomUUID().toString());
+
         deviceService = spy(DeviceGatewayService.class);
-        handler = new DeviceIdentifierHandler(domain, deviceService);
+        deviceIdentifierManager = spy(DeviceIdentifierManager.class);
+        jwtService = spy(JWTService.class);
+        handler = new DeviceIdentifierHandler(domain, deviceService, deviceIdentifierManager, jwtService, DEFAULT_REMEMBER_DEVICE_COOKIE_NAME);
 
         userId = UUID.randomUUID().toString();
         deviceIdentifierId = UUID.randomUUID().toString();
@@ -196,6 +208,34 @@ public class DeviceIdentifierHandlerTest {
     }
 
     @Test
+    public void mustDoNext_DeviceAlreadyExists_Cookie() {
+        final MFASettings mfaSettings = new MFASettings();
+        final RememberDeviceSettings rememberDevice = new RememberDeviceSettings();
+        rememberDevice.setActive(true);
+        rememberDevice.setDeviceIdentifierId(UUID.randomUUID().toString());
+        mfaSettings.setRememberDevice(rememberDevice);
+        client.setMfaSettings(mfaSettings);
+        spyRoutingContext.put(CLIENT_CONTEXT_KEY, client);
+
+        final User user = new User();
+        user.setId(userId);
+        spyRoutingContext.put(USER_CONTEXT_KEY, user);
+        final var request = (DummyHttpRequest) spyRoutingContext.request().getDelegate();
+        request.putCookie(Cookie.cookie(DEFAULT_REMEMBER_DEVICE_COOKIE_NAME, UUID.randomUUID().toString()));
+
+        JWT jwt = new JWT();
+        jwt.setJti("deviceId");
+        doReturn(Single.just(jwt)).when(jwtService).decodeAndVerify(anyString(), any(Client.class), any());
+
+        when(deviceIdentifierManager.useCookieBasedDeviceIdentifier(any())).thenReturn(true);
+        doReturn(Single.just(false)).when(deviceService).deviceExists(any(), anyString(), any(), anyString(), anyString());
+        handler.handle(spyRoutingContext);
+
+        Assert.assertTrue(spyRoutingContext.session().get(DEVICE_ALREADY_EXISTS_KEY));
+        verify(spyRoutingContext, times(1)).next();
+    }
+
+    @Test
     public void mustDoNext_DeviceNotExists() {
         final MFASettings mfaSettings = new MFASettings();
         final RememberDeviceSettings rememberDevice = new RememberDeviceSettings();
@@ -209,6 +249,40 @@ public class DeviceIdentifierHandlerTest {
         user.setId(userId);
         spyRoutingContext.put(USER_CONTEXT_KEY, user);
         spyRoutingContext.putParam(DEVICE_ID, "deviceId2");
+        spyRoutingContext.putParam(DEVICE_TYPE, "deviceType");
+
+        doReturn(Single.just(true)).when(deviceService).deviceExists(any(), anyString(), any(), anyString(), anyString());
+        handler.handle(spyRoutingContext);
+
+        Assert.assertFalse(spyRoutingContext.session().get(DEVICE_ALREADY_EXISTS_KEY));
+        Assert.assertEquals(spyRoutingContext.session().get(DEVICE_ID), "deviceId2");
+        Assert.assertEquals(spyRoutingContext.session().get(DEVICE_TYPE), "deviceType");
+        verify(spyRoutingContext, times(1)).next();
+    }
+
+    @Test
+    public void mustDoNext_DeviceNotExists_Cookie() {
+        final MFASettings mfaSettings = new MFASettings();
+        final RememberDeviceSettings rememberDevice = new RememberDeviceSettings();
+        rememberDevice.setActive(true);
+        rememberDevice.setDeviceIdentifierId(UUID.randomUUID().toString());
+        mfaSettings.setRememberDevice(rememberDevice);
+        client.setMfaSettings(mfaSettings);
+        spyRoutingContext.put(CLIENT_CONTEXT_KEY, client);
+
+        final User user = new User();
+        user.setId(userId);
+        spyRoutingContext.put(USER_CONTEXT_KEY, user);
+
+        final var request = (DummyHttpRequest) spyRoutingContext.request().getDelegate();
+        request.putCookie(Cookie.cookie(DEFAULT_REMEMBER_DEVICE_COOKIE_NAME, UUID.randomUUID().toString()));
+
+        JWT jwt = new JWT();
+        jwt.setJti("deviceId2");
+        doReturn(Single.just(jwt)).when(jwtService).decodeAndVerify(anyString(), any(Client.class), any());
+
+        when(deviceIdentifierManager.useCookieBasedDeviceIdentifier(any())).thenReturn(true);
+
         spyRoutingContext.putParam(DEVICE_TYPE, "deviceType");
 
         doReturn(Single.just(true)).when(deviceService).deviceExists(any(), anyString(), any(), anyString(), anyString());
