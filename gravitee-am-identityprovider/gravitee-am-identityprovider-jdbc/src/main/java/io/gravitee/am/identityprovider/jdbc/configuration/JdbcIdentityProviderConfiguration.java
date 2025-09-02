99e1115e3f5af0462a82dd307670e7e8a996f332
Lukasz Gawel <lukasz.gawel@graviteesource.com>
2025-09-02 21:16:47 +0200

    feat: added currentSchema property for postgres uri connection
    closes AM-4760
    
    (cherry picked from commit 04f76df814134344317fa9437137d5bcac03afc4)

--- gravitee-am-identityprovider/gravitee-am-identityprovider-jdbc/src/main/java/io/gravitee/am/identityprovider/jdbc/configuration/JdbcIdentityProviderConfiguration.java
+++ gravitee-am-identityprovider/gravitee-am-identityprovider-jdbc/src/main/java/io/gravitee/am/identityprovider/jdbc/configuration/JdbcIdentityProviderConfiguration.java
@@ -103,6 +103,7 @@ public class JdbcIdentityProviderConfiguration implements IdentityProviderConfig
         this.database = database;
     }
 
+
     public String getUsersTable() {
         return usersTable;
     }
--- gravitee-am-repository/gravitee-am-repository-mongodb/src/main/java/io/gravitee/am/repository/mongodb/common/AbstractRepositoryConfiguration.java
+++ gravitee-am-repository/gravitee-am-repository-mongodb/src/main/java/io/gravitee/am/repository/mongodb/common/AbstractRepositoryConfiguration.java
@@ -19,7 +19,6 @@ import io.gravitee.am.common.env.RepositoriesEnvironment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 
 /**
  * @author Titouan COMPIEGNE (titouan.compiegne at graviteesource.com)
