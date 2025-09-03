cfc467aed700b6503e940dac9e10d8cf9a80c759
Fu Yu <fu.yu@Fus-MacBook-Pro.local>
2025-09-03 09:01:55 +0100

    AM-5484 use vertx timer to prevent timer thread failures

--- gravitee-am-reporter/gravitee-am-reporter-file/src/main/java/io/gravitee/am/reporter/file/vertx/VertxFileWriter.java
+++ gravitee-am-reporter/gravitee-am-reporter-file/src/main/java/io/gravitee/am/reporter/file/vertx/VertxFileWriter.java
@@ -33,9 +33,30 @@ import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
 import java.util.TimeZone;
-import java.util.Timer;
-import java.util.TimerTask;
 import java.util.regex.Pattern;
+
+/**
+ * Time provider interface to allow time mocking in tests
+ */
+interface TimeProvider {
+    ZonedDateTime now();
+    ZonedDateTime now(TimeZone timeZone);
+}
+
+/**
+ * Default time provider implementation
+ */
+class DefaultTimeProvider implements TimeProvider {
+    @Override
+    public ZonedDateTime now() {
+        return ZonedDateTime.now();
+    }
+    
+    @Override
+    public ZonedDateTime now(TimeZone timeZone) {
+        return ZonedDateTime.now(timeZone.toZoneId());
+    }
+}
 
 /**
  * @author David BRASSELY (david.brassely at graviteesource.com)
@@ -75,52 +96,62 @@ public class VertxFileWriter<T extends ReportEntry> {
     private AsyncFile asyncFile;
 
     private static final String ROLLOVER_FILE_DATE_FORMAT = "yyyy_MM_dd";
-
     public static final String YYYY_MM_DD = "yyyy_mm_dd";
+    
+    // File retention constants
+    private static final long MILLISECONDS_PER_DAY = 24 * 60 * 60 * 1000L;
 
-    private static Timer __rollover;
-    private RollTask _rollTask;
+    private Long rolloverTimerId;
 
     private final SimpleDateFormat fileDateFormat = new SimpleDateFormat(ROLLOVER_FILE_DATE_FORMAT);
 
     private long retainDays = -1;
 
-    private final Pattern rolloverFiles;
+    private Pattern rolloverFiles;
+    
+    private final TimeProvider timeProvider;
 
     public VertxFileWriter(Vertx vertx, Formatter<T> formatter, String filename) throws IOException {
+        this(vertx, formatter, filename, new DefaultTimeProvider());
+    }
+    
+    public VertxFileWriter(Vertx vertx, Formatter<T> formatter, String filename, TimeProvider timeProvider) throws IOException {
         this.vertx = vertx;
         this.formatter = formatter;
-
-        if (filename != null) {
-            filename = filename.trim();
-            if (filename.length() == 0)
-                filename = null;
-        }
-
-        if (filename == null) {
-            throw new IllegalArgumentException("Invalid filename");
-        }
-
-        this.filename = filename;
-
-        // calculate the rollover files pattern
-        File file = new File(this.filename);
-        templateFilename = file.getName();
-
-        rolloverFiles = Pattern.compile(templateFilename.replaceFirst(YYYY_MM_DD, "([0-9]{4}_[0-9]{2}_[0-9]{2})"));
-
-        __rollover = new Timer(VertxFileWriter.class.getName(), true);
+        this.timeProvider = timeProvider;
+        this.filename = validateAndNormalizeFilename(filename);
+        
+        initializeFileComponents();
     }
 
     public VertxFileWriter(Vertx vertx, Formatter<T> formatter, String filename, long retainDays) throws IOException {
         this(vertx, formatter, filename);
         this.retainDays = retainDays;
     }
+    
+    public VertxFileWriter(Vertx vertx, Formatter<T> formatter, String filename, long retainDays, TimeProvider timeProvider) throws IOException {
+        this(vertx, formatter, filename, timeProvider);
+        this.retainDays = retainDays;
+    }
+    
+    private String validateAndNormalizeFilename(String filename) {
+        if (filename == null || filename.trim().isEmpty()) {
+            throw new IllegalArgumentException("Invalid filename");
+        }
+        return filename.trim();
+    }
+    
+    private void initializeFileComponents() {
+        File file = new File(this.filename);
+        this.templateFilename = file.getName();
+        this.rolloverFiles = Pattern.compile(templateFilename.replaceFirst(YYYY_MM_DD, "([0-9]{4}_[0-9]{2}_[0-9]{2})"));
+        this.fileDateFormat.setTimeZone(TimeZone.getDefault());
+    }
 
     public Future<Void> initialize() {
         LOGGER.debug("Initializing file reporter, retainDays: {}", retainDays);
         // Calculate Today's Midnight, based on Configured TimeZone (will be in past, even if by a few milliseconds)
-        ZonedDateTime now = ZonedDateTime.now(TimeZone.getDefault().toZoneId());
+        ZonedDateTime now = timeProvider.now(TimeZone.getDefault());
 
         // This will schedule the rollover event to the next midnight
         scheduleNextRollover(now);
@@ -135,54 +166,68 @@ public class VertxFileWriter<T extends ReportEntry> {
         Promise<Void> promise = Promise.promise();
 
         synchronized (this) {
-            // Check directory
-            File file = new File(filename);
             try {
-                filename = file.getCanonicalPath();
-
-                file = new File(filename);
-                File dir = new File(file.getParent());
-                if (!dir.isDirectory() || !dir.canWrite()) {
-                    LOGGER.error("Cannot write reporter data to directory {}", dir);
-                    promise.fail(new IOException("Cannot write reporter data to directory " + dir));
+                String newFilename = prepareNewFilename(now);
+                if (newFilename == null) {
+                    promise.fail(new IOException("Cannot prepare filename for rollover"));
                     return promise.future();
                 }
 
-                String newDate = fileDateFormat.format(new Date(now.toInstant().toEpochMilli()));
-                filename = dir.getAbsolutePath() + File.separatorChar + 
-                          templateFilename.replaceFirst("(?i)" + YYYY_MM_DD, newDate);
-
-                LOGGER.info("Initializing file reporter to write into file: {}", filename);
-
-                AsyncFile oldAsyncFile = asyncFile;
-
-                vertx.fileSystem().open(filename, new OpenOptions()
-                                .setAppend(true)
-                                .setCreate(true)
-                                .setDsync(true), event -> {
-                            if (event.succeeded()) {
-                                asyncFile = event.result();
-
-                                if (oldAsyncFile != null) {
-                                    // Now we can close previous file safely
-                                    close(oldAsyncFile).onFailure(throwable -> {
-                                        LOGGER.error("An error occurs while closing file writer [{}]", this.filename, throwable);
-                                    });
-                                }
-
-                                promise.complete();
-                            } else {
-                                LOGGER.error("An error occurs while starting file writer [{}]", this.filename, event.cause());
-                                promise.fail(event.cause());
-                            }
-                        }
-                );
+                openNewFile(newFilename, promise);
             } catch (IOException ioe) {
                 promise.fail(ioe);
             }
         }
 
         return promise.future();
+    }
+    
+    private String prepareNewFilename(ZonedDateTime now) throws IOException {
+        File file = new File(filename);
+        filename = file.getCanonicalPath();
+        
+        File dir = new File(file.getParent());
+        if (!isDirectoryWritable(dir)) {
+            LOGGER.error("Cannot write reporter data to directory {}", dir);
+            return null;
+        }
+
+        String newDate = fileDateFormat.format(new Date(now.toInstant().toEpochMilli()));
+        return dir.getAbsolutePath() + File.separatorChar + 
+               templateFilename.replaceFirst("(?i)" + YYYY_MM_DD, newDate);
+    }
+    
+    private boolean isDirectoryWritable(File dir) {
+        return dir.isDirectory() && dir.canWrite();
+    }
+    
+    private void openNewFile(String newFilename, Promise<Void> promise) {
+        filename = newFilename;
+        LOGGER.info("Initializing file reporter to write into file: {}", filename);
+
+        AsyncFile oldAsyncFile = asyncFile;
+
+        vertx.fileSystem().open(filename, new OpenOptions()
+                .setAppend(true)
+                .setCreate(true)
+                .setDsync(true), event -> {
+            if (event.succeeded()) {
+                asyncFile = event.result();
+                closeOldFileIfExists(oldAsyncFile);
+                promise.complete();
+            } else {
+                LOGGER.error("An error occurs while starting file writer [{}]", filename, event.cause());
+                promise.fail(event.cause());
+            }
+        });
+    }
+    
+    private void closeOldFileIfExists(AsyncFile oldAsyncFile) {
+        if (oldAsyncFile != null) {
+            close(oldAsyncFile).onFailure(throwable -> {
+                LOGGER.error("An error occurs while closing file writer [{}]", filename, throwable);
+            });
+        }
     }
 
     public void write(T data) {
@@ -215,16 +260,40 @@ public class VertxFileWriter<T extends ReportEntry> {
     }
 
     private void scheduleNextRollover(ZonedDateTime now) {
-        _rollTask = new RollTask();
-
-        // Get tomorrow's midnight based on Configured TimeZone
+        cancelExistingTimer();
+        
         ZonedDateTime midnight = toMidnight(now);
-        LOGGER.debug("Scheduling next rollover for {}", midnight);
+        LOGGER.debug("Scheduling next rollover for {} (current time: {})", midnight, now);
 
-        // Schedule next rollover event to occur, based on local machine's Unix Epoch milliseconds
-        long delay = midnight.toInstant().toEpochMilli() - now.toInstant().toEpochMilli();
-        synchronized (VertxFileWriter.class) {
-            __rollover.schedule(_rollTask, delay);
+        long delay = calculateDelayToMidnight(now, midnight);
+        scheduleRolloverTimer(midnight, delay);
+    }
+    
+    private void cancelExistingTimer() {
+        if (rolloverTimerId != null) {
+            vertx.cancelTimer(rolloverTimerId);
+        }
+    }
+    
+    private long calculateDelayToMidnight(ZonedDateTime now, ZonedDateTime midnight) {
+        return midnight.toInstant().toEpochMilli() - now.toInstant().toEpochMilli();
+    }
+    
+    private void scheduleRolloverTimer(ZonedDateTime midnight, long delay) {
+        rolloverTimerId = vertx.setTimer(delay, timerId -> executeRollover(midnight));
+        
+        LOGGER.debug("Rollover timer scheduled with ID: {} for delay: {}ms (scheduled for: {})", 
+                    rolloverTimerId, delay, midnight);
+    }
+    
+    private void executeRollover(ZonedDateTime midnight) {
+        try {
+            LOGGER.debug("Executing scheduled rollover for: {}", midnight);
+            setFile(midnight);
+            scheduleNextRollover(midnight.plus(1, ChronoUnit.DAYS));
+            removeOldFiles();
+        } catch (Throwable t) {
+            LOGGER.error("Unexpected error while moving to a new reporter file", t);
         }
     }
 
@@ -238,60 +307,70 @@ public class VertxFileWriter<T extends ReportEntry> {
         return now.toLocalDate().atStartOfDay(now.getZone()).plus(1, ChronoUnit.DAYS);
     }
 
-    private class RollTask extends TimerTask {
-        @Override
-        public void run() {
-            try {
-                ZonedDateTime now = ZonedDateTime.now(fileDateFormat.getTimeZone().toZoneId());
-                VertxFileWriter.this.setFile(now);
-                VertxFileWriter.this.scheduleNextRollover(now);
-                VertxFileWriter.this.removeOldFiles();
-            } catch (Throwable t) {
-                LOGGER.error("Unexpected error while moving to a new reporter file", t);
+    protected void removeOldFiles() {
+        if (!isFileRetentionEnabled()) {
+            return;
+        }
+
+        File dir = getParentDirectory();
+        if (dir == null) {
+            return;
+        }
+
+        String[] logFiles = dir.list();
+        if (logFiles == null) {
+            return;
+        }
+
+        processLogFilesForRetention(dir, logFiles);
+    }
+    
+    private boolean isFileRetentionEnabled() {
+        if (retainDays <= 0) {
+            LOGGER.debug("No file retention configured");
+            return false;
+        }
+        if (rolloverFiles == null) {
+            return false;
+        }
+        return true;
+    }
+    
+    private File getParentDirectory() {
+        File file = new File(this.filename);
+        return new File(file.getParent());
+    }
+    
+    private void processLogFilesForRetention(File dir, String[] logFiles) {
+        long currentTime = timeProvider.now().toInstant().toEpochMilli();
+        
+        for (String filename : logFiles) {
+            if (rolloverFiles.matcher(filename).matches()) {
+                processLogFile(dir, filename, currentTime);
+            } else {
+                LOGGER.debug("File [{}] does not match the rollover pattern", filename);
             }
         }
     }
-
-    protected void removeOldFiles() {
-        if (retainDays <= 0) {
-            LOGGER.debug("No file retention configured");
-            return;
+    
+    private void processLogFile(File dir, String filename, long currentTime) {
+        File file = new File(dir, filename);
+        if (shouldDeleteFile(file, currentTime)) {
+            deleteFile(filename, file);
+        } else {
+            LOGGER.debug("File [{}] should not be removed", filename);
         }
-
-        if (rolloverFiles == null) {
-            return;
-        }
-
-        long now = System.currentTimeMillis();
-        File file = new File(this.filename);
-
-
-        File dir = new File(file.getParent());
-        String[] logList = dir.list();
-        if (logList == null) {
-            return;
-        }
-
-        for (String fn : logList) {
-            LOGGER.debug("Checking file [{}]", fn);
-            if (rolloverFiles.matcher(fn).matches()) {
-                File f = new File(dir, fn);
-                if (shouldDeleteFile(f, now)) {
-                    LOGGER.debug("File [{}] should be removed", fn);
-                    boolean fileDeleted = f.delete();
-                    if (!fileDeleted) {
-                        LOGGER.warn("File [{}] has not been removed", fn);
-                    }
-                } else {
-                    LOGGER.debug("File [{}] should not be removed", fn);
-                }
-            } else {
-                LOGGER.debug("File [{}] does not match the rollover pattern", fn);
-            }
+    }
+    
+    private void deleteFile(String filename, File file) {
+        LOGGER.debug("File [{}] should be removed", filename);
+        boolean fileDeleted = file.delete();
+        if (!fileDeleted) {
+            LOGGER.warn("File [{}] has not been removed", filename);
         }
     }
 
     protected boolean shouldDeleteFile(File file, long currentTimeInMs) {
-        return retainDays > 0 && currentTimeInMs - file.lastModified() > retainDays * 1000 * 60 * 60 * 24;
+        return retainDays > 0 && currentTimeInMs - file.lastModified() > retainDays * MILLISECONDS_PER_DAY;
     }
 }
