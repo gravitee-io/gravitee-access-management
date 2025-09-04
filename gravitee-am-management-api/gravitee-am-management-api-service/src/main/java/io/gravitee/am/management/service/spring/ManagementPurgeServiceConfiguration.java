0769a8804410badd1c23e6c5aedca3ed1ac79f0c
Lukasz Gawel <lukasz.gawel@graviteesource.com>
2025-09-04 15:34:36 +0200

    feat: added ScheduledPurgeService to mapi and gateway
    fixes AM-5431

---
+++ gravitee-am-management-api/gravitee-am-management-api-service/src/main/java/io/gravitee/am/management/service/spring/ManagementPurgeServiceConfiguration.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.am.management.service.spring;
+
+import io.gravitee.am.service.purge.ExpiredDataSweepers;
+import io.gravitee.am.service.purge.ScheduledPurgeService;
+import io.gravitee.am.service.purge.ScheduledPurgeServiceFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.TaskScheduler;
+
+import java.util.List;
+
+import static io.gravitee.am.repository.common.ExpiredDataSweeper.Target.events;
+
+@Configuration
+@Slf4j
+public class ManagementPurgeServiceConfiguration {
+    private final ScheduledPurgeServiceFactory factory = new ScheduledPurgeServiceFactory(List.of(events));
+
+    @Bean
+    public ScheduledPurgeService scheduledPurgeService(@Value("${services.purge.enabled:true}") boolean enabled,
+                                                       @Value("${repositories.management.type:mongodb}") String managementRepositoryType,
+                                                       @Value("${services.purge.cron:0 0 23 * * *}") String cron,
+                                                       @Value("#{'${services.purge.exclude:}'.empty ? {} : '${services.purge.exclude}'.split(',')}") List<String> excluded,
+                                                       TaskScheduler taskScheduler,
+                                                       ExpiredDataSweepers sweepers) {
+        enabled = enabled && "jdbc".equalsIgnoreCase(managementRepositoryType);
+        return factory.createPurgeService(enabled, cron, excluded, taskScheduler, sweepers);
+    }
+
+
+}
