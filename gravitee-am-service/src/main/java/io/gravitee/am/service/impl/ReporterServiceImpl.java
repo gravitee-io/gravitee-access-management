9ee1af1928abbb89f3da025de233f8b86bb08e71
eric <eric.leleu@graviteesource.com>
2025-09-25 12:04:16 +0200

    fix: define port as null instead of empty string for default reporter when this value is not define in the config
     this change fix an issue with upgrader which prevent the MAPI to start
    
    fixes AM-5688
    
    (cherry picked from commit e62a665038c143db325a48b3b85e87656244a0ef)

--- gravitee-am-service/src/main/java/io/gravitee/am/service/impl/ReporterServiceImpl.java
+++ gravitee-am-service/src/main/java/io/gravitee/am/service/impl/ReporterServiceImpl.java
@@ -358,12 +358,11 @@ public class ReporterServiceImpl implements ReporterService {
             var collectionSuffix = (reference == null || reference.matches(ReferenceType.ORGANIZATION, Organization.DEFAULT))
                     ? ""
                     : ("_" + reference.id());
-
             reporterConfig = """
                         {
                           "uri": "%s",
-                          %s
-                          %s
+                          "host": "%s",
+                          "port": %d,
                           "enableCredentials": false,
                           "database": "%s",
                           "reportableCollection": "reporter_audits%s",
@@ -372,8 +371,8 @@ public class ReporterServiceImpl implements ReporterService {
                         }
                         """.formatted(
                     mongoUri,
-                    (mongoHost != null) ? "\"host\":\"" + mongoHost + "\"," : "",
-                    (mongoPort != null) ? "\"port\":" + Integer.parseInt(mongoPort) + "," : "",
+                    (mongoHost != null) ?  mongoHost : "",
+                    (mongoPort != null) ? Integer.parseInt(mongoPort) : null,
                     mongoDBName,
                     collectionSuffix
             );
