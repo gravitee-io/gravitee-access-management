46232a69ffa94523fcd5792258814b453d8196b4
stuart.clark <stuart.clark@graviteesource.com>
2025-11-16 14:25:07 +0000

    lint: fix linting and license

--- gravitee-am-test/scripts/provisioning/logger.ts
+++ gravitee-am-test/scripts/provisioning/logger.ts
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 /* eslint-disable no-console */
 export const ansi = {
   reset: '\x1b[0m',
@@ -56,7 +71,7 @@ export function bullet(msg: string) {
 }
 
 // Spinners
-const frames = ['⠋','⠙','⠹','⠸','⠼','⠴','⠦','⠧','⠇','⠏'];
+const frames = ['⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏'];
 type Spinner = { timer?: NodeJS.Timeout; i: number; text: string; renderLen?: number };
 let activeSpinner: Spinner | null = null;
 
@@ -98,5 +113,3 @@ export function stopSpinner(s: Spinner, finalText?: string) {
     activeSpinner = null;
   }
 }
-
-
