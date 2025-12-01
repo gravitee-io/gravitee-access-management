36643dcb80634f69b37bd67343de5195e624f0c4
podlesrafal <rafal.podles@graviteesource.com>
2025-12-01 14:00:44 +0100

    fix: reduce verbosity on MFA errors
    (cherry picked from commit 142c608107457180b4e7dc56ed11ac5397fd85d5)

--- gravitee-am-factor/gravitee-am-factor-api/src/test/java/io/gravitee/am/factor/api/OTPFactorProviderTest.java
+++ gravitee-am-factor/gravitee-am-factor-api/src/test/java/io/gravitee/am/factor/api/OTPFactorProviderTest.java
@@ -20,14 +20,22 @@ import io.gravitee.am.common.factor.FactorDataKeys;
 import io.gravitee.am.model.factor.EnrolledFactor;
 import io.gravitee.am.model.factor.EnrolledFactorSecurity;
 import io.reactivex.rxjava3.core.Completable;
+import io.reactivex.rxjava3.exceptions.UndeliverableException;
 import io.reactivex.rxjava3.observers.TestObserver;
+import io.reactivex.rxjava3.plugins.RxJavaPlugins;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.time.Instant;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
@@ -38,6 +46,24 @@ public class OTPFactorProviderTest {
 
     private static final String CODE = "886024";
     private static final String SHARED_SECRET = "RVVAGS23PDJNU3NPRBMUHFXU4OXZTNRA";
+
+    private AtomicReference<Throwable> undeliverableException;
+
+    @BeforeEach
+    public void setUp() {
+        undeliverableException = new AtomicReference<>();
+        RxJavaPlugins.setErrorHandler(e -> {
+            if (e instanceof UndeliverableException) {
+                undeliverableException.set(e);
+            }
+        });
+    }
+
+    @AfterEach
+    public void tearDown() {
+        // Reset RxJava error handler
+        RxJavaPlugins.reset();
+    }
 
     @Test
     public void shouldUseVariableFactorSecurity() {
@@ -123,6 +149,157 @@ public class OTPFactorProviderTest {
         testObserver.awaitDone(10, TimeUnit.SECONDS);
         testObserver.assertError(InvalidCodeException.class);
         testObserver.assertNotComplete();
+    }
+
+    @Test
+    public void shouldCallEmitterOnlyOnce_whenCodeMismatch() {
+        EnrolledFactor enrolledFactor = new EnrolledFactor();
+        EnrolledFactorSecurity enrolledFactorSecurity = new EnrolledFactorSecurity();
+        enrolledFactorSecurity.setValue(SHARED_SECRET);
+        enrolledFactorSecurity.putData(FactorDataKeys.KEY_MOVING_FACTOR, 0);
+        enrolledFactorSecurity.putData(FactorDataKeys.KEY_EXPIRE_AT, Instant.now().plusSeconds(60).toEpochMilli());
+        enrolledFactor.setSecurity(enrolledFactorSecurity);
+
+        OTPFactorProvider otpFactorProvider = new DummyOTPFactorProvider();
+
+        TestObserver<Void> testObserver = otpFactorProvider.verifyOTP(enrolledFactor, 6, "123456").test();
+        testObserver.awaitDone(10, TimeUnit.SECONDS);
+
+        testObserver.assertError(InvalidCodeException.class);
+        testObserver.assertNotComplete();
+
+        assertNull(undeliverableException.get(),
+            "UndeliverableException should not occur - emitter was called multiple times!");
+    }
+
+    @Test
+    public void shouldCallEmitterOnlyOnce_whenCodeExpired() {
+        EnrolledFactor enrolledFactor = new EnrolledFactor();
+        EnrolledFactorSecurity enrolledFactorSecurity = new EnrolledFactorSecurity();
+        enrolledFactorSecurity.setValue(SHARED_SECRET);
+        enrolledFactorSecurity.putData(FactorDataKeys.KEY_MOVING_FACTOR, 0);
+        enrolledFactorSecurity.putData(FactorDataKeys.KEY_EXPIRE_AT, Instant.now().minusSeconds(10).toEpochMilli());
+        enrolledFactor.setSecurity(enrolledFactorSecurity);
+
+        OTPFactorProvider otpFactorProvider = new DummyOTPFactorProvider();
+
+        TestObserver<Void> testObserver = otpFactorProvider.verifyOTP(enrolledFactor, 6, CODE).test();
+        testObserver.awaitDone(10, TimeUnit.SECONDS);
+
+        testObserver.assertError(InvalidCodeException.class);
+        testObserver.assertNotComplete();
+
+        assertNull(undeliverableException.get(),
+            "UndeliverableException should not occur - emitter was called multiple times!");
+    }
+
+    @Test
+    public void shouldCallEmitterOnlyOnce_whenExpirationIsNull() {
+        EnrolledFactor enrolledFactor = new EnrolledFactor();
+        EnrolledFactorSecurity enrolledFactorSecurity = new EnrolledFactorSecurity();
+        enrolledFactorSecurity.setValue(SHARED_SECRET);
+        enrolledFactorSecurity.putData(FactorDataKeys.KEY_MOVING_FACTOR, 0);
+        enrolledFactor.setSecurity(enrolledFactorSecurity);
+
+        OTPFactorProvider otpFactorProvider = new DummyOTPFactorProvider();
+
+        TestObserver<Void> testObserver = otpFactorProvider.verifyOTP(enrolledFactor, 6, CODE).test();
+        testObserver.awaitDone(10, TimeUnit.SECONDS);
+
+        testObserver.assertError(InvalidCodeException.class);
+        testObserver.assertNotComplete();
+
+        assertNull(undeliverableException.get(),
+            "UndeliverableException should not occur - emitter was called multiple times!");
+    }
+
+    @Test
+    public void shouldCallEmitterOnlyOnce_whenVerificationSucceeds() {
+        EnrolledFactor enrolledFactor = new EnrolledFactor();
+        EnrolledFactorSecurity enrolledFactorSecurity = new EnrolledFactorSecurity();
+        enrolledFactorSecurity.setValue(SHARED_SECRET);
+        enrolledFactorSecurity.putData(FactorDataKeys.KEY_MOVING_FACTOR, 0);
+        enrolledFactorSecurity.putData(FactorDataKeys.KEY_EXPIRE_AT, Instant.now().plusSeconds(60).toEpochMilli());
+        enrolledFactor.setSecurity(enrolledFactorSecurity);
+
+        OTPFactorProvider otpFactorProvider = new DummyOTPFactorProvider();
+
+        TestObserver<Void> testObserver = otpFactorProvider.verifyOTP(enrolledFactor, 6, CODE).test();
+        testObserver.awaitDone(10, TimeUnit.SECONDS);
+
+        testObserver.assertNoErrors();
+        testObserver.assertComplete();
+
+        assertNull(undeliverableException.get(),
+            "UndeliverableException should not occur - emitter was called multiple times!");
+    }
+
+    @Test
+    public void shouldInvokeEmitterExactlyOnce_wrongCode() {
+        EnrolledFactor enrolledFactor = new EnrolledFactor();
+        EnrolledFactorSecurity enrolledFactorSecurity = new EnrolledFactorSecurity();
+        enrolledFactorSecurity.setValue(SHARED_SECRET);
+        enrolledFactorSecurity.putData(FactorDataKeys.KEY_MOVING_FACTOR, 0);
+        enrolledFactorSecurity.putData(FactorDataKeys.KEY_EXPIRE_AT, Instant.now().plusSeconds(60).toEpochMilli());
+        enrolledFactor.setSecurity(enrolledFactorSecurity);
+
+        AtomicInteger emitterCallCount = new AtomicInteger(0);
+
+        OTPFactorProvider otpFactorProvider = new DummyOTPFactorProvider();
+
+        Completable completable = Completable.create(emitter -> {
+            otpFactorProvider.verifyOTP(enrolledFactor, 6, "wrong-code")
+                .subscribe(
+                    () -> {
+                        emitterCallCount.incrementAndGet();
+                        emitter.onComplete();
+                    },
+                    error -> {
+                        emitterCallCount.incrementAndGet();
+                        emitter.onError(error);
+                    }
+                );
+        });
+
+        TestObserver<Void> testObserver = completable.test();
+        testObserver.awaitDone(10, TimeUnit.SECONDS);
+
+        assertEquals(1, emitterCallCount.get(),
+            "Emitter should be invoked exactly once, but was invoked " + emitterCallCount.get() + " times");
+    }
+
+    @Test
+    public void shouldInvokeEmitterExactlyOnce_expiredCode() {
+        EnrolledFactor enrolledFactor = new EnrolledFactor();
+        EnrolledFactorSecurity enrolledFactorSecurity = new EnrolledFactorSecurity();
+        enrolledFactorSecurity.setValue(SHARED_SECRET);
+        enrolledFactorSecurity.putData(FactorDataKeys.KEY_MOVING_FACTOR, 0);
+        enrolledFactorSecurity.putData(FactorDataKeys.KEY_EXPIRE_AT, Instant.now().minusSeconds(10).toEpochMilli());
+        enrolledFactor.setSecurity(enrolledFactorSecurity);
+
+        AtomicInteger emitterCallCount = new AtomicInteger(0);
+
+        OTPFactorProvider otpFactorProvider = new DummyOTPFactorProvider();
+
+        Completable completable = Completable.create(emitter -> {
+            otpFactorProvider.verifyOTP(enrolledFactor, 6, CODE)
+                .subscribe(
+                    () -> {
+                        emitterCallCount.incrementAndGet();
+                        emitter.onComplete();
+                    },
+                    error -> {
+                        emitterCallCount.incrementAndGet();
+                        emitter.onError(error);
+                    }
+                );
+        });
+
+        TestObserver<Void> testObserver = completable.test();
+        testObserver.awaitDone(10, TimeUnit.SECONDS);
+
+        assertEquals(1, emitterCallCount.get(),
+            "Emitter should be invoked exactly once, but was invoked " + emitterCallCount.get() + " times");
     }
 
     private static class DummyOTPFactorProvider extends OTPFactorProvider {
