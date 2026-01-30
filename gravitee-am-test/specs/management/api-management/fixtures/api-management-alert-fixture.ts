4517fb2d1f2a02063073484d7c30c096f2a667bb
stuart.clark <stuart.clark@graviteesource.com>
2026-01-30 12:45:02 +0000

    test: convert api mgmt coolection to jest

---
+++ gravitee-am-test/specs/management/api-management/fixtures/api-management-alert-fixture.ts
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { Domain } from '@management-models/Domain';
+import { requestAdminAccessToken } from '@management-commands/token-management-commands';
+import {
+  createDomain,
+  patchDomain,
+  safeDeleteDomain,
+} from '@management-commands/domain-management-commands';
+import { uniqueName } from '@utils-commands/misc';
+import { Fixture } from '../../../test-fixture';
+
+export interface ApiManagementAlertFixture extends Fixture {
+  domain: Domain;
+}
+
+export const setupApiManagementAlertFixture = async (): Promise<ApiManagementAlertFixture> => {
+  const accessToken = await requestAdminAccessToken();
+  const domain = await createDomain(
+    accessToken,
+    uniqueName('alert', true),
+    'test alerting on domain',
+  );
+
+  const cleanup = async () => {
+    if (domain?.id && accessToken) {
+      await safeDeleteDomain(domain.id, accessToken);
+    }
+  };
+
+  return {
+    accessToken,
+    domain,
+    cleanUp: cleanup,
+  };
+};
