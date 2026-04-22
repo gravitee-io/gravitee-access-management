3a5a5327bbb7e0e89e8af6a0ae38734de07db62c
stuart.clark <stuart.clark@graviteesource.com>
2026-04-22 15:27:01 +0100

    feat: add read-only /applications/agents endpoint with agent_applications side table

--- gravitee-am-management-api/gravitee-am-management-api-rest/src/main/java/io/gravitee/am/management/handlers/management/api/resources/organizations/environments/domains/ApplicationsResource.java
+++ gravitee-am-management-api/gravitee-am-management-api-rest/src/main/java/io/gravitee/am/management/handlers/management/api/resources/organizations/environments/domains/ApplicationsResource.java
@@ -184,6 +184,11 @@ public class ApplicationsResource extends AbstractDomainResource {
                 .subscribe(response::resume, response::resume);
     }
 
+    @Path("agents")
+    public AgentApplicationsResource getAgentApplicationsResource() {
+        return resourceContext.getResource(AgentApplicationsResource.class);
+    }
+
     @Path("{application}")
     public ApplicationResource getApplicationResource() {
         return resourceContext.getResource(ApplicationResource.class);
