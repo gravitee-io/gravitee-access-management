f37770f3160f90c1669b9f401fb435a6fafba50f
stuart.clark <stuart.clark@graviteesource.com>
2026-04-27 20:49:14 +0100

    chore(test): regen management SDK for trust domains and SPIFFE settings

--- gravitee-am-test/api/management/models/PatchSAMLSettings.ts
+++ gravitee-am-test/api/management/models/PatchSAMLSettings.ts
@@ -110,6 +110,7 @@ export const PatchSAMLSettingsRequiredPermissionsEnum = {
   DomainAuthdeviceNotifier: 'DOMAIN_AUTHDEVICE_NOTIFIER',
   DomainI18NDictionary: 'DOMAIN_I18N_DICTIONARY',
   DomainTheme: 'DOMAIN_THEME',
+  DomainTrustDomain: 'DOMAIN_TRUST_DOMAIN',
   Application: 'APPLICATION',
   ApplicationSettings: 'APPLICATION_SETTINGS',
   ApplicationIdentityProvider: 'APPLICATION_IDENTITY_PROVIDER',
