64de611f9040c142f1b4332aef159fc76a8af314
Lukasz Gawel <lukasz.gawel@graviteesource.com>
2026-05-08 16:06:52 +0200

    feat: userinfo profile custom claims
    solves AM-7008

--- gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-oidc/src/test/java/io/gravitee/am/gateway/handler/oidc/resources/endpoint/UserInfoEndpointHandlerTest.java
+++ gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-oidc/src/test/java/io/gravitee/am/gateway/handler/oidc/resources/endpoint/UserInfoEndpointHandlerTest.java
@@ -26,6 +26,7 @@ import io.gravitee.am.gateway.handler.common.vertx.RxWebTestBase;
 import io.gravitee.am.gateway.handler.common.vertx.web.auth.handler.OAuth2AuthHandler;
 import io.gravitee.am.gateway.handler.common.vertx.web.auth.handler.OAuth2AuthResponse;
 import io.gravitee.am.gateway.handler.common.vertx.web.auth.provider.OAuth2AuthProvider;
+import io.gravitee.am.gateway.handler.context.ExecutionContextFactory;
 import io.gravitee.am.gateway.handler.oauth2.exception.InvalidClientException;
 import io.gravitee.am.gateway.handler.oauth2.exception.ServerErrorException;
 import io.gravitee.am.gateway.handler.oauth2.resources.handler.ExceptionHandler;
@@ -33,11 +34,14 @@ import io.gravitee.am.gateway.handler.oidc.service.discovery.OpenIDDiscoveryServ
 import io.gravitee.am.gateway.handler.oidc.service.jwe.JWEService;
 import io.gravitee.am.model.Role;
 import io.gravitee.am.model.User;
+import io.gravitee.am.model.UserInfoClaim;
 import io.gravitee.am.model.oidc.Client;
 import io.gravitee.am.service.impl.user.UserEnhancer;
 import io.gravitee.common.http.HttpHeaders;
 import io.gravitee.common.http.HttpStatusCode;
 import io.gravitee.common.http.MediaType;
+import io.gravitee.el.TemplateEngine;
+import io.gravitee.gateway.api.ExecutionContext;
 import io.reactivex.rxjava3.core.Maybe;
 import io.reactivex.rxjava3.core.Single;
 import io.vertx.core.AsyncResult;
@@ -61,6 +65,9 @@ import java.util.List;
 import java.util.Map;
 
 import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 /**
@@ -88,6 +95,9 @@ public class UserInfoEndpointHandlerTest extends RxWebTestBase {
     @Mock
     private Environment env;
 
+    @Mock
+    private ExecutionContextFactory executionContextFactory;
+
     private UserInfoEndpoint userInfoEndpoint;
 
     @Override
@@ -95,7 +105,7 @@ public class UserInfoEndpointHandlerTest extends RxWebTestBase {
         super.setUp();
 
         when(env.getProperty("legacy.openid.openid_scope_full_profile", boolean.class, false)).thenReturn(false);
-        userInfoEndpoint = new UserInfoEndpoint(userEnhancer, jwtService, jweService, openIDDiscoveryService, env, subjectManager);
+        userInfoEndpoint = new UserInfoEndpoint(userEnhancer, jwtService, jweService, openIDDiscoveryService, env, subjectManager, executionContextFactory);
 
         router.route(HttpMethod.GET, "/userinfo")
                 .handler(userInfoEndpoint);
@@ -764,6 +774,76 @@ public class UserInfoEndpointHandlerTest extends RxWebTestBase {
                     assertEquals(MediaType.APPLICATION_JWT,resp.getHeader(HttpHeaders.CONTENT_TYPE));
                     resp.bodyHandler(body -> assertEquals("signedJwtBearer",body.toString()));
                 },
+                HttpStatusCode.OK_200, "OK", null);
+    }
+
+    @Test
+    public void shouldInvokeUserEndpoint_userinfoCustomClaims_emptyList_skipsEnhancement() throws Exception {
+        JWT jwt = new JWT();
+        jwt.setJti("id-token");
+        jwt.setAud("client-id");
+        jwt.setSub("id-subject");
+        jwt.setScope("openid");
+
+        Client client = new Client();
+        client.setId("client-id");
+        client.setClientId("client-id");
+        client.setUserinfoCustomClaims(Collections.emptyList());
+
+        router.route().order(-1).handler(createOAuth2AuthHandler(oAuth2AuthProvider(jwt, client)));
+
+        User user = createUser();
+        when(subjectManager.findUserBySub(any())).thenReturn(Maybe.just(user));
+
+        testRequest(
+                HttpMethod.GET,
+                "/userinfo",
+                req -> req.putHeader(HttpHeaders.AUTHORIZATION, "Bearer test-token"),
+                resp -> resp.bodyHandler(body -> {
+                    final Map<String, Object> claims = Json.decodeValue(body.toString(), Map.class);
+                    assertNotNull(claims);
+                    assertFalse(claims.containsKey("custom_userinfo"));
+                }),
+                HttpStatusCode.OK_200, "OK", null);
+
+        verify(executionContextFactory, never()).create(any());
+    }
+
+    @Test
+    public void shouldInvokeUserEndpoint_userinfoCustomClaims_evaluatesAndAddsClaim() throws Exception {
+        JWT jwt = new JWT();
+        jwt.setJti("id-token");
+        jwt.setAud("client-id");
+        jwt.setSub("id-subject");
+        jwt.setScope("openid");
+
+        Client client = new Client();
+        client.setId("client-id");
+        client.setClientId("client-id");
+        client.setUserinfoCustomClaims(List.of(UserInfoClaim.of("custom_userinfo", "#{'evaluated'}")));
+
+        router.route().order(-1).handler(createOAuth2AuthHandler(oAuth2AuthProvider(jwt, client)));
+
+        User user = createUser();
+        when(subjectManager.findUserBySub(any())).thenReturn(Maybe.just(user));
+
+        ExecutionContext executionContext = mock(ExecutionContext.class);
+        TemplateEngine templateEngine = mock(TemplateEngine.class);
+        when(executionContext.getTemplateEngine()).thenReturn(templateEngine);
+        when(executionContext.getAttributes()).thenReturn(new HashMap<>());
+        when(templateEngine.getValue("#{'evaluated'}", Object.class)).thenReturn("evaluated");
+        when(executionContextFactory.create(any())).thenReturn(executionContext);
+
+        testRequest(
+                HttpMethod.GET,
+                "/userinfo",
+                req -> req.putHeader(HttpHeaders.AUTHORIZATION, "Bearer test-token"),
+                resp -> resp.bodyHandler(body -> {
+                    final Map<String, Object> claims = Json.decodeValue(body.toString(), Map.class);
+                    assertNotNull(claims);
+                    assertTrue(claims.containsKey("custom_userinfo"));
+                    assertEquals("evaluated", claims.get("custom_userinfo"));
+                }),
                 HttpStatusCode.OK_200, "OK", null);
     }
 
