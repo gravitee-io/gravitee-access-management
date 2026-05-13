994ac4da21f522ac3bd298798ce380ff7a92b552
wajb <william.blake@graviteesource.com>
2026-05-13 16:04:16 +0100

    fix: map domain master value
    AM-7012
    
    (cherry picked from commit e782e4baa596b915afe12ce02fa396e33d946676)

--- gravitee-am-management-api/gravitee-am-management-api-rest/src/main/java/io/gravitee/am/management/handlers/management/api/resources/organizations/environments/domains/AbstractDomainResource.java
+++ gravitee-am-management-api/gravitee-am-management-api-rest/src/main/java/io/gravitee/am/management/handlers/management/api/resources/organizations/environments/domains/AbstractDomainResource.java
@@ -47,6 +47,7 @@ public class AbstractDomainResource extends AbstractResource {
         filteredDomain.setName(domain.getName());
         filteredDomain.setDescription(domain.getDescription());
         filteredDomain.setEnabled(domain.isEnabled());
+        filteredDomain.setMaster(domain.isMaster());
         filteredDomain.setCreatedAt(domain.getCreatedAt());
         filteredDomain.setUpdatedAt(domain.getUpdatedAt());
 
