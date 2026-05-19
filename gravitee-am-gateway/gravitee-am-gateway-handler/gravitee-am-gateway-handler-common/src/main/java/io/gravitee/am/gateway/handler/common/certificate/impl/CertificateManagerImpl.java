d9d9d1cd7f5324d2eb0e2f3258c1874b073d93bd
Lukasz Gawel <lukasz.gawel@graviteesource.com>
2026-05-19 12:30:39 +0200

    fix: token validation on kid
    fixes AM-6966
    
    (cherry picked from commit 829e35e2c958d85c47977b6b8807056ca606ae74)

--- gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-common/src/main/java/io/gravitee/am/gateway/handler/common/certificate/impl/CertificateManagerImpl.java
+++ gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-common/src/main/java/io/gravitee/am/gateway/handler/common/certificate/impl/CertificateManagerImpl.java
@@ -298,10 +298,12 @@ public class CertificateManagerImpl extends AbstractService implements Certifica
 
     private void initDefaultCertificateProvider() throws InvalidKeyException {
         this.defaultCertificateProvider = certificateProviderManager.create(CertificateProviders.createShaCertificateProvider(signingKeyId(), signingKeySecret()));
+        this.defaultCertificateProvider.setDomain(domain.getId());
     }
 
     private void initNoneAlgorithmCertificateProvider() {
         this.noneAlgorithmCertificateProvider = certificateProviderManager.create(CertificateProviders.createNoneCertificateProvider());
+        this.noneAlgorithmCertificateProvider.setDomain(domain.getId());
     }
 
     private void initCertificateSettings() {
