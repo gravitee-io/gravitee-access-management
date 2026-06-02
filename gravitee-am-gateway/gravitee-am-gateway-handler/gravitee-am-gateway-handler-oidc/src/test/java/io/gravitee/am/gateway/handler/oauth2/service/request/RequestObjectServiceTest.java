73f8a977a405921f4b2a25e2d45e1181d4bd3a19
eric <eric.leleu@graviteesource.com>
2026-06-02 15:13:55 +0200

    fix: manage to resolve client keys before doing check on them on RequestObject validation
    this avoid nullpointer exception when teh JWKSet is providing from a URL
    
    fixes AM-7069

--- gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-oidc/src/test/java/io/gravitee/am/gateway/handler/oauth2/service/request/RequestObjectServiceTest.java
+++ gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-oidc/src/test/java/io/gravitee/am/gateway/handler/oauth2/service/request/RequestObjectServiceTest.java
@@ -17,18 +17,29 @@ package io.gravitee.am.gateway.handler.oauth2.service.request;
 
 import com.nimbusds.jose.JWSAlgorithm;
 import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.crypto.RSASSASigner;
 import com.nimbusds.jwt.JWT;
 import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.PlainJWT;
 import com.nimbusds.jwt.SignedJWT;
+import io.gravitee.am.common.exception.oauth2.InvalidRequestException;
 import io.gravitee.am.common.exception.oauth2.InvalidRequestObjectException;
 import io.gravitee.am.common.exception.oauth2.InvalidRequestUriException;
+import io.gravitee.am.gateway.handler.oidc.service.discovery.OpenIDDiscoveryService;
 import io.gravitee.am.gateway.handler.oidc.service.jwe.JWEService;
+import io.gravitee.am.gateway.handler.oidc.service.jwk.JWKService;
+import io.gravitee.am.gateway.handler.oidc.service.jws.JWSService;
+import io.gravitee.am.gateway.handler.oidc.service.request.RequestObjectRegistrationRequest;
+import io.gravitee.am.gateway.handler.oidc.service.request.RequestObjectRegistrationResponse;
 import io.gravitee.am.gateway.handler.oidc.service.request.RequestObjectService;
 import io.gravitee.am.gateway.handler.oidc.service.request.impl.RequestObjectServiceImpl;
 import io.gravitee.am.model.Domain;
 import io.gravitee.am.model.oidc.Client;
+import io.gravitee.am.model.oidc.JWKSet;
 import io.gravitee.am.model.oidc.OIDCSettings;
+import io.gravitee.am.repository.oidc.api.RequestObjectRepository;
+import io.gravitee.am.repository.oidc.model.RequestObject;
+import io.reactivex.rxjava3.core.Maybe;
 import io.reactivex.rxjava3.core.Single;
 import io.reactivex.rxjava3.observers.TestObserver;
 import io.vertx.rxjava3.ext.web.client.HttpRequest;
@@ -41,7 +52,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPrivateKey;
 import java.text.ParseException;
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -66,6 +81,18 @@ public class RequestObjectServiceTest {
 
     @Mock
     private JWEService jweService;
+
+    @Mock
+    private JWSService jwsService;
+
+    @Mock
+    private JWKService jwkService;
+
+    @Mock
+    private OpenIDDiscoveryService openIDDiscoveryService;
+
+    @Mock
+    private RequestObjectRepository requestObjectRepository;
 
     @Mock
     private Domain domain;
@@ -198,5 +225,192 @@ public class RequestObjectServiceTest {
 
         verify(webClient).getAbs(anyString());
         verify(jweService).decrypt(argThat(json -> json.equals("request_uri_payload")),any(), anyBoolean());
+    }
+
+    // --- registerRequestObject tests ---
+
+    @Test
+    public void shouldNotRegisterRequestObject_parseError() {
+        Client client = new Client();
+        RequestObjectRegistrationRequest request = new RequestObjectRegistrationRequest();
+        request.setRequest("not-a-valid-jwt");
+
+        TestObserver<RequestObjectRegistrationResponse> testObserver =
+                requestObjectService.registerRequestObject(request, client).test();
+        testObserver.assertNotComplete();
+        testObserver.assertError(InvalidRequestObjectException.class);
+    }
+
+    @Test
+    public void shouldNotRegisterRequestObject_plainJwt() throws Exception {
+        Client client = new Client();
+        PlainJWT plainJWT = new PlainJWT(new JWTClaimsSet.Builder().subject("test").build());
+
+        RequestObjectRegistrationRequest request = new RequestObjectRegistrationRequest();
+        request.setRequest(plainJWT.serialize());
+
+        TestObserver<RequestObjectRegistrationResponse> testObserver =
+                requestObjectService.registerRequestObject(request, client).test();
+        testObserver.assertNotComplete();
+        testObserver.assertError(InvalidRequestObjectException.class);
+    }
+
+    @Test
+    public void shouldNotRegisterRequestObject_keyNotInClientJwks() throws Exception {
+        KeyPair keyPair = generateRsaKeyPair();
+        SignedJWT signedJWT = buildSignedJWT("jwt-key-id", JWSAlgorithm.RS256, (RSAPrivateKey) keyPair.getPrivate());
+
+        io.gravitee.am.model.jose.RSAKey rsaKey = new io.gravitee.am.model.jose.RSAKey();
+        rsaKey.setKid("different-key-id");
+        JWKSet jwkSet = new JWKSet();
+        jwkSet.setKeys(List.of(rsaKey));
+
+        Client client = new Client();
+        client.setJwks(jwkSet);
+        when(jwkService.getKeys(any(Client.class))).thenReturn(Maybe.just(jwkSet));
+
+        RequestObjectRegistrationRequest request = new RequestObjectRegistrationRequest();
+        request.setRequest(signedJWT.serialize());
+
+        TestObserver<RequestObjectRegistrationResponse> testObserver =
+                requestObjectService.registerRequestObject(request, client).test();
+        testObserver.assertNotComplete();
+        testObserver.assertError(InvalidRequestException.class);
+    }
+
+    @Test
+    public void shouldNotRegisterRequestObject_noKeysFromJwkService() throws Exception {
+        KeyPair keyPair = generateRsaKeyPair();
+        SignedJWT signedJWT = buildSignedJWT("my-key-id", JWSAlgorithm.RS256, (RSAPrivateKey) keyPair.getPrivate());
+
+        io.gravitee.am.model.jose.RSAKey rsaKey = new io.gravitee.am.model.jose.RSAKey();
+        rsaKey.setKid("my-key-id");
+        JWKSet jwkSet = new JWKSet();
+        jwkSet.setKeys(List.of(rsaKey));
+
+        Client client = new Client();
+        client.setJwks(jwkSet);
+        client.setRequestObjectSigningAlg(JWSAlgorithm.RS256.getName());
+
+        when(jwkService.getKeys(client)).thenReturn(Maybe.error(new InvalidRequestObjectException("No keys found")));
+
+        RequestObjectRegistrationRequest request = new RequestObjectRegistrationRequest();
+        request.setRequest(signedJWT.serialize());
+
+        TestObserver<RequestObjectRegistrationResponse> testObserver =
+                requestObjectService.registerRequestObject(request, client).test();
+        testObserver.assertNotComplete();
+        testObserver.assertError(InvalidRequestObjectException.class);
+    }
+
+    @Test
+    public void shouldNotRegisterRequestObject_algorithmMismatch() throws Exception {
+        KeyPair keyPair = generateRsaKeyPair();
+        SignedJWT signedJWT = buildSignedJWT("my-key-id", JWSAlgorithm.RS256, (RSAPrivateKey) keyPair.getPrivate());
+
+        io.gravitee.am.model.jose.RSAKey rsaKey = new io.gravitee.am.model.jose.RSAKey();
+        rsaKey.setKid("my-key-id");
+        JWKSet jwkSet = new JWKSet();
+        jwkSet.setKeys(List.of(rsaKey));
+        io.gravitee.am.model.jose.JWK jwk = mock(io.gravitee.am.model.jose.JWK.class);
+
+        Client client = new Client();
+        client.setJwks(jwkSet);
+        client.setRequestObjectSigningAlg("PS256");
+
+        when(jwkService.getKeys(client)).thenReturn(Maybe.just(jwkSet));
+        when(jwkService.getKey(any(), anyString())).thenReturn(Maybe.just(jwk));
+
+        RequestObjectRegistrationRequest request = new RequestObjectRegistrationRequest();
+        request.setRequest(signedJWT.serialize());
+
+        TestObserver<RequestObjectRegistrationResponse> testObserver =
+                requestObjectService.registerRequestObject(request, client).test();
+        testObserver.assertNotComplete();
+        testObserver.assertError(InvalidRequestObjectException.class);
+    }
+
+    @Test
+    public void shouldNotRegisterRequestObject_invalidSignature() throws Exception {
+        KeyPair keyPair = generateRsaKeyPair();
+        SignedJWT signedJWT = buildSignedJWT("my-key-id", JWSAlgorithm.RS256, (RSAPrivateKey) keyPair.getPrivate());
+
+        io.gravitee.am.model.jose.RSAKey rsaKey = new io.gravitee.am.model.jose.RSAKey();
+        rsaKey.setKid("my-key-id");
+        JWKSet jwkSet = new JWKSet();
+        jwkSet.setKeys(List.of(rsaKey));
+        io.gravitee.am.model.jose.JWK jwk = mock(io.gravitee.am.model.jose.JWK.class);
+
+        Client client = new Client();
+        client.setJwks(jwkSet);
+        client.setRequestObjectSigningAlg(JWSAlgorithm.RS256.getName());
+
+        when(jwkService.getKeys(client)).thenReturn(Maybe.just(jwkSet));
+        when(jwkService.getKey(any(), anyString())).thenReturn(Maybe.just(jwk));
+        when(jwsService.isValidSignature(any(), any())).thenReturn(false);
+
+        RequestObjectRegistrationRequest request = new RequestObjectRegistrationRequest();
+        request.setRequest(signedJWT.serialize());
+
+        TestObserver<RequestObjectRegistrationResponse> testObserver =
+                requestObjectService.registerRequestObject(request, client).test();
+        testObserver.assertNotComplete();
+        testObserver.assertError(InvalidRequestObjectException.class);
+    }
+
+    @Test
+    public void shouldRegisterRequestObject() throws Exception {
+        KeyPair keyPair = generateRsaKeyPair();
+        SignedJWT signedJWT = buildSignedJWT("my-key-id", JWSAlgorithm.RS256, (RSAPrivateKey) keyPair.getPrivate());
+
+        io.gravitee.am.model.jose.RSAKey rsaKey = new io.gravitee.am.model.jose.RSAKey();
+        rsaKey.setKid("my-key-id");
+        JWKSet jwkSet = new JWKSet();
+        jwkSet.setKeys(List.of(rsaKey));
+        io.gravitee.am.model.jose.JWK jwk = mock(io.gravitee.am.model.jose.JWK.class);
+
+        Client client = new Client();
+        client.setClientId("my-client");
+        client.setJwks(jwkSet);
+        client.setRequestObjectSigningAlg(JWSAlgorithm.RS256.getName());
+
+        RequestObject storedObject = new RequestObject();
+        storedObject.setId("stored-obj-id");
+        storedObject.setExpireAt(new Date(System.currentTimeMillis() + 86400_000L));
+
+        when(jwkService.getKeys(client)).thenReturn(Maybe.just(jwkSet));
+        when(jwkService.getKey(any(), anyString())).thenReturn(Maybe.just(jwk));
+        when(jwsService.isValidSignature(any(), any())).thenReturn(true);
+        when(requestObjectRepository.create(any())).thenReturn(Single.just(storedObject));
+        when(openIDDiscoveryService.getIssuer(any())).thenReturn("https://issuer.example.com");
+
+        RequestObjectRegistrationRequest request = new RequestObjectRegistrationRequest();
+        request.setRequest(signedJWT.serialize());
+        request.setOrigin("https://issuer.example.com");
+
+        TestObserver<RequestObjectRegistrationResponse> testObserver =
+                requestObjectService.registerRequestObject(request, client).test();
+        testObserver.awaitDone(10, TimeUnit.SECONDS);
+        testObserver.assertComplete();
+        testObserver.assertNoErrors();
+        testObserver.assertValue(response ->
+                "my-client".equals(response.getAud())
+                        && response.getRequestUri().startsWith("urn:ros:stored-obj-id")
+                        && "https://issuer.example.com".equals(response.getIss())
+        );
+    }
+
+    private static KeyPair generateRsaKeyPair() throws Exception {
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+        keyGen.initialize(2048);
+        return keyGen.generateKeyPair();
+    }
+
+    private static SignedJWT buildSignedJWT(String keyId, JWSAlgorithm algorithm, RSAPrivateKey privateKey) throws Exception {
+        JWSHeader header = new JWSHeader.Builder(algorithm).keyID(keyId).build();
+        JWTClaimsSet claims = new JWTClaimsSet.Builder().subject("test").build();
+        SignedJWT signedJWT = new SignedJWT(header, claims);
+        signedJWT.sign(new RSASSASigner(privateKey));
+        return signedJWT;
     }
 }
