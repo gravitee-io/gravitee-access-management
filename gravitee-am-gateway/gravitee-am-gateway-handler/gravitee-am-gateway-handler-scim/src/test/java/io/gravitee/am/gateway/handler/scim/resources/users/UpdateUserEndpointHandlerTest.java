832849d4e51746c0155e7aef289dd84e933c1ba7
Lukasz Gawel <lukasz.gawel@graviteesource.com>
2026-06-15 15:30:21 +0200

    feat: enrich scim response body with user details
    fixes AM-7139
    fixes gravitee-io/issues#11539
    
    (cherry picked from commit 5e142e00461c3d595ce8064cf4a9b790a7152c44)

--- gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-scim/src/test/java/io/gravitee/am/gateway/handler/scim/resources/users/UpdateUserEndpointHandlerTest.java
+++ gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-scim/src/test/java/io/gravitee/am/gateway/handler/scim/resources/users/UpdateUserEndpointHandlerTest.java
@@ -15,6 +15,7 @@
  */
 package io.gravitee.am.gateway.handler.scim.resources.users;
 
+import io.gravitee.am.gateway.handler.scim.mapper.ScimErrorMapper;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import io.gravitee.am.common.jwt.JWT;
@@ -91,7 +92,7 @@ public class UpdateUserEndpointHandlerTest extends RxWebTestBase {
                     rc.put(ConstantKeys.TOKEN_CONTEXT_KEY, token);
                     rc.next();
                 })
-                .failureHandler(new ErrorHandler());
+                .failureHandler(new ErrorHandler(new ScimErrorMapper(false)));
     }
 
     @Test
