6354f27ce6031e60d8af3458810905df5751a325
eric <eric.leleu@graviteesource.com>
2026-07-27 11:44:20 +0200

    feat: send managed cloud access point templates to Cockpit on hello
    Expose environment/organization gateway access point templates from
    installation config through CockpitAccessService and include them in
    the HelloCommand payload, and add unit tests for the new/changed
    service and adapter.
    
    related-to AM-7326

---
+++ gravitee-am-management-api/gravitee-am-management-api-service/src/main/java/io/gravitee/am/management/service/impl/CockpitAccessServiceImpl.java
@@ -0,0 +1,103 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.gravitee.am.management.service.impl;
+
+import com.google.common.base.Strings;
+import com.google.common.net.InternetDomainName;
+import io.gravitee.am.common.env.CloudProperties;
+import io.gravitee.am.management.service.CockpitAccessService;
+import io.gravitee.am.management.service.model.AccessPointTemplate;
+import jakarta.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+import lombok.CustomLog;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+@CustomLog
+public class CockpitAccessServiceImpl implements CockpitAccessService {
+
+    private static final String MANAGED_ACCESS_POINT_PROPERTY =
+            "installation." + CloudProperties.INSTALLATION_TYPE_MANAGED + ".accessPoints";
+
+    private final ConfigurableEnvironment environment;
+
+    private final Map<AccessPointTemplate.Type, List<AccessPointTemplate>> accessPointsTemplate = new EnumMap<>(
+            AccessPointTemplate.Type.class
+    );
+
+    @PostConstruct
+    public void afterPropertiesSet() {
+        if (CloudProperties.isManagedCloudEnabled(environment)) {
+
+            for (AccessPointTemplate.Type type : AccessPointTemplate.Type.values()) {
+                List<AccessPointTemplate> accessPoints = new ArrayList<>();
+                for (AccessPointTemplate.Target target : AccessPointTemplate.Target.values()) {
+                    String host = environment.getProperty(
+                            MANAGED_ACCESS_POINT_PROPERTY + "." + type.getLabel() + "." + target.getLabel() + ".host"
+                    );
+                    if (host != null) {
+                        validateHost(host);
+                        Boolean secured = environment.getProperty(
+                                MANAGED_ACCESS_POINT_PROPERTY + "." + type.getLabel() + "." + target.getLabel() + ".secured",
+                                Boolean.class,
+                                true
+                        );
+                        accessPoints.add(AccessPointTemplate.builder().target(target).host(host).secured(secured).build());
+                    }
+                }
+                if (!accessPoints.isEmpty()) {
+                    accessPointsTemplate.put(type, accessPoints);
+                }
+            }
+
+            if (accessPointsTemplate.isEmpty()) {
+                throw new InvalidAccessPointException("Installation is managed but doesn't configure any access points.");
+            }
+        }
+    }
+
+    private void validateHost(String value) {
+        String toValidate = value.replaceAll("[{}]", "").replaceAll(":\\d*", "");
+        if (!isValidDomainName(toValidate)) {
+            throw new InvalidAccessPointException("Installation access point '%s' is malformed.".formatted(value));
+        }
+    }
+
+    private boolean isValidDomainName(String domain) {
+        if (Strings.isNullOrEmpty(domain)) {
+            return false;
+        }
+        return InternetDomainName.isValid(domain);
+    }
+
+    @Override
+    public Map<AccessPointTemplate.Type, List<AccessPointTemplate>> getAccessPointsTemplate() {
+        return accessPointsTemplate;
+    }
+
+    static class InvalidAccessPointException extends RuntimeException {
+        public InvalidAccessPointException(String message) {
+            super(message);
+        }
+    }
+}
---
+++ gravitee-am-management-api/gravitee-am-management-api-service/src/main/java/io/gravitee/am/management/service/model/AccessPointTemplate.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.gravitee.am.management.service.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * @author Eric Leleu (eric.leleu@graviteesource.com)
+ * @author GraviteeSource Team
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+public class AccessPointTemplate {
+
+    private Target target;
+    private String host;
+    private boolean secured;
+
+    @RequiredArgsConstructor
+    @Getter
+    public enum Type {
+        ENVIRONMENT("environment");
+
+        private final String label;
+    }
+
+    @RequiredArgsConstructor
+    @Getter
+    public enum Target {
+        GATEWAY("gateway");
+
+        private final String label;
+    }
+}
