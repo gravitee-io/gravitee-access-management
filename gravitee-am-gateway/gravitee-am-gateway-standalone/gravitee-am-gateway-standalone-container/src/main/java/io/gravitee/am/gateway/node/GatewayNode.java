b7c39803dff02085e59c992d73e913a98546ccff
Lukasz Gawel <lukasz.gawel@graviteesource.com>
2026-08-10 12:53:08 +0200

    fix: reporter index manipulation should be ran only by MAPI
    introduced reports-only ensureIndex setting property
    
    fixes AM-7454
    fixes github-io/issues#11712
    
    (cherry picked from commit 933b5d73077c1d5726b6c4b9e014cc450035eafe)

--- gravitee-am-gateway/gravitee-am-gateway-standalone/gravitee-am-gateway-standalone-container/src/main/java/io/gravitee/am/gateway/node/GatewayNode.java
+++ gravitee-am-gateway/gravitee-am-gateway-standalone/gravitee-am-gateway-standalone-container/src/main/java/io/gravitee/am/gateway/node/GatewayNode.java
@@ -15,6 +15,7 @@
  */
 package io.gravitee.am.gateway.node;
 
+import io.gravitee.am.common.node.AmNode;
 import io.gravitee.am.gateway.reactor.Reactor;
 import io.gravitee.am.gateway.core.upgrader.GatewayUpgraderConfiguration;
 import io.gravitee.am.gateway.vertx.VertxEmbeddedContainer;
@@ -34,7 +35,7 @@ import java.util.Map;
  * @author Titouan COMPIEGNE (titouan.compiegne at graviteesource.com)
  * @author GraviteeSource Team
  */
-public class GatewayNode extends AbstractNode {
+public class GatewayNode extends AbstractNode implements AmNode {
 
     @Autowired
     private NodeMetadataResolver nodeMetadataResolver;
@@ -49,6 +50,11 @@ public class GatewayNode extends AbstractNode {
     @Override
     public String application() {
         return "gio-am-gateway";
+    }
+
+    @Override
+    public boolean isGateway() {
+        return true;
     }
 
     @Override
