b2d464e2eff03bb52fd27acb228ba41c835660a7
stuart.clark <stuart.clark@graviteesource.com>
2026-08-24 15:23:43 +0100

    refactor(management-api): convert every enum query parameter, not just one
    Refs: AM-7476

--- gravitee-am-management-api/gravitee-am-management-api-rest/src/main/java/io/gravitee/am/management/handlers/management/api/ManagementApplication.java
+++ gravitee-am-management-api/gravitee-am-management-api-rest/src/main/java/io/gravitee/am/management/handlers/management/api/ManagementApplication.java
@@ -37,7 +37,7 @@ public class ManagementApplication extends ResourceConfig {
         packages("io.gravitee.am.management.handlers.management.api.resources");
 
         register(SortParamConverterProvider.class);
-        register(ProtectedResourceTypeParamConverterProvider.class);
+        register(EnumParamConverterProvider.class);
         register(JacksonFeature.withoutExceptionMappers());
         register(JacksonExceptionMapper.class);
 
