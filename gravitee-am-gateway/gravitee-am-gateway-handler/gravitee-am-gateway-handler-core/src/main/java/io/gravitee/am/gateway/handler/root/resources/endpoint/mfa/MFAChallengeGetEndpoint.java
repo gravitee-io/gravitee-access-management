bfac6d85819103f455fa1182b1cfe785b6762039
Stuart Clark <stuart.clark@graviteesource.com>
2026-09-02 12:22:15 +0100

    feat(mfa): let users return to the enrollment page from the verify page (#8497)
    fixes: AM-7609
    (cherry picked from commit d6794c5a3ad9933522fd1d8e420d1f96bcc1a7a4)

--- gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-core/src/main/java/io/gravitee/am/gateway/handler/root/resources/endpoint/mfa/MFAChallengeGetEndpoint.java
+++ gravitee-am-gateway/gravitee-am-gateway-handler/gravitee-am-gateway-handler-core/src/main/java/io/gravitee/am/gateway/handler/root/resources/endpoint/mfa/MFAChallengeGetEndpoint.java
@@ -44,11 +44,13 @@ import static io.gravitee.am.common.utils.ConstantKeys.MFA_ALTERNATIVES_ACTION_K
 import static io.gravitee.am.common.utils.ConstantKeys.MFA_ALTERNATIVES_ENABLE_KEY;
 import static io.gravitee.am.common.utils.ConstantKeys.MFA_CHALLENGE_RESEND_ACTION_KEY;
 import static io.gravitee.am.common.utils.ConstantKeys.MFA_CHALLENGE_RESEND_ENABLED_KEY;
+import static io.gravitee.am.common.utils.ConstantKeys.MFA_ENROLL_BACK_ACTION_KEY;
 import static io.gravitee.am.common.utils.ConstantKeys.RATE_LIMIT_ERROR_PARAM_KEY;
 import static io.gravitee.am.common.utils.ConstantKeys.VERIFY_ATTEMPT_ERROR_PARAM_KEY;
 import static io.gravitee.am.gateway.handler.common.utils.ThymeleafDataHelper.generateData;
 import static io.gravitee.am.gateway.handler.common.vertx.utils.UriBuilderRequest.CONTEXT_PATH;
 import static io.gravitee.am.gateway.handler.common.vertx.utils.UriBuilderRequest.resolveProxyRequest;
+import static io.gravitee.am.model.factor.FactorStatus.PENDING_ACTIVATION;
 
 /**
  * @author Titouan COMPIEGNE (titouan.compiegne at graviteesource.com)
@@ -135,9 +137,14 @@ public class MFAChallengeGetEndpoint extends MFAChallengeEndpoint {
                     return;
                 }
 
+                final EnrolledFactor enrolledFactor = resChallenge.result();
                 Map<String, Object> templateData = generateData(routingContext, domainDataPlane.getDomain(), client);
-                if (resChallenge.result() != null) {
-                    templateData.put(ENROLLED_FACTOR_KEY, new EnrolledFactorProperties(resChallenge.result()));
+                if (enrolledFactor != null) {
+                    templateData.put(ENROLLED_FACTOR_KEY, new EnrolledFactorProperties(enrolledFactor));
+                    if (PENDING_ACTIVATION.equals(enrolledFactor.getStatus())) {
+                        templateData.put(MFA_ENROLL_BACK_ACTION_KEY,
+                                resolveProxyRequest(routingContext.request(), routingContext.get(CONTEXT_PATH) + "/mfa/enroll", queryParams, true));
+                    }
                 }
                 this.renderPage(routingContext, templateData, client, logger, "Unable to render MFA challenge page");
             });
