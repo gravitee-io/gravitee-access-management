d23508c38bb4e683f176add40a3070529cb3a00b
wajb <william.blake@graviteesource.com>
2026-09-22 13:22:14 +0100

    fix: invert legacy flag
    AM-7773
    gravitee-io/issues#11866
    
    (cherry picked from commit acfd7b6fb4dd3bfa70472d560e63fc16227d2b66)

--- gravitee-am-service/src/main/java/io/gravitee/am/service/reporter/SystemReporterConfigResolver.java
+++ gravitee-am-service/src/main/java/io/gravitee/am/service/reporter/SystemReporterConfigResolver.java
@@ -56,8 +56,7 @@ public class SystemReporterConfigResolver {
     public static final Set<String> SYSTEM_ONLY_TYPES = Set.of(MONGODB, REPORTER_AM_JDBC);
     public static final String MANAGEMENT_TYPE = Scope.MANAGEMENT.getRepositoryPropertyKey() + ".type";
     public static final String HIDDEN_VALUE = "********";
-    public static final String LEGACY_REPORTERS_DEFAULT_HIDE_SENSITIVE_DATA = "legacy.reporters.default.hide.sensitive.data";
-    public static final boolean HIDE_SENSITIVE_DATA_DEFAULT_VALUE = false;
+    public static final String LEGACY_REPORTERS_DEFAULT_PLAINTEXT_SENSITIVE_DATA = "legacy.reporters.default.plaintextSensitiveData";
 
     private final RepositoriesEnvironment environment;
 
@@ -204,9 +203,8 @@ public class SystemReporterConfigResolver {
         return reporterConfig;
     }
 
-    private Boolean hideSensitiveData() {
-        var hideSensitiveData = environment.getProperty(LEGACY_REPORTERS_DEFAULT_HIDE_SENSITIVE_DATA, Boolean.class, HIDE_SENSITIVE_DATA_DEFAULT_VALUE);
-        return hideSensitiveData;
+    private boolean hideSensitiveData() {
+        return !environment.getProperty(LEGACY_REPORTERS_DEFAULT_PLAINTEXT_SENSITIVE_DATA, Boolean.class, true);
     }
 
     private static String getReporterTableSuffix(Reference reference) {
